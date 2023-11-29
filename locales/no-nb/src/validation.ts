8f54a200a1147ea2eea71f53011e6af645ee2dde
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-11-29 13:45:59 -0800

    feat(no-nb): add actual norwegian translations

--- locales/no-nb/src/validation.ts
+++ locales/no-nb/src/validation.ts
@@ -2,91 +2,134 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Title for the actual "Validation" panel/feature */
-  'panel.title': undefined, // 'Validation'
+  'panel.title': 'Validering',
+
   /** Accessibility label for closing the validation panel */
-  'panel.close-button-aria-label': undefined, // 'Close validation'
+  'panel.close-button-aria-label': 'Lukk validering',
+
   /** Message shown when the validation panel is opened but there are no errors/warnings */
-  'panel.no-errors-message': undefined, // 'No validation errors'
+  'panel.no-errors-message': 'Ingen valideringsfeil',
+
   /** A value of incorrect type is found, eg found `number` instead of `string` */
-  'generic.incorrect-type': undefined, // 'Expected type "{{expectedType}}", got "{{actualType}}"'
+  'generic.incorrect-type': 'Forventet type "{{expectedType}}", fikk "{{actualType}}"',
+
   /** A value is expected, but none is provided */
-  'generic.required': undefined, // 'Required'
+  'generic.required': 'Obligatorisk',
+
   /** Value is not one of the values specifically allowed */
-  'generic.not-allowed': undefined, // 'Value did not match any allowed values'
+  'generic.not-allowed': 'Verdien samsvarer ikke med tillatte verdier',
+
   /** Value "$givenValue" is not one of the values specifically allowed */
-  'generic.not-allowed_hint': undefined, // 'Value "{{hint}}" did not match any allowed values'
+  'generic.not-allowed_hint': 'Verdien "{{hint}}" samsvarer ikke med tillatte verdier',
+
   /** Array has less than the minimum of "$minLength" items */
-  'array.minimum-length': undefined, // 'Must have at least {{minLength}} items'
+  'array.minimum-length': 'Må ha minst {{minLength}} elementer',
+
   /** Portable Text array has less than the minimum of "$minLength" blocks */
-  'array.minimum-length_blocks': undefined, // 'Must have at least {{minLength}} blocks'
+  'array.minimum-length_blocks': 'Må ha minst {{minLength}} blokker',
+
   /** Array has more than the maximum of "$maxLength" items */
-  'array.maximum-length': undefined, // 'Must have at most {{maxLength}} items'
+  'array.maximum-length': 'Må ha maksimalt {{maxLength}} elementer',
+
   /** Portable Text array has more than the maximum of "$maxLength" items */
-  'array.maximum-length_blocks': undefined, // 'Must have at most {{maxLength}} blocks'
+  'array.maximum-length_blocks': 'Må ha maksimalt {{maxLength}} blokker',
+
   /** Array must have exactly "$wantedLength" items, but has more/less */
-  'array.exact-length': undefined, // 'Must have exactly {{wantedLength}} items'
+  'array.exact-length': 'Må ha nøyaktig {{wantedLength}} elementer',
+
   /** Portable Text array must have exactly "$wantedLength" blocks, but has more/less */
-  'array.exact-length_blocks': undefined, // 'Must have exactly {{wantedLength}} blocks'
+  'array.exact-length_blocks': 'Må ha nøyaktig {{wantedLength}} blokker',
+
   /** Array item is a duplicate, but array wants only unique items */
-  'array.item-duplicate': undefined, // 'Can\'t be a duplicate'
+  'array.item-duplicate': `Kan ikke være en duplikat`,
+
   /** Date is not valid or not in the correct format (ISO-8601) */
-  'date.invalid-format': undefined, // 'Must be a valid ISO-8601 formatted date string'
+  'date.invalid-format': 'Må være en gyldig dato i ISO-8601-format',
+
   /** Date is earlier than the given minimum date "$minDate" */
-  'date.minimum': undefined, // 'Must be at or after {{minDate}}'
+  'date.minimum': 'Må være på eller etter {{minDate}}',
+
   /** Date is later than the given maximum date "$maxDate" */
-  'date.maximum': undefined, // 'Must be at or before {{maxDate}}'
+  'date.maximum': 'Må være på eller før {{maxDate}}',
+
   /** Number is not an integer ("whole number") */
-  'number.non-integer': undefined, // 'Must be an integer'
+  'number.non-integer': 'Må være et heltall',
+
   /** Number has more precision (decimals) than the allowed "$limit" */
-  'number.maximum-precision': undefined, // 'Max precision is {{limit}}'
+  'number.maximum-precision': 'Maksimal presisjon er {{limit}}',
+
   /** Number is lower than the given minimum value "$minNumber" */
-  'number.minimum': undefined, // 'Must be greater than or equal to {{minNumber}}'
+  'number.minimum': 'Må være større enn eller lik {{minNumber}}',
+
   /** Number is higher than the given maximum value "$maxNumber" */
-  'number.maximum': undefined, // 'Must be lower than or equal to {{maxNumber}}'
+  'number.maximum': 'Må være mindre enn eller lik {{maxNumber}}',
+
   /** Number is less than the given minimum threshold value "$threshold" */
-  'number.greater-than': undefined, // 'Must be greater than {{threshold}}'
+  'number.greater-than': 'Må være større enn {{threshold}}',
+
   /** Number is greater than the given maximum threshold value "$threshold" */
-  'number.less-than': undefined, // 'Must be less than {{threshold}}'
+  'number.less-than': 'Må være mindre enn {{threshold}}',
+
   /** Object is not a reference to a document (eg `{_ref: 'documentId'}`) */
-  'object.not-reference': undefined, // 'Must be a reference to a document'
+  'object.not-reference': 'Må være en referanse til et dokument',
+
   /** Object references a document which is not published */
-  'object.reference-not-published': undefined, // 'Referenced document must be published'
+  'object.reference-not-published': 'Referert dokument må være publisert',
+
   /** Object is missing a reference to an asset document in its `asset` field */
-  'object.asset-required': undefined, // 'Asset is required'
+  'object.asset-required': 'Ressurs er påkrevd',
+
   /** Object is missing a reference to an image asset document in its `asset` field */
-  'object.asset-required_image': undefined, // 'Image is required'
+  'object.asset-required_image': 'Bilde er påkrevd',
+
   /** Object is missing a reference to a file asset document in its `asset` field */
-  'object.asset-required_file': undefined, // 'File is required'
+  'object.asset-required_file': 'Fil er påkrevd',
+
   /** Slug is not an object (eg `{current: 'some-slug'}`) */
-  'slug.not-object': undefined, // 'Slug must be an object'
+  'slug.not-object': 'Slug må være et objekt',
+
   /** Slug is an object, but is missing a `current` string property */
-  'slug.missing-current': undefined, // 'Slug must have a value'
+  'slug.missing-current': 'Slug må ha en verdi',
+
   /** Slug is already in use somewhere else, but needs to be unique */
-  'slug.not-unique': undefined, // 'Slug is already in use'
+  'slug.not-unique': 'Slug er allerede i bruk et annet sted, og må være unik',
+
   /** String is shorter than the limit of "$minLength" characters */
-  'string.minimum-length': undefined, // 'Must be at least {{minLength}} characters long'
+  'string.minimum-length': 'Må være minst {{minLength}} tegn lang',
+
   /** String is longer than the limit of "$maxLength" characters */
-  'string.maximum-length': undefined, // 'Must be at most {{maxLength}} characters long'
+  'string.maximum-length': 'Må være maksimalt {{maxLength}} tegn lang',
+
   /** String has a different character length than the exact number "$wantedLength" */
-  'string.exact-length': undefined, // 'Must be exactly {{wantedLength}} characters long'
+  'string.exact-length': 'Må være nøyaktig {{wantedLength}} tegn lang',
+
   /** String contains characters that are not in uppercase */
-  'string.uppercase': undefined, // 'Must be all uppercase characters'
+  'string.uppercase': 'Må bestå av kun store bokstaver',
+
   /** String contains characters that are not in lowercase  */
-  'string.lowercase': undefined, // 'Must be all lowercase characters'
+  'string.lowercase': 'Må bestå av kun små bokstaver',
+
   /** String matches the given regular expression, but should not */
-  'string.regex-match': undefined, // 'Should not match "{{name}}"-pattern'
+  'string.regex-match': 'Må ikke stemme overens med "{{name}}"-mønsteret',
+
   /** String does not match the given regular expression, but should */
-  'string.regex-does-not-match': undefined, // 'Does not match "{{name}}"-pattern'
+  'string.regex-does-not-match': 'Må stemme overens med "{{name}}"-mønsteret',
+
   /** String is not a valid email address */
-  'string.email': undefined, // 'Must be a valid email address'
+  'string.email': 'Må være en gyldig e-postadresse',
+
   /** String is not a valid URL */
-  'string.url.invalid': undefined, // 'Not a valid URL'
+  'string.url.invalid': 'Ikke en gyldig URL',
+
   /** String is not a relative URL (eg it contains a protocol/host) */
-  'string.url.not-relative': undefined, // 'Only relative URLs are allowed'
+  'string.url.not-relative': 'Bare relative URL-er er tillatt',
+
   /** String is not an absolute URL (eg it is missing a protocol/host) */
-  'string.url.not-absolute': undefined, // 'Relative URLs are not allowed'
+  'string.url.not-absolute': 'Absolutte URL-er er ikke tillatt',
+
   /** String contains a URL with a username or password specified before the host */
-  'string.url.includes-credentials': undefined, // 'Username/password not allowed'
+  'string.url.includes-credentials': 'Brukernavn/passord er ikke tillatt',
+
   /** String contains a protocol/scheme that is not allowed, eg (`ftp`, `mailto`…) */
-  'string.url.disallowed-scheme': undefined, // 'Does not match allowed protocols/schemes'
+  'string.url.disallowed-scheme': 'Stemmer ikke overens med tillatte protokoller/skjemaer',
 })
--- locales/no-nb/src/vision.ts
+++ locales/no-nb/src/vision.ts
@@ -10,67 +10,87 @@ export default removeUndefinedLocaleResources({
    * Some features has a "New" label indicating that the feature was recently introduced.
    * This defines what the text of that label is. Keep it short and sweet.
    */
-  'label.new': undefined, // 'New'
+  'label.new': 'Nyhet',
 
   /** --- Settings --- */
   /** Label for the "Dataset" dropdown in vision settings */
-  'settings.dataset-label': undefined, // 'Dataset'
+  'settings.dataset-label': 'Datasett',
+
   /** Label for the "API version" dropdown in settings */
-  'settings.api-version-label': undefined, // 'API version'
+  'settings.api-version-label': 'API-versjon',
+
   /** Label for the "other" versions within the "API version" dropdown */
-  'settings.other-api-version-label': undefined, // 'Other'
+  'settings.other-api-version-label': 'Annen',
+
   /** Label for the "Custom API version" input in settings, shown when "other" is chosen as API version */
-  'settings.custom-api-version-label': undefined, // 'Custom API version'
+  'settings.custom-api-version-label': 'Egendefinert API-versjon',
+
   /** Error label for when the API version in 'Custom API version' input is invalid */
-  'settings.error.invalid-api-version': undefined, // 'Invalid API version'
+  'settings.error.invalid-api-version': 'Ugyldig API-versjon',
+
   /**
    * Label for the "Perspective" dropdown in vision settings
    * @see {@link https://www.sanity.io/docs/perspectives}
    */
-  'settings.perspective-label': undefined, // 'Perspective'
+  'settings.perspective-label': 'Perspektiv',
+
   /** Title for popover that explains what "Perspectives" are */
-  'settings.perspectives.title': undefined, // 'Perspectives'
+  'settings.perspectives.title': 'Perspektiver',
+
   /** Description for popover that explains what "Perspectives" are */
-  'settings.perspectives.description': undefined, // 'Perspectives allow your query to run against different "views" of the content in your dataset'
+  'settings.perspectives.description':
+    'Perspektiver lar deg kjøre spørringen din mot forskjellige "visninger" av innholdet i datasettet ditt',
+
   /** Call to action to read the docs related to "Perspectives" */
-  'settings.perspectives.action.docs-link': undefined, // 'Read docs'
+  'settings.perspectives.action.docs-link': 'Les dokumentasjon',
 
   /** --- Query editor --- */
   /** Label for "Query" editor/input */
-  'query.label': undefined, // 'Query'
+  'query.label': 'Spørring',
+
   /** Label for 'Line' indicator when there is an error within the query */
-  'query.error.line': undefined, // 'Line'
+  'query.error.line': 'Linje',
+
   /** Label for 'Column' indicator when there is an error within the query */
-  'query.error.column': undefined, // 'Column'
+  'query.error.column': 'Kolonne',
+
   /** Label for the "Query URL" field, shown after executing a query, and allows for copying */
-  'query.url': undefined, // 'Query URL'
+  'query.url': 'URL',
 
   /** --- Params editor --- */
   /** Label for "Params" (parameters) editor/input */
-  'params.label': undefined, // 'Params'
+  'params.label': 'Parametere',
+
   /** Error message for when the "Params" input are not a valid json */
-  'params.error.params-invalid-json': undefined, // 'Parameters are not valid JSON'
+  'params.error.params-invalid-json': 'Parametere er ikke gyldig JSON',
 
   /** --- Results view/explorer --- */
   /** Label for "Result" explorer/view */
-  'result.label': undefined, // 'Result'
+  'result.label': 'Resultat',
+
   /** Label for "Execution time" information of the fetched query */
-  'result.execution-time-label': undefined, // 'Execution'
+  'result.execution-time-label': 'Utførelsestid',
+
   /** Label for "End to End time" information of the fetched query */
-  'result.end-to-end-time-label': undefined, // 'End-to-end'
+  'result.end-to-end-time-label': 'Ende-til-ende',
+
   /** "Not applicable" message for when there is no Execution time or End to End time information
    * available for the query (eg when the query has not been executed, or errored) */
-  'result.timing-not-applicable': undefined, // 'n/a'
+  'result.timing-not-applicable': '-',
 
   /** --- Actions -- */
   /** Label for executing the query, eg doing a fetch */
-  'action.query-execute': undefined, // 'Fetch'
+  'action.query-execute': 'Utfør',
+
   /** Label for cancelling an ongoing query */
-  'action.query-cancel': undefined, // 'Cancel'
+  'action.query-cancel': 'Avbryt',
+
   /** Label for setting up a listener */
-  'action.listen-execute': undefined, // 'Listen'
+  'action.listen-execute': 'Lytt',
+
   /** Label for stopping an ongoing listen operation */
-  'action.listen-cancel': undefined, // 'Stop'
+  'action.listen-cancel': 'Stopp',
+
   /** Label for action "Copy to clipboard", tied to the "Query URL" field. Also used for accessibility purposes on button */
-  'action.copy-url-to-clipboard': undefined, // 'Copy to clipboard'
+  'action.copy-url-to-clipboard': 'Kopier til utklippstavle',
 })
