e8b39bda50d32105e96692611511f68588c562af
AutoTranslator <hello@sanity.io>
2023-12-06 02:34:08 +0000

    fix(it): automated translation updates

--- locales/it/src/structure.ts
+++ locales/it/src/structure.ts
@@ -81,12 +81,18 @@ export default removeUndefinedLocaleResources({
   'banners.deleted-document-banner.restore-button.text': 'Ripristina la versione più recente',
   /** The text content for the deleted document banner */
   'banners.deleted-document-banner.text': 'Questo documento è stato eliminato.',
-  /** The text for the permission check banner if there is are multiple roles */
-  'banners.permission-check-banner.plural-roles.text':
-    'I tuoi ruoli {{roles}} non hanno i permessi per {{requiredPermission}} questo documento.',
-  /** The text for the permission check banner if there is only one role */
-  'banners.permission-check-banner.singular-role.text':
-    'Il tuo ruolo {{roles}} non ha i permessi per {{requiredPermission}} questo documento.',
+  /** The text for the permission check banner if the user only has one role, and it does not allow updating this document */
+  'banners.permission-check-banner.missing-permission_create_one':
+    'Il tuo ruolo <Roles/> non ha i permessi per creare questo documento.',
+  /** The text for the permission check banner if the user only has multiple roles, but they do not allow updating this document */
+  'banners.permission-check-banner.missing-permission_create_other':
+    'I tuoi ruoli <Roles/> non hanno i permessi per creare questo documento.',
+  /** The text for the permission check banner if the user only has one role, and it does not allow updating this document */
+  'banners.permission-check-banner.missing-permission_update_one':
+    'Il tuo ruolo <Roles/> non ha i permessi per aggiornare questo documento.',
+  /** The text for the permission check banner if the user only has multiple roles, but they do not allow updating this document */
+  'banners.permission-check-banner.missing-permission_update_other':
+    'I tuoi ruoli <Roles/> non hanno i permessi per aggiornare questo documento.',
   /** The text for the reload button */
   'banners.reference-changed-banner.reason-changed.reload-button.text': 'Ricarica riferimento',
   /** The text for the reference change banner if the reason is that the reference has been changed */
@@ -210,7 +216,7 @@ export default removeUndefinedLocaleResources({
   /** Title shown for menu item that opens the "Inspect" dialog */
   'document-inspector.menu-item.title': 'Ispeziona',
   /** the placeholder text for the search input on the inspect dialog */
-  'document-inspector.search.placeholder': undefined, // 'Search'
+  'document-inspector.search.placeholder': 'Cerca',
   /** The "parsed" view mode, meaning the JSON is searchable, collapsible etc */
   'document-inspector.view-mode.parsed': 'Analizzato',
   /** The "raw" view mode, meaning the JSON is presented syntax-highlighted, but with no other features - optimal for copying */
