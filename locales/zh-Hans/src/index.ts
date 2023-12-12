d1141013d97fbd862cf984fbef7c248bb0d18f5c
AutoTranslator <hello@sanity.io>
2023-12-12 01:05:48 +0000

    fix(zh-Hans): automated translation updates

--- locales/zh-Hans/src/index.ts
+++ locales/zh-Hans/src/index.ts
@@ -25,7 +25,7 @@ const locale = defineLocale({
     },
   ],
   weekInfo: {
-    firstDay: 1,
+    firstDay: 7,
     minimalDays: 1,
     weekend: [6, 7],
   },
--- locales/zh-Hans/src/studio.ts
+++ locales/zh-Hans/src/studio.ts
@@ -269,19 +269,19 @@ export default removeUndefinedLocaleResources({
   'changes.title': '审查更改',
 
   /** Title for the default ordering/SortOrder if no orderings are provided and the caption field is found */
-  'default-orderings.caption': undefined, // 'Sort by Caption'
+  'default-orderings.caption': '按标题排序',
   /** Title for the default ordering/SortOrder if no orderings are provided and the description field is found */
-  'default-orderings.description': undefined, // 'Sort by Description'
+  'default-orderings.description': '按描述排序',
   /** Title for the default ordering/SortOrder if no orderings are provided and the header field is found */
-  'default-orderings.header': undefined, // 'Sort by Header'
+  'default-orderings.header': '按页眉排序',
   /** Title for the default ordering/SortOrder if no orderings are provided and the heading field is found */
-  'default-orderings.heading': undefined, // 'Sort by Heading'
+  'default-orderings.heading': '按标题排序',
   /** Title for the default ordering/SortOrder if no orderings are provided and the label field is found */
-  'default-orderings.label': undefined, // 'Sort by Label'
+  'default-orderings.label': '按标签排序',
   /** Title for the default ordering/SortOrder if no orderings are provided and the name field is found */
-  'default-orderings.name': undefined, // 'Sort by Name'
+  'default-orderings.name': '按名称排序',
   /** Title for the default ordering/SortOrder if no orderings are provided and the title field is found */
-  'default-orderings.title': undefined, // 'Sort by Title'
+  'default-orderings.title': '按标题排序',
 
   /** The value of the <code>_key</code> property must be a unique string. */
   'form.error.duplicate-keys-alert.details.additional-description':
