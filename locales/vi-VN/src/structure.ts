82a59654576846f76b2174b41f353440229a15cc
AutoTranslator <hello@sanity.io>
2023-12-12 01:06:15 +0000

    fix(vi-VN): automated translation updates

--- locales/vi-VN/src/structure.ts
+++ locales/vi-VN/src/structure.ts
@@ -181,9 +181,9 @@ export default removeUndefinedLocaleResources({
     'Bạn có thể không thể bỏ xuất bản “<DocumentTitle/>” vì các tài liệu sau đây tham chiếu đến nó:',
 
   /** The text for the cancel button in the confirm dialog used in document action shortcuts if none is provided */
-  'confirm-dialog.cancel-button.fallback-text': undefined, // 'Cancel'
+  'confirm-dialog.cancel-button.fallback-text': 'Hủy bỏ',
   /** The text for the confirm button in the confirm dialog used in document action shortcuts if none is provided */
-  'confirm-dialog.confirm-button.fallback-text': undefined, // 'Confirm'
+  'confirm-dialog.confirm-button.fallback-text': 'Xác nhận',
 
   /** The text shown if there was an error while getting the document's title via a preview value */
   'doc-title.error.text': 'Lỗi: {{errorMessage}}',
@@ -220,13 +220,13 @@ export default removeUndefinedLocaleResources({
 
   /** --- Menu items --- */
   /** The menu item title to use the compact view */
-  'menu-items.layout.compact-view': undefined, // 'Compact view'
+  'menu-items.layout.compact-view': 'Chế độ xem gọn gàng',
   /** The menu item title to use the detailed view */
-  'menu-items.layout.detailed-view': undefined, // 'Detailed view'
+  'menu-items.layout.detailed-view': 'Chế độ xem chi tiết',
   /** The menu item title to Sort by Created */
-  'menu-items.sort-by.created': undefined, // 'Sort by Created'
+  'menu-items.sort-by.created': 'Sắp xếp theo Ngày tạo',
   /** The menu item title to Sort by Last Edited */
-  'menu-items.sort-by.last-edited': undefined, // 'Sort by Last Edited'
+  'menu-items.sort-by.last-edited': 'Sắp xếp theo Lần chỉnh sửa cuối',
 
   /** The link text of the no document type screen that appears directly below the subtitle */
   'no-document-types-screen.link-text': 'Tìm hiểu cách thêm một loại tài liệu →',
--- locales/vi-VN/src/studio.ts
+++ locales/vi-VN/src/studio.ts
@@ -279,19 +279,19 @@ export default removeUndefinedLocaleResources({
   'changes.title': 'Xem lại các thay đổi',
 
   /** Title for the default ordering/SortOrder if no orderings are provided and the caption field is found */
-  'default-orderings.caption': undefined, // 'Sort by Caption'
+  'default-orderings.caption': 'Sắp xếp theo Chú thích',
   /** Title for the default ordering/SortOrder if no orderings are provided and the description field is found */
-  'default-orderings.description': undefined, // 'Sort by Description'
+  'default-orderings.description': 'Sắp xếp theo Mô tả',
   /** Title for the default ordering/SortOrder if no orderings are provided and the header field is found */
-  'default-orderings.header': undefined, // 'Sort by Header'
+  'default-orderings.header': 'Sắp xếp theo Đầu trang',
   /** Title for the default ordering/SortOrder if no orderings are provided and the heading field is found */
-  'default-orderings.heading': undefined, // 'Sort by Heading'
+  'default-orderings.heading': 'Sắp xếp theo Tiêu đề',
   /** Title for the default ordering/SortOrder if no orderings are provided and the label field is found */
-  'default-orderings.label': undefined, // 'Sort by Label'
+  'default-orderings.label': 'Sắp xếp theo Nhãn',
   /** Title for the default ordering/SortOrder if no orderings are provided and the name field is found */
-  'default-orderings.name': undefined, // 'Sort by Name'
+  'default-orderings.name': 'Sắp xếp theo Tên',
   /** Title for the default ordering/SortOrder if no orderings are provided and the title field is found */
-  'default-orderings.title': undefined, // 'Sort by Title'
+  'default-orderings.title': 'Sắp xếp theo Tiêu đề',
 
   /** The value of the <code>_key</code> property must be a unique string. */
   'form.error.duplicate-keys-alert.details.additional-description':
