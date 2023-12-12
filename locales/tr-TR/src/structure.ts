5dfe1337ca89a04d7ed06e53927c3956d64d1de5
AutoTranslator <hello@sanity.io>
2023-12-12 01:05:48 +0000

    fix(tr-TR): automated translation updates

--- locales/tr-TR/src/structure.ts
+++ locales/tr-TR/src/structure.ts
@@ -199,9 +199,9 @@ export default removeUndefinedLocaleResources({
     '“<DocumentTitle/>” başlıklı belgeyi yayından kaldırmakta zorlanabilirsiniz çünkü aşağıdaki belgeler ona atıfta bulunuyor:',
 
   /** The text for the cancel button in the confirm dialog used in document action shortcuts if none is provided */
-  'confirm-dialog.cancel-button.fallback-text': undefined, // 'Cancel'
+  'confirm-dialog.cancel-button.fallback-text': 'İptal',
   /** The text for the confirm button in the confirm dialog used in document action shortcuts if none is provided */
-  'confirm-dialog.confirm-button.fallback-text': undefined, // 'Confirm'
+  'confirm-dialog.confirm-button.fallback-text': 'Onayla',
 
   /** The text shown if there was an error while getting the document's title via a preview value */
   'doc-title.error.text': 'Hata: {{errorMessage}}',
@@ -238,13 +238,13 @@ export default removeUndefinedLocaleResources({
 
   /** --- Menu items --- */
   /** The menu item title to use the compact view */
-  'menu-items.layout.compact-view': undefined, // 'Compact view'
+  'menu-items.layout.compact-view': 'Kompakt görünüm',
   /** The menu item title to use the detailed view */
-  'menu-items.layout.detailed-view': undefined, // 'Detailed view'
+  'menu-items.layout.detailed-view': 'Detaylı görünüm',
   /** The menu item title to Sort by Created */
-  'menu-items.sort-by.created': undefined, // 'Sort by Created'
+  'menu-items.sort-by.created': 'Oluşturulma Tarihine Göre Sırala',
   /** The menu item title to Sort by Last Edited */
-  'menu-items.sort-by.last-edited': undefined, // 'Sort by Last Edited'
+  'menu-items.sort-by.last-edited': 'Son Düzenlenme Tarihine Göre Sırala',
 
   /** The link text of the no document type screen that appears directly below the subtitle */
   'no-document-types-screen.link-text': 'Belge tipi nasıl ekleneceğini öğrenin →',
