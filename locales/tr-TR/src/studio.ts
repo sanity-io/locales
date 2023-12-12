f1d52efa8632159c8fa56d7d70b1106e558ea869
AutoTranslator <hello@sanity.io>
2023-12-12 01:06:13 +0000

    fix(tr-TR): automated translation updates

--- locales/tr-TR/src/studio.ts
+++ locales/tr-TR/src/studio.ts
@@ -295,19 +295,19 @@ export default removeUndefinedLocaleResources({
   'changes.title': 'Değişiklikleri İncele',
 
   /** Title for the default ordering/SortOrder if no orderings are provided and the caption field is found */
-  'default-orderings.caption': undefined, // 'Sort by Caption'
+  'default-orderings.caption': 'Başlığa Göre Sırala',
   /** Title for the default ordering/SortOrder if no orderings are provided and the description field is found */
-  'default-orderings.description': undefined, // 'Sort by Description'
+  'default-orderings.description': 'Açıklamaya Göre Sırala',
   /** Title for the default ordering/SortOrder if no orderings are provided and the header field is found */
-  'default-orderings.header': undefined, // 'Sort by Header'
+  'default-orderings.header': 'Üst Bilgiye Göre Sırala',
   /** Title for the default ordering/SortOrder if no orderings are provided and the heading field is found */
-  'default-orderings.heading': undefined, // 'Sort by Heading'
+  'default-orderings.heading': 'Başlığa Göre Sırala',
   /** Title for the default ordering/SortOrder if no orderings are provided and the label field is found */
-  'default-orderings.label': undefined, // 'Sort by Label'
+  'default-orderings.label': 'Etikete Göre Sırala',
   /** Title for the default ordering/SortOrder if no orderings are provided and the name field is found */
-  'default-orderings.name': undefined, // 'Sort by Name'
+  'default-orderings.name': 'İsme Göre Sırala',
   /** Title for the default ordering/SortOrder if no orderings are provided and the title field is found */
-  'default-orderings.title': undefined, // 'Sort by Title'
+  'default-orderings.title': 'Başlığa Göre Sırala',
 
   /** The value of the <code>_key</code> property must be a unique string. */
   'form.error.duplicate-keys-alert.details.additional-description':
