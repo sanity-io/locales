eab4feb40a7a46d121a8fd9a283f54e4933e43d4
AutoTranslator <hello@sanity.io>
2023-12-19 17:13:08 +0000

    fix(nn-NO): automated translation updates

--- locales/nn-NO/src/structure.ts
+++ locales/nn-NO/src/structure.ts
@@ -210,7 +210,7 @@ export default removeUndefinedLocaleResources({
   'confirm-dialog.confirm-button.fallback-text': 'Stadfest',
 
   /** For the default structure definition, the title for the "Content" pane */
-  'default-definition.content-title': undefined, // 'Content'
+  'default-definition.content-title': 'Innhald',
 
   /** The text shown if there was an error while getting the document's title via a preview value */
   'doc-title.error.text': 'Feil: {{errorMessage}}',
@@ -247,11 +247,11 @@ export default removeUndefinedLocaleResources({
 
   /** --- Menu items --- */
   /** The menu item group title to use for the Action menu items */
-  'menu-item-groups.actions-group': undefined, // 'Actions'
+  'menu-item-groups.actions-group': 'Handlingar',
   /** The menu item group title to use for the Layout menu items */
-  'menu-item-groups.layout-group': undefined, // 'Layout'
+  'menu-item-groups.layout-group': 'Oppsett',
   /** The menu item group title to use for the Sort menu items */
-  'menu-item-groups.sorting-group': undefined, // 'Sort'
+  'menu-item-groups.sorting-group': 'Sortering',
 
   /** The menu item title to use the compact view */
   'menu-items.layout.compact-view': 'Kompakt visning',
@@ -355,9 +355,9 @@ export default removeUndefinedLocaleResources({
   'panes.document-pane.document-unknown-type.without-schema.text':
     'Dette dokumentet eksisterer ikkje, og ingen skjematype var spesifisert for det.',
   /** Default message shown while resolving the structure definition for an asynchronous node */
-  'panes.resolving.default-message': undefined, // 'Loading…'
+  'panes.resolving.default-message': 'Lastar…',
   /** Message shown while resolving the structure definition for an asynchronous node and it is taking a while (more than 5s) */
-  'panes.resolving.slow-resolve-message': undefined, // 'Still loading…'
+  'panes.resolving.slow-resolve-message': 'Lastar framleis…',
   /** The text to display when type is missing */
   'panes.unknown-pane-type.missing-type.text':
     'Strukturelement manglar påkrevd <Code>type</Code>-eigenskap.',
--- locales/nn-NO/src/studio.ts
+++ locales/nn-NO/src/studio.ts
@@ -294,7 +294,7 @@ export default removeUndefinedLocaleResources({
   'changes.title': 'Gå gjennom endringar',
 
   /** The fallback title for an ordering menu item if no localized titles are provided. */
-  'default-menu-item.fallback-title': undefined, // 'Sort by {{title}}'
+  'default-menu-item.fallback-title': 'Sorter etter {{title}}',
 
   /** Title for the default ordering/SortOrder if no orderings are provided and the caption field is found */
   'default-orderings.caption': 'Sorter etter bildetekst',
@@ -402,7 +402,7 @@ export default removeUndefinedLocaleResources({
   'help-resources.title': 'Hjelp og ressursar',
 
   /** Text for button to cancel an ongoing upload */
-  'input.files.common.cancel-upload': undefined, // 'Cancel'
+  'input.files.common.cancel-upload': 'Avbryt',
   /** Text for file input button in upload placeholder */
   'input.files.common.upload-placeholder.file-input-button.text': 'Last opp',
   /** Uploading <FileName/> */
@@ -798,7 +798,7 @@ export default removeUndefinedLocaleResources({
   /** Label for action to create a new document from the reference input */
   'inputs.reference.action.create-new-document': 'Opprett ny',
   /** Label for action to create a new document from the reference input, when there are multiple templates or document types to choose from */
-  'inputs.reference.action.create-new-document-select': undefined, // 'Create new…'
+  'inputs.reference.action.create-new-document-select': 'Opprett ny…',
   /** Label for action to duplicate the current item to a new item (used within arrays) */
   'inputs.reference.action.duplicate': 'Dupliser',
   /** Label for action that opens the referenced document in a new tab */
@@ -923,9 +923,9 @@ export default removeUndefinedLocaleResources({
   'inputs.slug.error.missing-source':
     'Kjelda manglar. Sjekk kjelda på type {{schemaType}} i skjemaet',
   /** Placeholder for an empty tag input */
-  'inputs.tags.placeholder': undefined, // 'Enter tag and press ENTER…'
+  'inputs.tags.placeholder': 'Skriv inn merkelapp og trykk ENTER…',
   /** Placeholder for an empty tag input on touch devices */
-  'inputs.tags.placeholder_touch': undefined, // 'Enter tag…'
+  'inputs.tags.placeholder_touch': 'Skriv inn merkelapp…',
   /** Convert to <code>`{{targetType}}`</code> */
   'inputs.untyped-value.convert-button.text': 'Konverter til <code>{{targetType}}</code>',
   /** Encountered an object value without a <code>_type</code> property. */
@@ -1521,10 +1521,10 @@ export default removeUndefinedLocaleResources({
   'timeline.since-version-missing': 'Sidan: ukjend versjon',
 
   /** Label for button showing the free trial days left */
-  'user-menu.action.free-trial_one': undefined, // '{{count}} day left in trial'
-  'user-menu.action.free-trial_other': undefined, // '{{count}} days left in trial'
+  'user-menu.action.free-trial_one': '{{count}} dag att i prøveperioden',
+  'user-menu.action.free-trial_other': '{{count}} dagar att i prøveperioden',
   /** Label for the button showed after trial ended */
-  'user-menu.action.free-trial-finished': undefined, // 'Upgrade from free'
+  'user-menu.action.free-trial-finished': 'Oppgrader frå gratis',
   /** Label for action to invite members to the current sanity project */
   'user-menu.action.invite-members': 'Inviter medlemmer',
   /** Accessibility label for action to invite members to the current sanity project */
@@ -1534,9 +1534,11 @@ export default removeUndefinedLocaleResources({
   /** Accessibility label for the action to manage the current project */
   'user-menu.action.manage-project-aria-label': 'Administrer prosjekt',
   /** Tooltip helper text when portable text annotation is disabled for empty block*/
-  'user-menu.action.portable-text.annotation-disabled_empty-block': undefined, // 'Cannot apply {{name}} to empty block'
+  'user-menu.action.portable-text.annotation-disabled_empty-block':
+    'Kan ikkje bruke {{name}} på ein tom blokk',
   /** Tooltip helper text when portable text annotation is disabled for multiple blocks */
-  'user-menu.action.portable-text.annotation-disabled_multiple-blocks': undefined, // 'Cannot apply {{name}} to multiple blocks'
+  'user-menu.action.portable-text.annotation-disabled_multiple-blocks':
+    'Kan ikkje bruke {{name}} på fleire blokker',
   /** Label for action to sign out of the current sanity project */
   'user-menu.action.sign-out': 'Logg ut',
   /** Title for appearance section for the current studio (dark / light / system scheme) */
