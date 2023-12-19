d19daa01456f02805222369f2a0060c3cb35f78e
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-12-19 08:57:33 -0800

    feat!: require sanity v3.22.0 or higher

--- src/api/builders/buildPackageJson.ts
+++ src/api/builders/buildPackageJson.ts
@@ -5,7 +5,7 @@ import {readJsonFile} from '../../util/readJsonFile'
 import {getRootPath} from '../../util/getRootPath'
 import {packageJsonSchema} from '../../schemas'
 
-const MINIMUM_SANITY_VERSION = '3.21.3'
+const MINIMUM_SANITY_VERSION = '3.22.0'
 
 export async function buildPackageJson(locale: Locale): Promise<string> {
   const targetPath = joinPath(await getLocalePath(locale), 'package.json')
