6bd041795b45991d4268688d13e6afe583287961
AutoTranslator <hello@sanity.io>
2023-12-19 20:31:14 +0000

    fix(fr-FR): automated translation updates

--- locales/fr-FR/src/structure.ts
+++ locales/fr-FR/src/structure.ts
@@ -106,6 +106,8 @@ export default removeUndefinedLocaleResources({
 
   /** The action menu button aria-label */
   'buttons.action-menu-button.aria-label': 'Ouvrir les actions du document',
+  /** The action menu button tooltip */
+  'buttons.action-menu-button.tooltip': 'Actions du document',
   /** The aria-label for the split pane button on the document panel header */
   'buttons.split-pane-button.aria-label': 'Diviser le volet à droite',
   /** The tool tip for the split pane button on the document panel header */
@@ -268,10 +270,14 @@ export default removeUndefinedLocaleResources({
   /** The title of the no document type screen */
   'no-document-types-screen.title': 'Aucun type de document',
 
+  /** Text shown on back button visible on smaller breakpoints */
+  'pane-header.back-button.text': 'Retour',
   /** tooltip text (via `title` attribute) for the menu button */
   'pane-header.context-menu-button.tooltip': 'Afficher le menu',
   /** Appears in a document list pane header if there are more than one option for create. This is the label for that menu */
   'pane-header.create-menu.label': 'Créer',
+  /** Tooltip displayed on the create new button in document lists */
+  'pane-header.create-new-button.tooltip': 'Créer un nouveau document',
   /** The `aria-label` for the disabled button in the pane header if create permissions are granted */
   'pane-header.disabled-created-button.aria-label': 'Permissions insuffisantes',
 
