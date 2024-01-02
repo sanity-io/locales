847ad353f3e3a920d690fb7560da15ef4cdf3711
Rune Botten <rbotten@gmail.com>
2024-01-02 09:46:21 -0800

    feat(sv-SE): Add alexanderwarendh as maintainer. Closes #192

--- locales/registry.ts
+++ locales/registry.ts
@@ -118,7 +118,7 @@ const registry: LocaleRegistry = [
     id: 'sv-SE',
     name: 'Svenska',
     englishName: 'Swedish',
-    maintainers: ['wisechimp'],
+    maintainers: ['wisechimp', 'alexanderwarendh'],
     contributors: [],
   },
   {
