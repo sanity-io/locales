cd32dc878d8c778dc48fa0c17e929000d1e145ca
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-01-11 07:18:57 -0800

    fix(be-BY): align resource keys

--- locales/be-BY/src/structure.ts
+++ locales/be-BY/src/structure.ts
@@ -57,7 +57,7 @@ export default removeUndefinedLocaleResources({
   /** Tooltip when publish button is waiting for validation and async tasks to complete.*/
   'action.publish.waiting': 'Чаканне завяршэння задач перад публікацыяй',
   /** Message prompting the user to confirm that they want to restore to an earlier version*/
-  'action.restore.confirm.message': undefined, // 'Are you sure you want to restore this document?'
+  'action.restore.confirm.message': 'Вы ўпэўнены, што хочаце аднавіць гэты дакумент?', // 'Are you sure you want to restore this document?'
   /** Fallback tooltip for when user is looking at the initial version */
   'action.restore.disabled.cannot-restore-initial': 'Вы не можаце аднавіць пачатковую версію',
   /** Label for the "Restore" document action */
@@ -212,7 +212,7 @@ export default removeUndefinedLocaleResources({
   /** The text for when a form is hidden */
   'document-view.form-view.form-hidden': 'Гэтая форма схаваная',
   /** Fallback title shown when a form title is not provided */
-  'document-view.form-view.form-title-fallback': undefined, // 'Untitled'
+  'document-view.form-view.form-title-fallback': 'Без назвы',
   /** The text for when the form view is loading a document */
   'document-view.form-view.loading': 'Загрузка дакумента…',
   /** The description of the sync lock toast on the form view */
