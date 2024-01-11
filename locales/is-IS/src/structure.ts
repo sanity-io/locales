00d2568c9a7e09465c679e7badbf44849c22a7a3
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-01-11 07:52:03 -0800

    fix(is-IS): align resource keys

--- locales/is-IS/src/structure.ts
+++ locales/is-IS/src/structure.ts
@@ -57,7 +57,7 @@ export default removeUndefinedLocaleResources({
   /** Tooltip when publish button is waiting for validation and async tasks to complete.*/
   'action.publish.waiting': 'Bíð eftir að verkefni klárist áður en birt er',
   /** Message prompting the user to confirm that they want to restore to an earlier version*/
-  'action.restore.confirm.message': undefined, // 'Are you sure you want to restore this document?'
+  'action.restore.confirm.message': 'Ertu viss um að þú viljir endurheimta þetta skjal?',
   /** Fallback tooltip for when user is looking at the initial version */
   'action.restore.disabled.cannot-restore-initial': 'Þú getur ekki endurheimt upphaflegu útgáfuna',
   /** Label for the "Restore" document action */
@@ -229,7 +229,7 @@ export default removeUndefinedLocaleResources({
   /** The text for when a form is hidden */
   'document-view.form-view.form-hidden': 'Þessi eyðublað er falið',
   /** Fallback title shown when a form title is not provided */
-  'document-view.form-view.form-title-fallback': undefined, // 'Untitled'
+  'document-view.form-view.form-title-fallback': 'Titilllaus',
   /** The text for when the form view is loading a document */
   'document-view.form-view.loading': 'Hleð skjali…',
   /** The description of the sync lock toast on the form view */
