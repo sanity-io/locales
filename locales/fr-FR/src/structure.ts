d4d1f83754b6e67839a0a4379af9219f8f751646
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-01-11 07:39:02 -0800

    fix(fr-FR): align resource keys

--- locales/fr-FR/src/structure.ts
+++ locales/fr-FR/src/structure.ts
@@ -58,7 +58,7 @@ export default removeUndefinedLocaleResources({
   /** Tooltip when publish button is waiting for validation and async tasks to complete.*/
   'action.publish.waiting': 'En attente de la fin des tâches avant publication',
   /** Message prompting the user to confirm that they want to restore to an earlier version*/
-  'action.restore.confirm.message': undefined, // 'Are you sure you want to restore this document?'
+  'action.restore.confirm.message': 'Êtes-vous sûr de vouloir restaurer ce document ?',
   /** Fallback tooltip for when user is looking at the initial version */
   'action.restore.disabled.cannot-restore-initial':
     'Vous ne pouvez pas restaurer la version initiale',
@@ -234,7 +234,7 @@ export default removeUndefinedLocaleResources({
   /** The text for when a form is hidden */
   'document-view.form-view.form-hidden': 'Ce formulaire est caché',
   /** Fallback title shown when a form title is not provided */
-  'document-view.form-view.form-title-fallback': undefined, // 'Untitled'
+  'document-view.form-view.form-title-fallback': 'Sans titre',
   /** The text for when the form view is loading a document */
   'document-view.form-view.loading': 'Chargement du document…',
   /** The description of the sync lock toast on the form view */
