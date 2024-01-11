8be8633fd586f59500db48f36b4f4365772b71d2
AutoTranslator <hello@sanity.io>
2024-01-11 23:31:18 +0000

    fix(fr-FR): automated translation updates

--- locales/fr-FR/src/studio.ts
+++ locales/fr-FR/src/studio.ts
@@ -406,10 +406,10 @@ export default removeUndefinedLocaleResources({
   'form.validation.summary.warnings-count_other': '{{count}} avertissements',
 
   /** Tooltip for free trial navbar button indicating remaining days */
-  'free-trial.tooltip.days-count_one': undefined, // '{{count}} day left in trial'
-  'free-trial.tooltip.days-count_other': undefined, // '{{count}} days left in trial'
+  'free-trial.tooltip.days-count_one': "{{count}} jour restant dans l'essai",
+  'free-trial.tooltip.days-count_other': "{{count}} jours restants dans l'essai",
   /** Tooltip for free trial navbar button, once trial has ended */
-  'free-trial.tooltip.trial-finished': undefined, // 'Upgrade your project'
+  'free-trial.tooltip.trial-finished': 'Mettez à niveau votre projet',
 
   /**
    * Label for "contact sales" call to action
