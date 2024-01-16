d5c6c2bdfaf1532fa97ef659501c0baeeca54bab
Rune Botten <rbotten@gmail.com>
2024-01-16 13:01:17 -0800

    Update locales/tr-TR/src/studio.ts

--- locales/tr-TR/src/studio.ts
+++ locales/tr-TR/src/studio.ts
@@ -402,7 +402,7 @@ export default removeUndefinedLocaleResources({
   'form.validation.summary.warnings-count_other': '{{count}} uyarı',
 
   /** Tooltip for free trial navbar button indicating remaining days */
-  'free-trial.tooltip.days-count_one': '{{count}} gün kaldı deneme süresinde',
+  'free-trial.tooltip.days-count_one': 'deneme süresinde {{count}} gün kaldı',
   'free-trial.tooltip.days-count_other': '{{count}} gün kaldı deneme süresinde',
   /** Tooltip for free trial navbar button, once trial has ended */
   'free-trial.tooltip.trial-finished': 'Projeyi yükseltin',
