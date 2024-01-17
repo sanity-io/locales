1783a4b4bae7055ef169d966d65d3cbf9ad841e6
AutoTranslator <hello@sanity.io>
2024-01-17 22:50:38 +0000

    fix(nn-NO): automated translation updates

--- locales/nn-NO/src/structure.ts
+++ locales/nn-NO/src/structure.ts
@@ -378,9 +378,9 @@ export default removeUndefinedLocaleResources({
   'production-preview.menu-item.title': 'Opne førehandsvising',
 
   /** Label for button when status is saved */
-  'status-bar.document-status-pulse.status.saved.text': undefined, // 'Saved'
+  'status-bar.document-status-pulse.status.saved.text': 'Lagra',
   /** Label for button when status is syncing */
-  'status-bar.document-status-pulse.status.syncing.text': undefined, // 'Saving...'
+  'status-bar.document-status-pulse.status.syncing.text': 'Lagrar...',
   /** Accessibility label indicating when the document was last published, in relative time, eg "3 weeks ago" */
   'status-bar.publish-status-button.last-published-time.aria-label':
     'Sist publisert {{relativeTime}}',
