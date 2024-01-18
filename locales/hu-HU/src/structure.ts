ed6f38f546d74411c3dcd2b95ba9b217b8135f73
AutoTranslator <hello@sanity.io>
2024-01-18 21:19:40 +0000

    fix(hu-HU): automated translation updates

--- locales/hu-HU/src/structure.ts
+++ locales/hu-HU/src/structure.ts
@@ -378,9 +378,9 @@ export default removeUndefinedLocaleResources({
   'production-preview.menu-item.title': 'Előnézet megnyitása',
 
   /** Label for button when status is saved */
-  'status-bar.document-status-pulse.status.saved.text': undefined, // 'Saved'
+  'status-bar.document-status-pulse.status.saved.text': 'Mentve',
   /** Label for button when status is syncing */
-  'status-bar.document-status-pulse.status.syncing.text': undefined, // 'Saving...'
+  'status-bar.document-status-pulse.status.syncing.text': 'Mentés...',
   /** Accessibility label indicating when the document was last published, in relative time, eg "3 weeks ago" */
   'status-bar.publish-status-button.last-published-time.aria-label':
     'Utoljára közzétéve {{relativeTime}}',
