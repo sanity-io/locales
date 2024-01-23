a29ec4eba021e296e2e6c0d4217f2be4dcb54358
AutoTranslator <hello@sanity.io>
2024-01-23 18:40:16 +0000

    fix(is-IS): automated translation updates

--- locales/is-IS/src/structure.ts
+++ locales/is-IS/src/structure.ts
@@ -78,6 +78,8 @@ export default removeUndefinedLocaleResources({
   'banners.deleted-document-banner.restore-button.text': 'Endurheimta nýjustu útgáfuna',
   /** The text content for the deleted document banner */
   'banners.deleted-document-banner.text': 'Þetta skjal hefur verið eytt.',
+  /** The text content for the deprecated document type banner */
+  'banners.deprecated-document-type-banner.text': 'Þessi skjaltýpa er úrelt.',
   /** The text for the permission check banner if the user only has one role, and it does not allow updating this document */
   'banners.permission-check-banner.missing-permission_create_one':
     'Hlutverk þitt <Roles/> hefur ekki heimildir til að búa til þetta skjal.',
@@ -100,6 +102,11 @@ export default removeUndefinedLocaleResources({
   /** The text for the reference change banner if the reason is that the reference has been deleted */
   'banners.reference-changed-banner.reason-removed.text':
     'Þessi tilvísun hefur verið fjarlægð síðan þú opnaðir hana.',
+
+  /** Browser/tab title when creating a new document of a given type */
+  'browser-document-title.new-document': 'Nýtt {{schemaType}}',
+  /** Browser/tab title when editing a document where the title cannot be resolved from preview configuration */
+  'browser-document-title.untitled-document': 'Ótitlað',
 
   /** The action menu button aria-label */
   'buttons.action-menu-button.aria-label': 'Opna skjalaðgerðir',
@@ -213,6 +220,8 @@ export default removeUndefinedLocaleResources({
   /** The text shown if a document's title via a preview value cannot be determined due to an unknown schema type */
   'doc-title.unknown-schema-type.text': 'Óþekkt skema tegund: {{schemaType}}',
 
+  /** Tooltip text shown for the close button of the document inspector */
+  'document-inspector.close-button.tooltip': 'Loka',
   /** The title shown in the dialog header, when inspecting a valid document */
   'document-inspector.dialog.title': 'Skoða <DocumentTitle/>',
   /** The title shown in the dialog header, when the document being inspected is not created yet/has no value */
