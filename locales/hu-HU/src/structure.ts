6118b74dde7492cb5c91dc5b52cccee17a7495a5
AutoTranslator <hello@sanity.io>
2024-02-07 21:48:10 +0000

    fix(hu-HU): automated translation updates

--- locales/hu-HU/src/structure.ts
+++ locales/hu-HU/src/structure.ts
@@ -353,7 +353,7 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success_unpublish':
     'A dokumentum közzétételét megszüntették. A legutóbbi közzétett verzióból vázlat készült.',
   /** The document title shown when document title is "undefined" in operation message */
-  'panes.document-operation-results.operation-undefined-title': undefined, // 'Untitled'
+  'panes.document-operation-results.operation-undefined-title': 'Cím nélkül',
   /** The title of the reconnecting toast */
   'panes.document-pane-provider.reconnecting.title': 'A kapcsolat megszakadt. Újracsatlakozás…',
   /** The loading message for the document not found pane */
