bdbd05290735aabfc30f997d5cb6188a92668178
AutoTranslator <hello@sanity.io>
2024-02-12 22:11:23 +0000

    fix(sv-SE): automated translation updates

--- locales/sv-SE/src/structure.ts
+++ locales/sv-SE/src/structure.ts
@@ -351,7 +351,7 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success_unpublish':
     'Dokumentet avpublicerades. Ett utkast har skapats från den senaste publicerade versionen.',
   /** The document title shown when document title is "undefined" in operation message */
-  'panes.document-operation-results.operation-undefined-title': undefined, // 'Untitled'
+  'panes.document-operation-results.operation-undefined-title': 'Namnlös',
   /** The title of the reconnecting toast */
   'panes.document-pane-provider.reconnecting.title': 'Anslutningen förlorad. Återansluter…',
   /** The loading message for the document not found pane */
