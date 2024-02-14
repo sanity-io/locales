e5d69ba0d1bebbd8e65e66ce15e7e61242507421
AutoTranslator <hello@sanity.io>
2024-02-14 21:37:12 +0000

    fix(es-ES): automated translation updates

--- locales/es-ES/src/structure.ts
+++ locales/es-ES/src/structure.ts
@@ -353,7 +353,7 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success_unpublish':
     'El documento fue despublicado. Se ha creado un borrador a partir de la última versión publicada.',
   /** The document title shown when document title is "undefined" in operation message */
-  'panes.document-operation-results.operation-undefined-title': undefined, // 'Untitled'
+  'panes.document-operation-results.operation-undefined-title': 'Sin título',
   /** The title of the reconnecting toast */
   'panes.document-pane-provider.reconnecting.title': 'Conexión perdida. Reconectando…',
   /** The loading message for the document not found pane */
--- locales/es-ES/src/studio.ts
+++ locales/es-ES/src/studio.ts
@@ -665,7 +665,7 @@ export default removeUndefinedLocaleResources({
   /** Collapse the editor to save screen space  */
   'inputs.portable-text.action.collapse-editor': 'Contraer editor',
   /** Aria label for action to edit an existing annotation */
-  'inputs.portable-text.action.edit-annotation-aria-label': undefined, // 'Edit annotation'
+  'inputs.portable-text.action.edit-annotation-aria-label': 'Editar anotación',
   /** Expand the editor to give more editing space */
   'inputs.portable-text.action.expand-editor': 'Expandir editor',
   /** Label label for action to insert a block of a given type (`{{typeName}}`) */
@@ -677,7 +677,7 @@ export default removeUndefinedLocaleResources({
   /** Accessibility label for action to insert an inline object of a given type (`{{typeName}}`) */
   'inputs.portable-text.action.insert-inline-object-aria-label': 'Insertar {{typeName}} (en línea)',
   /** Aria label for action to remove an annotation */
-  'inputs.portable-text.action.remove-annotation-aria-label': undefined, // 'Remove annotation'
+  'inputs.portable-text.action.remove-annotation-aria-label': 'Eliminar anotación',
   /** Label for activate on focus with context of click and not focused */
   'inputs.portable-text.activate-on-focus-message_click': 'Haz clic para activar',
   /** Label for activate on focus with context of click and focused */
@@ -686,13 +686,13 @@ export default removeUndefinedLocaleResources({
   /** Label for activate on focus with context of tap and not focused */
   'inputs.portable-text.activate-on-focus-message_tap': 'Toca para activar',
   /**Aria label for the annotation object */
-  'inputs.portable-text.annotation-aria-label': undefined, // 'Annotation object'
+  'inputs.portable-text.annotation-aria-label': 'Objeto de anotación',
   /** Title for dialog that allows editing an annotation */
   'inputs.portable-text.annotation-editor.title': 'Editar {{schemaType}}',
   /** Title of the default "link" annotation */
   'inputs.portable-text.annotation.link': 'Enlace',
   /**Aria label for the block object */
-  'inputs.portable-text.block.aria-label': undefined, // 'Block object'
+  'inputs.portable-text.block.aria-label': 'Objeto de bloque',
   /** Label for action to edit a block item, in the case where it is editable */
   'inputs.portable-text.block.edit': 'Editar',
   /** Accessibility label for the button that opens the actions menu on blocks */
@@ -716,15 +716,15 @@ export default removeUndefinedLocaleResources({
   /** Placeholder text for when the editor is empty */
   'inputs.portable-text.empty-placeholder': 'Vacío',
   /**Aria label for the block object */
-  'inputs.portable-text.inline-block.aria-label': undefined, // 'Inline object'
+  'inputs.portable-text.inline-block.aria-label': 'Objeto en línea',
   /** Label for action to edit an inline object item */
   'inputs.portable-text.inline-object.edit': 'Editar objeto',
   /** Aria label for icon for action to edit an inline object item */
-  'inputs.portable-text.inline-object.edit-aria-label': undefined, // 'Edit object'
+  'inputs.portable-text.inline-object.edit-aria-label': 'Editar objeto',
   /** Label for action to remove an inline object item */
   'inputs.portable-text.inline-object.remove': 'Eliminar objeto',
   /** Aria label for icon for action to remove an inline object item */
-  'inputs.portable-text.inline-object.remove-aria-label': undefined, // 'Remove object'
+  'inputs.portable-text.inline-object.remove-aria-label': 'Eliminar objeto',
   /** Disclaimer text shown on invalid Portable Text value, when an action is available to unblock the user, but it is not guaranteed to be safe */
   'inputs.portable-text.invalid-value.action-disclaimer':
     'NOTA: Generalmente es seguro realizar la acción anterior, pero si tienes dudas, ponte en contacto con los responsables de configurar tu estudio.',
