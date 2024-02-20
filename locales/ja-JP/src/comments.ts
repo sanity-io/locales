d439730d01d7419e149a6158ca22e5395afc0187
AutoTranslator <hello@sanity.io>
2024-02-20 19:24:24 +0000

    fix(ja-JP): automated translation updates

--- locales/ja-JP/src/comments.ts
+++ locales/ja-JP/src/comments.ts
@@ -91,7 +91,11 @@ export default removeUndefinedLocaleResources({
   'list-item.edit-comment-upsell': 'コメントを編集するためにアップグレード',
   /** Aria label for the button that takes you to the field, which wraps a thread/comment */
   'list-item.go-to-field-button.aria-label': 'フィールドに移動',
-  /** The text showing the comment context */
+  /**
+   * The text shown below the author and timestamp of a comment including a link back to the context in which the comment was made.
+   * Consists of a document title wrapped in a link, and a word or phrase to indicate that the link refers to a location:
+   * eg "on Home", "on Coffee Machine | Products", "on Pricing – Sanity"
+   */
   'list-item.layout-context': '<IntentLink>{{title}}</IntentLink>の上に',
   /** The marker to indicate that a comment has been edited in brackets */
   'list-item.layout-edited': '編集済み',
