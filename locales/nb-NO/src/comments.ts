f21c628c4860d6889c8c2957520036ffa5daa0de
AutoTranslator <hello@sanity.io>
2024-02-22 18:01:16 +0000

    fix(nb-NO): automated translation updates

--- locales/nb-NO/src/comments.ts
+++ locales/nb-NO/src/comments.ts
@@ -94,6 +94,12 @@ export default removeUndefinedLocaleResources({
   'list-item.edit-comment-upsell': 'Oppgrader for å redigere kommentar',
   /** Aria label for the button that takes you to the field, which wraps a thread/comment */
   'list-item.go-to-field-button.aria-label': 'Gå til felt',
+  /**
+   * The text shown below the author and timestamp of a comment including a link back to the context in which the comment was made.
+   * Consists of a document title wrapped in a link, and a word or phrase to indicate that the link refers to a location:
+   * eg "on Home", "on Coffee Machine | Products", "on Pricing – Sanity"
+   */
+  'list-item.layout-context': 'på <IntentLink>{{title}}</IntentLink>',
   /** The marker to indicate that a comment has been edited in brackets */
   'list-item.layout-edited': 'redigert',
   /** The error text when sending a comment has failed */
