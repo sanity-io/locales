f05af5c6f846a9ad785a8e7027f70f397bfa9ba5
AutoTranslator <hello@sanity.io>
2024-03-02 00:27:40 +0000

    fix(ka-GE): automated translation updates

--- locales/ka-GE/src/validation.ts
+++ locales/ka-GE/src/validation.ts
@@ -2,98 +2,98 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Array must have exactly "$wantedLength" items, but has more/less */
-  'array.exact-length': undefined, // 'Must have exactly {{wantedLength}} items'
+  'array.exact-length': 'უნდა იყოს ზუსტად {{wantedLength}} ელემენტი',
   /** Portable Text array must have exactly "$wantedLength" blocks, but has more/less */
-  'array.exact-length_blocks': undefined, // 'Must have exactly {{wantedLength}} blocks'
+  'array.exact-length_blocks': 'უნდა იყოს ზუსტად {{wantedLength}} ბლოკი',
   /** Array item is a duplicate, but array wants only unique items */
-  'array.item-duplicate': undefined, // 'Can\'t be a duplicate'
+  'array.item-duplicate': 'არ უნდა იყოს დუბლიკატი',
   /** Array has more than the maximum of "$maxLength" items */
-  'array.maximum-length': undefined, // 'Must have at most {{maxLength}} items'
+  'array.maximum-length': 'უნდა იყოს მაქსიმუმ {{maxLength}} ელემენტი',
   /** Portable Text array has more than the maximum of "$maxLength" items */
-  'array.maximum-length_blocks': undefined, // 'Must have at most {{maxLength}} blocks'
+  'array.maximum-length_blocks': 'უნდა იყოს მაქსიმუმ {{maxLength}} ბლოკი',
   /** Array has less than the minimum of "$minLength" items */
-  'array.minimum-length': undefined, // 'Must have at least {{minLength}} items'
+  'array.minimum-length': 'უნდა იყოს მინიმუმ {{minLength}} ელემენტი',
   /** Portable Text array has less than the minimum of "$minLength" blocks */
-  'array.minimum-length_blocks': undefined, // 'Must have at least {{minLength}} blocks'
+  'array.minimum-length_blocks': 'უნდა იყოს მინიმუმ {{minLength}} ბლოკი',
 
   /** Date is not valid or not in the correct format (ISO-8601) */
-  'date.invalid-format': undefined, // 'Must be a valid ISO-8601 formatted date string'
+  'date.invalid-format': 'უნდა იყოს ვალიდური ISO-8601 ფორმატის თარიღის სტრიქონი',
   /** Date is later than the given maximum date "$maxDate" */
-  'date.maximum': undefined, // 'Must be at or before {{maxDate}}'
+  'date.maximum': 'უნდა იყოს ან მანამდე {{maxDate}}',
   /** Date is earlier than the given minimum date "$minDate" */
-  'date.minimum': undefined, // 'Must be at or after {{minDate}}'
+  'date.minimum': 'უნდა იყოს ან მერე {{minDate}}',
 
   /** A value of incorrect type is found, eg found `number` instead of `string` */
-  'generic.incorrect-type': undefined, // 'Expected type "{{expectedType}}", got "{{actualType}}"'
+  'generic.incorrect-type': 'მოსალოდნელი ტიპია "{{expectedType}}", მიღებულია "{{actualType}}"',
   /** Value is not one of the values specifically allowed */
-  'generic.not-allowed': undefined, // 'Value did not match any allowed values'
+  'generic.not-allowed': 'მნიშვნელობა არ ემთხვევა ნებადართულ მნიშვნელობებს',
   /** Value "$givenValue" is not one of the values specifically allowed */
-  'generic.not-allowed_hint': undefined, // 'Value "{{hint}}" did not match any allowed values'
+  'generic.not-allowed_hint': 'მნიშვნელობა "{{hint}}" არ ემთხვევა ნებადართულ მნიშვნელობებს',
   /** A value is expected, but none is provided */
-  'generic.required': undefined, // 'Required'
+  'generic.required': 'საჭიროა',
 
   /** Number is less than the given minimum threshold value "$threshold" */
-  'number.greater-than': undefined, // 'Must be greater than {{threshold}}'
+  'number.greater-than': 'უნდა იყოს მეტი {{threshold}}-ზე',
   /** Number is greater than the given maximum threshold value "$threshold" */
-  'number.less-than': undefined, // 'Must be less than {{threshold}}'
+  'number.less-than': 'უნდა იყოს ნაკლები {{threshold}}-ზე',
   /** Number is higher than the given maximum value "$maxNumber" */
-  'number.maximum': undefined, // 'Must be lower than or equal to {{maxNumber}}'
+  'number.maximum': 'უნდა იყოს ნაკლები ან ტოლი {{maxNumber}}-ს',
   /** Number has more precision (decimals) than the allowed "$limit" */
-  'number.maximum-precision': undefined, // 'Max precision is {{limit}}'
+  'number.maximum-precision': 'მაქსიმუმ სიზუსტეა {{limit}}',
   /** Number is lower than the given minimum value "$minNumber" */
-  'number.minimum': undefined, // 'Must be greater than or equal to {{minNumber}}'
+  'number.minimum': 'უნდა იყოს მეტი ან ტოლი {{minNumber}}-ს',
   /** Number is not an integer ("whole number") */
-  'number.non-integer': undefined, // 'Must be an integer'
+  'number.non-integer': 'უნდა იყოს მთელი რიცხვი',
 
   /** Object is missing a reference to an asset document in its `asset` field */
-  'object.asset-required': undefined, // 'Asset is required'
+  'object.asset-required': 'აქტივი საჭიროა',
   /** Object is missing a reference to a file asset document in its `asset` field */
-  'object.asset-required_file': undefined, // 'File is required'
+  'object.asset-required_file': 'ფაილი საჭიროა',
   /** Object is missing a reference to an image asset document in its `asset` field */
-  'object.asset-required_image': undefined, // 'Image is required'
+  'object.asset-required_image': 'სურათი საჭიროა',
   /** Object is not a reference to a document (eg `{_ref: 'documentId'}`) */
-  'object.not-reference': undefined, // 'Must be a reference to a document'
+  'object.not-reference': 'უნდა იყოს დოკუმენტის მითითება',
   /** Object references a document which is not published */
-  'object.reference-not-published': undefined, // 'Referenced document must be published'
+  'object.reference-not-published': 'მითითებული დოკუმენტი უნდა იყოს გამოქვეყნებული',
 
   /** Accessibility label for closing the validation panel */
-  'panel.close-button-aria-label': undefined, // 'Close validation'
+  'panel.close-button-aria-label': 'ვალიდაციის პანელის დახურვა',
   /** Message shown when the validation panel is opened but there are no errors/warnings */
-  'panel.no-errors-message': undefined, // 'No validation errors'
+  'panel.no-errors-message': 'ვალიდაციის შეცდომები არ არის',
   /** Title for the actual "Validation" panel/feature */
-  'panel.title': undefined, // 'Validation'
+  'panel.title': 'ვალიდაცია',
 
   /** Slug is an object, but is missing a `current` string property */
-  'slug.missing-current': undefined, // 'Slug must have a value'
+  'slug.missing-current': 'Slug-ის უნდა ჰქონდეს მნიშვნელობა',
   /** Slug is not an object (eg `{current: 'some-slug'}`) */
-  'slug.not-object': undefined, // 'Slug must be an object'
+  'slug.not-object': 'Slug-ის უნდა იყოს ობიექტი',
   /** Slug is already in use somewhere else, but needs to be unique */
-  'slug.not-unique': undefined, // 'Slug is already in use'
+  'slug.not-unique': 'Slug უკვე გამოიყენება',
 
   /** String is not a valid email address */
-  'string.email': undefined, // 'Must be a valid email address'
+  'string.email': 'უნდა იყოს ვალიდური ელ-ფოსტის მისამართი',
   /** String has a different character length than the exact number "$wantedLength" */
-  'string.exact-length': undefined, // 'Must be exactly {{wantedLength}} characters long'
+  'string.exact-length': 'უნდა იყოს ზუსტად {{wantedLength}} სიმბოლოს სიგრძე',
   /** String contains characters that are not in lowercase  */
-  'string.lowercase': undefined, // 'Must be all lowercase characters'
+  'string.lowercase': 'უნდა იყოს ყველა პატარა ასო',
   /** String is longer than the limit of "$maxLength" characters */
-  'string.maximum-length': undefined, // 'Must be at most {{maxLength}} characters long'
+  'string.maximum-length': 'უნდა იყოს მაქსიმუმ {{maxLength}} სიმბოლოს სიგრძე',
   /** String is shorter than the limit of "$minLength" characters */
-  'string.minimum-length': undefined, // 'Must be at least {{minLength}} characters long'
+  'string.minimum-length': 'უნდა იყოს მინიმუმ {{minLength}} სიმბოლოს სიგრძე',
   /** String does not match the given regular expression, but should */
-  'string.regex-does-not-match': undefined, // 'Does not match "{{name}}"-pattern'
+  'string.regex-does-not-match': 'არ ემთხვევა "{{name}}"-შაბლონს',
   /** String matches the given regular expression, but should not */
-  'string.regex-match': undefined, // 'Should not match "{{name}}"-pattern'
+  'string.regex-match': 'არ უნდა ემთხვევა "{{name}}"-შაბლონს',
   /** String contains characters that are not in uppercase */
-  'string.uppercase': undefined, // 'Must be all uppercase characters'
+  'string.uppercase': 'უნდა იყოს ყველა დიდი ასო',
   /** String contains a protocol/scheme that is not allowed, eg (`ftp`, `mailto`…) */
-  'string.url.disallowed-scheme': undefined, // 'Does not match allowed protocols/schemes'
+  'string.url.disallowed-scheme': 'არ ემთხვევა დასაშვებ პროტოკოლებს/სქემებს',
   /** String contains a URL with a username or password specified before the host */
-  'string.url.includes-credentials': undefined, // 'Username/password not allowed'
+  'string.url.includes-credentials': 'მომხმარებლის სახელი/პაროლი არ არის დასაშვები',
   /** String is not a valid URL */
-  'string.url.invalid': undefined, // 'Not a valid URL'
+  'string.url.invalid': 'არ არის ვალიდური URL',
   /** String is not an absolute URL (eg it is missing a protocol/host) */
-  'string.url.not-absolute': undefined, // 'Relative URLs are not allowed'
+  'string.url.not-absolute': 'მხრივ მისამართები არ არის დასაშვები',
   /** String is not a relative URL (eg it contains a protocol/host) */
-  'string.url.not-relative': undefined, // 'Only relative URLs are allowed'
+  'string.url.not-relative': 'მხოლოდ მხრივ მისამართები არის დასაშვები',
 })
