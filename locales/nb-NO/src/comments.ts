2a00ed5a026a8b8028ddca5e9f5d0169eb3b3d1f
AutoTranslator <hello@sanity.io>
2024-04-03 21:32:20 +0000

    fix(nb-NO): automated translation updates

--- locales/nb-NO/src/comments.ts
+++ locales/nb-NO/src/comments.ts
@@ -77,9 +77,9 @@ export default removeUndefinedLocaleResources({
   'field-button.title': 'Legg til kommentar',
 
   /* The text shown in the inline comment button when the button is disabled due to overlap */
-  'inline-add-comment-button.disabled-overlap-title': undefined, // 'Comments cannot overlap'
+  'inline-add-comment-button.disabled-overlap-title': 'Kommentarer kan ikke overlappe',
   /** The text shown in the inline comment button */
-  'inline-add-comment-button.title': undefined, // 'Add comment'
+  'inline-add-comment-button.title': 'Legg til kommentar',
 
   /** Aria label for the breadcrumb button showing the field path. `{{field}}` is the last (most specific) field. */
   'list-item.breadcrumb-button-go-to-field-aria-label': 'Gå til {{field}}-feltet',
@@ -114,7 +114,7 @@ export default removeUndefinedLocaleResources({
   /** The text for retrying posting a comment */
   'list-item.layout-retry': 'Prøv igjen',
   /** The text shown when the value a comment references has been deleted */
-  'list-item.missing-referenced-value-tooltip-content': undefined, // 'The commented text has been deleted'
+  'list-item.missing-referenced-value-tooltip-content': 'Den kommenterte teksten har blitt slettet',
   /** The aria label for the comments menu button to open the actions menu */
   'list-item.open-menu-aria-label': 'Åpne kommentarmeny',
   /** The button text to re-open a resolved comment  */
