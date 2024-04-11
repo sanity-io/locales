57ce0c2231e826db84c2c8e160d34a2d353f93a0
AutoTranslator <hello@sanity.io>
2024-04-11 04:50:05 +0000

    fix(tr-TR): automated translation updates

--- locales/tr-TR/src/comments.ts
+++ locales/tr-TR/src/comments.ts
@@ -77,9 +77,9 @@ export default removeUndefinedLocaleResources({
   'field-button.title': 'Yorum ekle',
 
   /* The text shown in the inline comment button when the button is disabled due to overlap */
-  'inline-add-comment-button.disabled-overlap-title': undefined, // 'Comments cannot overlap'
+  'inline-add-comment-button.disabled-overlap-title': 'Yorumlar üst üste binemez',
   /** The text shown in the inline comment button */
-  'inline-add-comment-button.title': undefined, // 'Add comment'
+  'inline-add-comment-button.title': 'Yorum ekle',
 
   /** Aria label for the breadcrumb button showing the field path. `{{field}}` is the last (most specific) field. */
   'list-item.breadcrumb-button-go-to-field-aria-label': '{{field}} alanına git',
@@ -114,7 +114,7 @@ export default removeUndefinedLocaleResources({
   /** The text for retrying posting a comment */
   'list-item.layout-retry': 'Yeniden dene',
   /** The text shown when the value a comment references has been deleted */
-  'list-item.missing-referenced-value-tooltip-content': undefined, // 'The commented text has been deleted'
+  'list-item.missing-referenced-value-tooltip-content': 'Yorum yapılan metin silinmiştir',
   /** The aria label for the comments menu button to open the actions menu */
   'list-item.open-menu-aria-label': 'Yorum işlemleri menüsünü aç',
   /** The button text to re-open a resolved comment  */
