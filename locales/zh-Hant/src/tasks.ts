d89a761fbddf3ec827754f3289caa10a841e5d7e
AutoTranslator <hello@sanity.io>
2024-05-08 06:46:54 +0000

    fix(zh-Hant): automated translation updates

--- locales/zh-Hant/src/tasks.ts
+++ locales/zh-Hant/src/tasks.ts
@@ -2,95 +2,95 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** The label for the create task action */
-  'actions.create.text': undefined, // 'Create new task'
+  'actions.create.text': '創建新任務',
   /** The label for the open tasks panel action */
-  'actions.open.text': undefined, // 'Tasks'
+  'actions.open.text': '任務',
 
   /** The label for the button to create a new task */
-  'buttons.create.text': undefined, // 'Create Task'
+  'buttons.create.text': '創建任務',
   /** The label for the button to discard changes */
-  'buttons.discard.text': undefined, // 'Discard'
+  'buttons.discard.text': '放棄',
   /** The label for the button to open the draft */
-  'buttons.draft.text': undefined, // 'Draft'
+  'buttons.draft.text': '草稿',
   /** The label for the button to create a new task */
-  'buttons.new.text': undefined, // 'New task'
+  'buttons.new.text': '新任務',
   /** The label for the button that will navigate to the next task */
-  'buttons.next.tooltip': undefined, // 'Go to next task'
+  'buttons.next.tooltip': '前往下一個任務',
   /** The label for the button that will previous to the next task */
-  'buttons.previous.tooltip': undefined, // 'Go to previous task'
+  'buttons.previous.tooltip': '前往上一個任務',
 
   /** Text for the remove task dialog asking for confirmation of deletion */
-  'dialog.remove-task.body': undefined, // 'Are you sure you want to delete this task?'
+  'dialog.remove-task.body': '您確定要刪除這個任務嗎？',
   /** Text for the remove task dialog clarifying that deletion is permanent */
-  'dialog.remove-task.body2': undefined, // 'Once deleted, it cannot be restored.'
+  'dialog.remove-task.body2': '一旦刪除，將無法恢復。',
   /** The label for the cancel button on the remove task dialog */
-  'dialog.remove-task.buttons.cancel.text': undefined, // 'Cancel'
+  'dialog.remove-task.buttons.cancel.text': '取消',
   /** The label for the confirmation button on the remove task dialog */
-  'dialog.remove-task.buttons.confirm.text': undefined, // 'Remove'
+  'dialog.remove-task.buttons.confirm.text': '移除',
   /** The title for the remove task dialog */
-  'dialog.remove-task.title': undefined, // 'Remove task'
+  'dialog.remove-task.title': '移除任務',
 
   /** The text used as a placeholder for the footer action in a document with multiple tasks */
-  'document.footer.open-tasks.placeholder_other': undefined, // 'Open tasks'
+  'document.footer.open-tasks.placeholder_other': '開啟任務',
   /** The label used in the button in the footer action in a document with multiple tasks */
-  'document.footer.open-tasks.text_other': undefined, // '{{count}} open tasks'
+  'document.footer.open-tasks.text_other': '{{count}} 個開放任務',
 
-  'empty-state.list.assigned.heading': undefined, // 'You haven\'t been assigned any tasks'
-  'empty-state.list.assigned.text': undefined, // 'Once you\'re assigned tasks they\'ll show up here'
-  'empty-state.list.create-new': undefined, // 'Create new task'
-  'empty-state.list.document.heading': undefined, // 'This document doesn\'t have any tasks yet'
-  'empty-state.list.document.text': undefined, // 'Once a document has connected tasks, they will be shown here.'
-  'empty-state.list.subscribed.heading': undefined, // 'You haven\'t subscribed to any tasks'
-  'empty-state.list.subscribed.text': undefined, // 'When you create, modify, or comment on a task you will be subscribed automatically'
-  'empty-state.status.list.closed.assigned.heading': undefined, // 'No completed tasks'
-  'empty-state.status.list.closed.assigned.text': undefined, // 'Your tasks marked done will show up here'
-  'empty-state.status.list.closed.document.heading': undefined, // 'No completed tasks'
-  'empty-state.status.list.closed.subscribed.heading': undefined, // 'No completed tasks'
-  'empty-state.status.list.closed.subscribed.text': undefined, // 'Tasks you subscribe to marked done will show up here'
-  'empty-state.status.list.open.assigned.heading': undefined, // 'You\'re all caught up'
-  'empty-state.status.list.open.assigned.text': undefined, // 'New tasks assigned to you will show up here'
-  'empty-state.status.list.open.document.heading': undefined, // 'No tasks on this document'
-  'empty-state.status.list.open.subscribed.heading': undefined, // 'No subscribed tasks'
-  'empty-state.status.list.open.subscribed.text': undefined, // 'Tasks you subscribe to will show up here'
+  'empty-state.list.assigned.heading': '您還沒有被分配任何任務',
+  'empty-state.list.assigned.text': '一旦您被分配任務，它們將顯示在這裡',
+  'empty-state.list.create-new': '創建新任務',
+  'empty-state.list.document.heading': '這個文件還沒有任何任務',
+  'empty-state.list.document.text': '一旦文檔有了相關任務，它們將在這裡顯示。',
+  'empty-state.list.subscribed.heading': '您還沒有訂閱任何任務',
+  'empty-state.list.subscribed.text': '當您創建、修改或評論任務時，您將自動訂閱',
+  'empty-state.status.list.closed.assigned.heading': '沒有已完成的任務',
+  'empty-state.status.list.closed.assigned.text': '您標記為完成的任務將顯示在這裡',
+  'empty-state.status.list.closed.document.heading': '沒有已完成的任務',
+  'empty-state.status.list.closed.subscribed.heading': '沒有已完成的任務',
+  'empty-state.status.list.closed.subscribed.text': '您訂閱並標記為完成的任務將顯示在這裡',
+  'empty-state.status.list.open.assigned.heading': '您已經趕上了',
+  'empty-state.status.list.open.assigned.text': '分配給您的新任務將顯示在這裡',
+  'empty-state.status.list.open.document.heading': '這個文件上沒有任務',
+  'empty-state.status.list.open.subscribed.heading': '沒有訂閱的任務',
+  'empty-state.status.list.open.subscribed.text': '您訂閱的任務將顯示在這裡',
 
   /** Text used in the assignee input when there is no user assigned */
-  'form.input.assignee.no-user-assigned.text': undefined, // 'Not assigned'
+  'form.input.assignee.no-user-assigned.text': '未指派',
   /** Text used in the assignee input when searching and no users are found */
-  'form.input.assignee.search.no-users.text': undefined, // 'No users found'
+  'form.input.assignee.search.no-users.text': '找不到用戶',
   /** Placeholder text used in the search box in the assignee input */
-  'form.input.assignee.search.placeholder': undefined, // 'Select username'
+  'form.input.assignee.search.placeholder': '選擇用戶名',
   /** Text used in the assignee input when user is not authorized */
-  'form.input.assignee.unauthorized.text': undefined, // 'Unauthorized'
+  'form.input.assignee.unauthorized.text': '未授權',
   /** Text used in the assignee input when user is not found */
-  'form.input.assignee.user-not-found.text': undefined, // 'User not found'
+  'form.input.assignee.user-not-found.text': '找不到用戶',
   /** The label used in the create more toggle */
-  'form.input.create-more.text': undefined, // 'Create more'
+  'form.input.create-more.text': '創建更多',
   /** The label used in the date input to remove the current value */
-  'form.input.date.buttons.remove.text': undefined, // 'Remove'
+  'form.input.date.buttons.remove.text': '移除',
   /** Placeholder text used in the description input */
-  'form.input.description.placeholder': undefined, // 'Add description'
+  'form.input.description.placeholder': '添加描述',
   /** The label used in the target input to remove the current value */
-  'form.input.target.buttons.remove.text': undefined, // 'Remove target content'
+  'form.input.target.buttons.remove.text': '移除目標內容',
   /** The text used in the target input when encountering a schema error */
-  'form.input.target.error.schema-not-found': undefined, // 'Schema not found'
+  'form.input.target.error.schema-not-found': '找不到架構',
   /** The placeholder text used in the target input for the search component */
-  'form.input.target.search.placeholder': undefined, // 'Select document'
+  'form.input.target.search.placeholder': '選擇文件',
   /** The placeholder text for the title input */
-  'form.input.title.placeholder': undefined, // 'Task title'
+  'form.input.title.placeholder': '任務標題',
   /** The status error message presented when the user does not supply a title */
-  'form.status.error.title-required': undefined, // 'Title is required'
+  'form.status.error.title-required': '標題是必需的',
   /** The status message upon successful creation of a task */
-  'form.status.success': undefined, // 'Task created'
+  'form.status.success': '任務創建成功',
 
   /** The text displayed when no tasks are found */
-  'list.empty.text': undefined, // 'No tasks'
+  'list.empty.text': '沒有任務',
 
   /** The label for the copy link menu item */
-  'menuitem.copylink.text': undefined, // 'Copy link to task'
+  'menuitem.copylink.text': '複製任務連結',
   /** The label for the delete task menu item */
-  'menuitem.delete.text': undefined, // 'Delete task'
+  'menuitem.delete.text': '刪除任務',
   /** The label for the duplicate task menu item */
-  'menuitem.duplicate.text': undefined, // 'Duplicate task'
+  'menuitem.duplicate.text': '複製任務',
 
   /** Fragment used to construct the first entry in the activity log */
   'panel.activity.created-fragment': undefined, // 'created this task'
