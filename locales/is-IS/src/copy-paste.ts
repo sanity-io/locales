7f35560bb0eac5a9a31034d86e91e633f0c62b1e
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-07-12 11:41:08 -0700

    fix(is-IS): apply suggestions from code review
    Co-authored-by: magnimarels <70704459+magnimarels@users.noreply.github.com>

--- locales/is-IS/src/copy-paste.ts
+++ locales/is-IS/src/copy-paste.ts
@@ -27,7 +27,7 @@ export default removeUndefinedLocaleResources({
     'Gildi af gerðinni "{{type}}" er ekki leyfilegt í þessum fylkisreit',
   /** The validation message that is shown when array values are incompatible */
   'copy-paste.on-paste.validation.array-value-incompatible.description':
-    'Gildi af gerðinni "{{type}}" er ekki leyfilegt í þessum fylkisreit',
+    'Gildi af taginu "{{type}}" er ekki leyfilegt í þessum fylkisreit',
   /** The validation message that is shown when the clipboard is empty */
   'copy-paste.on-paste.validation.clipboard-empty.title': 'Ekkert til að líma inn',
   /** The validation message that is shown when the clipboard item is invalid */
@@ -42,24 +42,24 @@ export default removeUndefinedLocaleResources({
     '"{{sourceSchemaType}}" er ekki leyfilegt í "{{targetSchemaType}}"',
   /** The error message that is shown when the MIME type is not accepted */
   'copy-paste.on-paste.validation.mime-type-incompatible.description':
-    'MIME gerð "{{mimeType}}" er ekki samþykkt fyrir þennan reit',
+    'MIME tag "{{mimeType}}" er ekki samþykkt fyrir þennan reit',
   /** The error message that is shown when the MIME type validation fails */
   'copy-paste.on-paste.validation.mime-type-validation-failed.description':
-    'MIME gerðarstaðfesting mistókst',
+    'MIME tagstaðfesting mistókst',
   /** The warning message that is shown when not all values can be pasted */
   'copy-paste.on-paste.validation.partial-warning.title': 'Gat ekki límt inn öll gildi',
   /** The validation message that is shown when primitive types are incompatible */
   'copy-paste.on-paste.validation.primitive-type-incompatible.description':
-    'Gildi af gerðinni "{{type}}" er ekki leyfilegt í þessum reit',
+    'Gildi af taginu "{{type}}" er ekki leyfilegt í þessum reit',
   /** --- On paste --- */
   /** The validation message that is shown when pasting a value into a read-only target */
-  'copy-paste.on-paste.validation.read-only-target.description': 'Markmiðið er aðeins-lesið',
+  'copy-paste.on-paste.validation.read-only-target.description': 'Markið er skrifvarið',
   /** The validation message that is shown when reference is incompatible with filter */
   'copy-paste.on-paste.validation.reference-filter-incompatible.description':
     'Tilvísun er ekki leyfileg í tilvísunarreit samkvæmt síu',
   /** The validation message that is shown when reference types are incompatible */
   'copy-paste.on-paste.validation.reference-type-incompatible.description':
-    'Tilvísanir af gerðinni "{{sourceReferenceType}}" eru ekki leyfðar í tilvísunarreit sem samþykkir gerðir "{{targetReferenceTypes}}"',
+    'Tilvísanir af taginu "{{sourceReferenceType}}" eru ekki leyfðar í tilvísunarreit sem samþykkir tög "{{targetReferenceTypes}}"',
   /** The validation message that is shown when the source and target schema types are incompatible */
   'copy-paste.on-paste.validation.schema-type-incompatible.description':
     'Uppruna- og markskematýpur eru ekki samrýmanlegar',
