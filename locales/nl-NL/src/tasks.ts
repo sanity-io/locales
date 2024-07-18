7b91a3fcec0ee4a004f65f9e7e3b98d4070a404f
AutoTranslator <hello@sanity.io>
2024-07-18 21:52:05 +0000

    fix(nl-NL): automated translation updates

--- locales/nl-NL/src/tasks.ts
+++ locales/nl-NL/src/tasks.ts
@@ -15,7 +15,7 @@ export default removeUndefinedLocaleResources({
   /** The label for the button to create a new task */
   'buttons.new.text': 'Nieuwe taak',
   /** The text for tooltip in the create a new task button when mode is upsell */
-  'buttons.new.upsell-tooltip': undefined, // 'Upgrade to create tasks'
+  'buttons.new.upsell-tooltip': 'Upgrade om taken aan te maken',
   /** The label for the button that will navigate to the next task */
   'buttons.next.tooltip': 'Ga naar volgende taak',
   /** The label for the button that will previous to the next task */
@@ -51,9 +51,11 @@ export default removeUndefinedLocaleResources({
   'empty-state.list.document.text':
     'Zodra een document verbonden taken heeft, zullen ze hier getoond worden.',
   /** The heading in the tasks sidebar, when viewing the document tab, but there is not an active document*/
-  'empty-state.list.no-active-document.heading': undefined, // 'Open a document to see it\'s task'
+  'empty-state.list.no-active-document.heading':
+    'Open een document om de bijbehorende taak te zien',
   /** The text in the tasks sidebar, when viewing the document tab, but there is not an active document*/
-  'empty-state.list.no-active-document.text': undefined, // 'Tasks on your active document will be shown here.'
+  'empty-state.list.no-active-document.text':
+    'Taken van uw actieve document worden hier weergegeven.',
   /** The heading in the tasks sidebar, in the subscriber tab, when the user is not subscribed to any task*/
   'empty-state.list.subscribed.heading': 'Je hebt je nog niet ingeschreven voor taken',
   /** The text in the tasks sidebar, in the subscriber tab, when the user is not subscribed to any task*/
@@ -87,7 +89,7 @@ export default removeUndefinedLocaleResources({
   /** Text used in the assignee input when there is no user assigned */
   'form.input.assignee.no-user-assigned.text': 'Niet toegewezen',
   /** Text used in the assignee input tooltip when there is no user assigned */
-  'form.input.assignee.no-user-assigned.tooltip': undefined, // 'Set assignee'
+  'form.input.assignee.no-user-assigned.tooltip': 'Wijs een toegewezen persoon toe',
   /** Text used in the assignee input when searching and no users are found */
   'form.input.assignee.search.no-users.text': 'Geen gebruikers gevonden',
   /** Placeholder text used in the search box in the assignee input */
@@ -95,21 +97,21 @@ export default removeUndefinedLocaleResources({
   /** Text used in the assignee input when user is not authorized */
   'form.input.assignee.unauthorized.text': 'Niet geautoriseerd',
   /** Text used in the assignee input tooltip when there is no user assigned */
-  'form.input.assignee.user-assigned.tooltip': undefined, // 'Change assignee'
+  'form.input.assignee.user-assigned.tooltip': 'Verander toegewezen persoon',
   /** Text used in the assignee input when user is not found */
   'form.input.assignee.user-not-found.text': 'Gebruiker niet gevonden',
   /** The label used in the create more toggle */
   'form.input.create-more.text': 'Meer aanmaken',
   /** The label used in the date input button tooltip when it's empty */
-  'form.input.date.buttons.empty.tooltip': undefined, // 'Set due date'
+  'form.input.date.buttons.empty.tooltip': 'Stel een vervaldatum in',
   /** The label used in the date input to remove the current value */
   'form.input.date.buttons.remove.text': 'Verwijderen',
   /** The label used in the date input button tooltip when it has value */
-  'form.input.date.buttons.tooltip': undefined, // 'Change due date'
+  'form.input.date.buttons.tooltip': 'Verander vervaldatum',
   /** Placeholder text used in the description input */
   'form.input.description.placeholder': 'Voeg beschrijving toe',
   /**  Text used in the tooltip in the status change button  */
-  'form.input.status.button.tooltip': undefined, // 'Change status'
+  'form.input.status.button.tooltip': 'Verander status',
   /** The label used in the target input to remove the current value */
   'form.input.target.buttons.remove.text': 'Verwijder doelinhoud',
   /** The text used in the target input when encountering a schema error */
@@ -126,7 +128,7 @@ export default removeUndefinedLocaleResources({
   /** The text displayed when no tasks are found */
   'list.empty.text': 'Geen taken',
   /** The text displayed at the bottom of the tasks list inviting users provide feedback */
-  'list.feedback.text': undefined, // 'Help us improve, <Link>share feedback on Tasks</Link> '
+  'list.feedback.text': 'Help ons te verbeteren, <Link>geef feedback over Taken</Link>',
 
   /** The label for the copy link menu item */
   'menuitem.copylink.text': 'Kopieer link naar taak',
@@ -135,7 +137,7 @@ export default removeUndefinedLocaleResources({
   /** The label for the duplicate task menu item */
   'menuitem.duplicate.text': 'Taak dupliceren',
   /** The text for the duplicate task menu item tooltip when mode is upsell */
-  'menuitem.duplicate.upsell-tooltip': undefined, // 'Upgrade to duplicate tasks'
+  'menuitem.duplicate.upsell-tooltip': 'Upgrade om taken te dupliceren',
 
   /** Fragment used to construct the first entry in the activity log */
   'panel.activity.created-fragment': 'heeft deze taak aangemaakt',
@@ -148,7 +150,7 @@ export default removeUndefinedLocaleResources({
   /** The placeholder text for the comment text box */
   'panel.comment.placeholder': 'Voeg een opmerking toe...',
   /** The placeholder text for the comment text box when mode is upsell */
-  'panel.comment.placeholder.upsell': undefined, // 'Upgrade to comment on tasks'
+  'panel.comment.placeholder.upsell': 'Upgrade om te reageren op taken',
   /** The title used in the task panel when showing the create task form */
   'panel.create.title': 'Aanmaken',
   /** The title used in the drafts pulldown */
