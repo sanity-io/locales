ff80cca31c4f5abbc6e799b542bcad853d187cf9
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-07-19 12:05:49 -0700

    refactor: define locale function used for extraction as a constant

--- src/util/getBaseBundles.ts
+++ src/util/getBaseBundles.ts
@@ -9,6 +9,7 @@ import {memoizeAsyncFunction} from './memoizeAsyncFunction'
 
 const DEPENDENCIES = ['sanity', '@sanity/vision']
 const GLOB_PATTERN = `node_modules/{${DEPENDENCIES.join(',')}}/src/**/*.ts`
+const LOCALE_DEF_FN_NAME = 'defineLocalesResources'
 
 /**
  * Get officially defined bundles from the Sanity source code.
@@ -47,7 +48,7 @@ export const getBaseBundles = memoizeAsyncFunction(async function getBaseBundles
 })
 
 function getLocaleResourceImport(file: ProxifiedModule) {
-  return file.imports.$items.find((item) => item.imported === 'defineLocalesResources')
+  return file.imports.$items.find((item) => item.imported === LOCALE_DEF_FN_NAME)
 }
 
 function extractResources(ast: Node, local: string, fileName: string): ResourceBundle {
@@ -63,17 +64,17 @@ function extractResources(ast: Node, local: string, fileName: string): ResourceB
   })
 
   if (!node) {
-    throw new Error(`Could not find call to defineLocaleResources in ${fileName}`)
+    throw new Error(`Could not find call to ${LOCALE_DEF_FN_NAME} in ${fileName}`)
   }
 
   if (node.arguments.length !== 2) {
-    throw new Error(`Expected two arguments to defineLocaleResources in ${fileName}`)
+    throw new Error(`Expected two arguments to ${LOCALE_DEF_FN_NAME} in ${fileName}`)
   }
 
   const namespaceArg = node.arguments[0]
   if (namespaceArg.type !== 'StringLiteral') {
     throw new Error(
-      `Expected first argument to defineLocaleResources to be a string literal in ${fileName}`,
+      `Expected first argument to ${LOCALE_DEF_FN_NAME} to be a string literal in ${fileName}`,
     )
   }
 
@@ -86,39 +87,39 @@ function extractResources(ast: Node, local: string, fileName: string): ResourceB
 
   if (resourcesArg.type !== 'ObjectExpression') {
     throw new Error(
-      `Expected second argument to defineLocaleResources to be an object expression in ${fileName}`,
+      `Expected second argument to ${LOCALE_DEF_FN_NAME} to be an object expression in ${fileName}`,
     )
   }
 
   const resources: BaseResource[] = resourcesArg.properties.map((prop) => {
     if (prop.type !== 'ObjectProperty') {
-      throw new Error(`Found non-object property in defineLocaleResources in ${fileName}`)
+      throw new Error(`Found non-object property in ${LOCALE_DEF_FN_NAME} in ${fileName}`)
     }
 
     if (prop.computed || prop.shorthand) {
       throw new Error(
-        `Found computed or shorthand property in defineLocaleResources in ${fileName}`,
+        `Found computed or shorthand property in ${LOCALE_DEF_FN_NAME} in ${fileName}`,
       )
     }
 
     if (prop.key.type !== 'StringLiteral') {
-      throw new Error(`Found non-string key in defineLocaleResources in ${fileName}`)
+      throw new Error(`Found non-string key in ${LOCALE_DEF_FN_NAME} in ${fileName}`)
     }
 
     if (prop.value.type !== 'StringLiteral' && prop.value.type !== 'TemplateLiteral') {
-      throw new Error(`Found non-string value in defineLocaleResources in ${fileName}`)
+      throw new Error(`Found non-string value in ${LOCALE_DEF_FN_NAME} in ${fileName}`)
     }
 
     if (prop.value.type === 'TemplateLiteral') {
       if (prop.value.expressions.length > 0) {
         throw new Error(
-          `Found template literal with expressions in defineLocaleResources in ${fileName}`,
+          `Found template literal with expressions in ${LOCALE_DEF_FN_NAME} in ${fileName}`,
         )
       }
 
       if (prop.value.quasis.length > 1) {
         throw new Error(
-          `Found template literal with multiple quasis in defineLocaleResources in ${fileName}`,
+          `Found template literal with multiple quasis in ${LOCALE_DEF_FN_NAME} in ${fileName}`,
         )
       }
     }
