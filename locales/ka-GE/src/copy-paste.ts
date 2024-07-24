bf0958d14e021a48afe46a09af26d0a26affc5f8
AutoTranslator <hello@sanity.io>
2024-07-24 13:04:52 +0000

    fix(ka-GE): automated translation updates

--- locales/ka-GE/src/copy-paste.ts
+++ locales/ka-GE/src/copy-paste.ts
@@ -2,57 +2,72 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Text on the field action button to copy a document */
-  'copy-paste.field-action-copy-button.document.title': undefined, // 'Copy document'
+  'copy-paste.field-action-copy-button.document.title': 'დოკუმენტის კოპირება',
   /** Text on the field action button to copy a field */
-  'copy-paste.field-action-copy-button.field.title': undefined, // 'Copy field'
+  'copy-paste.field-action-copy-button.field.title': 'ველის კოპირება',
   /** Text on the field action button to paste a document */
-  'copy-paste.field-action-paste-button.document.title': undefined, // 'Paste document'
+  'copy-paste.field-action-paste-button.document.title': 'დოკუმენტის ჩასმა',
   /** Text on the field action button to paste a field */
-  'copy-paste.field-action-paste-button.field.title': undefined, // 'Paste field'
+  'copy-paste.field-action-paste-button.field.title': 'ველის ჩასმა',
   /** The error message that is shown when the clipboard is not supported */
-  'copy-paste.on-copy.validation.clipboard-not-supported.title': undefined, // 'Your browser does not support this action'
+  'copy-paste.on-copy.validation.clipboard-not-supported.title':
+    'თქვენი ბრაუზერი ამ ქმედებას არ მხარდაჭერს',
   /** The success message that is shown when a document is copied */
-  'copy-paste.on-copy.validation.copy-document-success.title': undefined, // 'Document "{{fieldNames}}" copied'
+  'copy-paste.on-copy.validation.copy-document-success.title':
+    'დოკუმენტი "{{fieldNames}}" დაკოპირდა',
   /** The success message that is shown when a field is copied */
-  'copy-paste.on-copy.validation.copy-field_one-success.title': undefined, // 'Field "{{fieldName}}" copied'
+  'copy-paste.on-copy.validation.copy-field_one-success.title': 'ველი "{{fieldName}}" დაკოპირდა',
   /** The error message that is shown when there is no value to copy */
-  'copy-paste.on-copy.validation.no-value.title': undefined, // 'Empty value, nothing to copy'
+  'copy-paste.on-copy.validation.no-value.title': 'ცარიელი მნიშვნელობა, კოპირებადი არაფერია',
   /** --- On copy --- */
   /** The error message that is shown when schema types are incompatible */
-  'copy-paste.on-copy.validation.schema-type-incompatible.title': undefined, // 'Could not resolve schema type for path: {{path}}'
+  'copy-paste.on-copy.validation.schema-type-incompatible.title':
+    'ვერ მოხდა სქემის ტიპის გარჩევა გზისთვის: {{path}}',
   /** The validation message that is shown when array types are incompatible */
-  'copy-paste.on-paste.validation.array-type-incompatible.description': undefined, // 'Value of type "{{type}}" is not allowed in this array field'
+  'copy-paste.on-paste.validation.array-type-incompatible.description':
+    'ტიპის "{{type}}" მნიშვნელობა არ არის ნებადართული ამ მასივის ველში',
   /** The validation message that is shown when array values are incompatible */
-  'copy-paste.on-paste.validation.array-value-incompatible.description': undefined, // 'Value of type "{{type}}" is not allowed in this array field'
+  'copy-paste.on-paste.validation.array-value-incompatible.description':
+    'ტიპის "{{type}}" მნიშვნელობა არ არის ნებადართული ამ მასივის ველში',
   /** The validation message that is shown when the clipboard is empty */
-  'copy-paste.on-paste.validation.clipboard-empty.title': undefined, // 'Nothing to paste'
+  'copy-paste.on-paste.validation.clipboard-empty.title': 'ჩასასმელი არაფერია',
   /** The validation message that is shown when the clipboard item is invalid */
-  'copy-paste.on-paste.validation.clipboard-invalid.title': undefined, // 'Invalid clipboard item'
+  'copy-paste.on-paste.validation.clipboard-invalid.title': 'კლიპბორდის ელემენტი არასწორია',
   /** The success message that is shown when a document is pasted */
-  'copy-paste.on-paste.validation.document-paste-success.title': undefined, // 'Document "{{fieldNames}}" updated'
+  'copy-paste.on-paste.validation.document-paste-success.title':
+    'დოკუმენტი "{{fieldNames}}" განახლდა',
   /** The success message that is shown when a field is pasted */
-  'copy-paste.on-paste.validation.field_one-paste-success.title': undefined, // 'Field "{{fieldName}}" updated'
+  'copy-paste.on-paste.validation.field_one-paste-success.title': 'ველი "{{fieldName}}" განახლდა',
   /** The validation message that is shown when image files are incompatible */
-  'copy-paste.on-paste.validation.image-file-incompatible.description': undefined, // 'A "{{sourceSchemaType}}" is not allowed in a "{{targetSchemaType}}"'
+  'copy-paste.on-paste.validation.image-file-incompatible.description':
+    '"{{sourceSchemaType}}" არ არის ნებადართული "{{targetSchemaType}}"-ში',
   /** The error message that is shown when the MIME type is not accepted */
-  'copy-paste.on-paste.validation.mime-type-incompatible.description': undefined, // 'MIME type "{{mimeType}}" is not accepted for this field'
+  'copy-paste.on-paste.validation.mime-type-incompatible.description':
+    'MIME ტიპი "{{mimeType}}" არ არის ნებადართული ამ ველისთვის',
   /** The error message that is shown when the MIME type validation fails */
-  'copy-paste.on-paste.validation.mime-type-validation-failed.description': undefined, // 'MIME type validation failed'
+  'copy-paste.on-paste.validation.mime-type-validation-failed.description':
+    'MIME ტიპის ვალიდაცია ჩავარდა',
   /** The warning message that is shown when not all values can be pasted */
-  'copy-paste.on-paste.validation.partial-warning.title': undefined, // 'Could not paste all values'
+  'copy-paste.on-paste.validation.partial-warning.title': 'ყველა მნიშვნელობა ვერ ჩაისვა',
   /** The validation message that is shown when primitive types are incompatible */
-  'copy-paste.on-paste.validation.primitive-type-incompatible.description': undefined, // 'Value of type "{{type}}" is not allowed in this field'
+  'copy-paste.on-paste.validation.primitive-type-incompatible.description':
+    'ტიპის "{{type}}" მნიშვნელობა არ არის ნებადართული ამ ველში',
   /** --- On paste --- */
   /** The validation message that is shown when pasting a value into a read-only target */
-  'copy-paste.on-paste.validation.read-only-target.description': undefined, // 'The target is read-only'
+  'copy-paste.on-paste.validation.read-only-target.description': 'მიმღები არის მხოლოდ კითხვისთვის',
   /** The validation message that is shown when reference is incompatible with filter */
-  'copy-paste.on-paste.validation.reference-filter-incompatible.description': undefined, // 'Reference is not allowed in reference field according to filter'
+  'copy-paste.on-paste.validation.reference-filter-incompatible.description':
+    'რეფერენსი არ არის ნებადართული რეფერენსის ველში ფილტრის მიხედვით',
   /** The validation message that is shown when reference types are incompatible */
-  'copy-paste.on-paste.validation.reference-type-incompatible.description': undefined, // 'References of type "{{sourceReferenceType}}" is not allowed in reference field that accepts types "{{targetReferenceTypes}}"'
+  'copy-paste.on-paste.validation.reference-type-incompatible.description':
+    '"{{sourceReferenceType}}" ტიპის რეფერენსები არ არის ნებადართული რეფერენსის ველში, რომელიც მიიღებს ტიპებს "{{targetReferenceTypes}}"',
   /** The validation message that is shown when the source and target schema types are incompatible */
-  'copy-paste.on-paste.validation.schema-type-incompatible.description': undefined, // 'Source and target schema types are not compatible'
+  'copy-paste.on-paste.validation.schema-type-incompatible.description':
+    'წყაროს და მიმღების სქემის ტიპები არ არის თანამედროვე',
   /** The validation message that is shown when schema types are incompatible */
-  'copy-paste.on-paste.validation.schema-type-incompatible.title': undefined, // 'Could not resolve schema type for path: {{path}}'
+  'copy-paste.on-paste.validation.schema-type-incompatible.title':
+    'ვერ მოხდა სქემის ტიპის გარჩევა გზისთვის: {{path}}',
   /** The validation message that is shown when string values are incompatible */
-  'copy-paste.on-paste.validation.string-value-incompatible.description': undefined, // 'Value "{{value}}" is not allowed in "{{allowedStrings}}"'
+  'copy-paste.on-paste.validation.string-value-incompatible.description':
+    'მნიშვნელობა "{{value}}" არ არის დასაშვები "{{allowedStrings}}"-ში',
 })
