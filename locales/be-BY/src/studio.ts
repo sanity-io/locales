e1330bee61fd632d2e3eee3f7382c0a2544788b9
AutoTranslator <hello@sanity.io>
2024-08-06 20:58:50 +0000

    fix(be-BY): automated translation updates

--- locales/be-BY/src/studio.ts
+++ locales/be-BY/src/studio.ts
@@ -129,9 +129,9 @@ export default removeUndefinedLocaleResources({
   /** Label for selecting an hour preset. Receives a `time` param as a string on hh:mm format and a `date` param as a Date instance denoting the preset date */
   'calendar.action.set-to-time-preset': '{{time}} на {{date, datetime}}',
   /** Aria label for button to open date picker */
-  'calendar.button.aria-label': undefined, // 'Open calendar'
+  'calendar.button.aria-label': 'Адкрыць каляндар',
   /** Tooltip content for button to open datetime input */
-  'calendar.button.tooltip-text': undefined, // 'Select date'
+  'calendar.button.tooltip-text': 'Выбраць дату',
   /** Error message displayed in calendar when entered date is not the correct format */
   'calendar.error.must-be-in-format': 'Павінна быць у фармаце: {{exampleDate}}',
   /** Month name for April */
