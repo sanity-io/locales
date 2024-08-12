e97af4e825c4d575a365bd604f4885657e23e952
AutoTranslator <hello@sanity.io>
2024-08-12 20:15:19 +0000

    fix(ka-GE): automated translation updates

--- locales/ka-GE/src/studio.ts
+++ locales/ka-GE/src/studio.ts
@@ -135,9 +135,9 @@ export default removeUndefinedLocaleResources({
   /** Label for selecting an hour preset. Receives a `time` param as a string on hh:mm format and a `date` param as a Date instance denoting the preset date */
   'calendar.action.set-to-time-preset': '{{time}} დროზე {{date, datetime}}',
   /** Aria label for button to open date picker */
-  'calendar.button.aria-label': undefined, // 'Open calendar'
+  'calendar.button.aria-label': 'კალენდარის გახსნა',
   /** Tooltip content for button to open datetime input */
-  'calendar.button.tooltip-text': undefined, // 'Select date'
+  'calendar.button.tooltip-text': 'თარიღის არჩევა',
   /** Error message displayed in calendar when entered date is not the correct format */
   'calendar.error.must-be-in-format': 'უნდა იყოს ფორმატში: {{exampleDate}}',
   /** Month name for April */
