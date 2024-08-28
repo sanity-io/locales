02eee3a8059a266248722195c6efd5d711dad55a
AutoTranslator <hello@sanity.io>
2024-08-28 02:07:59 +0000

    fix(it-IT): automated translation updates

--- locales/it-IT/src/structure.ts
+++ locales/it-IT/src/structure.ts
@@ -1,6 +1,8 @@
 import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
+  /** Label for the "Copy Document URL" document action */
+  'action.copy-document-url.label': 'Copia URL documento',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'Operazione non pronta',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -342,6 +344,9 @@ export default removeUndefinedLocaleResources({
   /** The text when a generic operation succeeded (fallback, generally not shown)  */
   'panes.document-operation-results.operation-success':
     'Operazione su documento {{context}} eseguita con successo',
+  /** The text when copy URL operation succeeded  */
+  'panes.document-operation-results.operation-success_copy-url':
+    'URL del documento copiato negli appunti',
   /** The text when a delete operation succeeded  */
   'panes.document-operation-results.operation-success_delete':
     'Il documento è stato eliminato con successo',
