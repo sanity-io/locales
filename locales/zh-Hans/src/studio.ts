f932c3cbf7462c87c473016dc6c3b6bc77a69f1f
AutoTranslator <hello@sanity.io>
2024-08-28 02:08:37 +0000

    fix(zh-Hans): automated translation updates

--- locales/zh-Hans/src/studio.ts
+++ locales/zh-Hans/src/studio.ts
@@ -1022,7 +1022,7 @@ export default removeUndefinedLocaleResources({
   /** Title of the alert for studio users when packages in their studio are out-of-date */
   'package-version.new-package-available.title': 'Sanity Studio 已更新',
 
-  /** Label for action to manage members of the current studio project */
+  /** Label for action to invite members to the current studio project */
   'presence.action.manage-members': '管理成员',
   /** Accessibility label for presence menu button */
   'presence.aria-label': '谁在这里',
