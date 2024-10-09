308d6d2a523f6079502c6e75aa5ff5408640dbd0
AutoTranslator <hello@sanity.io>
2024-10-09 05:18:10 +0000

    fix(it-IT): automated translation updates

--- locales/it-IT/src/studio.ts
+++ locales/it-IT/src/studio.ts
@@ -2,15 +2,15 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** The text used in the tooltip shown in the dialog close button */
-  'announcement.dialog.close': undefined, // 'Close'
+  'announcement.dialog.close': 'Chiudi',
   /** Aria label to be used in the dialog close button */
-  'announcement.dialog.close-label': undefined, // 'Close dialog'
+  'announcement.dialog.close-label': 'Chiudi dialogo',
   /**Text to be used in the tooltip in the button in the studio announcement card */
-  'announcement.floating-button.dismiss': undefined, // 'Close'
+  'announcement.floating-button.dismiss': 'Chiudi',
   /**Aria label to be used in the floating button in the studio announcement card, to dismiss the card */
-  'announcement.floating-button.dismiss-label': undefined, // 'Dismiss announcements'
+  'announcement.floating-button.dismiss-label': 'Ignora annunci',
   /**Aria label to be used in the floating button in the studio announcement card */
-  'announcement.floating-button.open-label': undefined, // 'Open announcements'
+  'announcement.floating-button.open-label': 'Apri annunci',
 
   /** Menu item for deleting the asset */
   'asset-source.asset-list.menu.delete': 'Elimina',
