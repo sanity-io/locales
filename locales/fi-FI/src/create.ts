49346f3075b1d67babcdef5d74293738d67e846a
AutoTranslator <hello@sanity.io>
2024-11-22 18:04:50 +0000

    fix(fi-FI): automated translation updates

--- locales/fi-FI/src/create.ts
+++ locales/fi-FI/src/create.ts
@@ -2,65 +2,74 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Text in badge above header in Create Link info popover */
-  'create-link-info-popover.eyebrow-badge': undefined, // 'Early access'
+  'create-link-info-popover.eyebrow-badge': 'Varhainen pääsy',
   /** Text above header in Create Link info popover */
-  'create-link-info-popover.eyebrow-title': undefined, // 'Sanity Create'
+  'create-link-info-popover.eyebrow-title': 'Sanity Create',
   /** Header in Create Link info popover */
-  'create-link-info-popover.header': undefined, // 'Idea-first authoring'
+  'create-link-info-popover.header': 'Idea-ensin kirjoittaminen',
   /** Informational text in Create Link info popover */
-  'create-link-info-popover.text': undefined, // 'Write naturally in an AI-powered editor. Your content automatically maps to Studio fields as you type.'
+  'create-link-info-popover.text':
+    'Kirjoita luonnollisesti tekoälyä hyödyntävässä editorissa. Sisältösi kartoittuu automaattisesti Studio-kenttiin kirjoittaessasi.',
 
   /** Tooltip for Create Link button */
-  'create-link-info.tooltip': undefined, // 'Learn more'
+  'create-link-info.tooltip': 'Lue lisää',
 
   /** Edit in Create button text */
-  'edit-in-create-button.text': undefined, // 'Edit with Sanity Create'
+  'edit-in-create-button.text': 'Muokkaa Sanity Creatella',
 
   /** Details for the "Linking in progress" dialog */
-  'linking-in-progress-dialog.details': undefined, // 'Once linked, your edits will appear here in real-time.'
+  'linking-in-progress-dialog.details':
+    'Kun linkitys on valmis, muokkauksesi näkyvät täällä reaaliajassa.',
   /** Header of the "Linking in progress" dialog */
-  'linking-in-progress-dialog.header': undefined, // 'Linking to Sanity Create'
+  'linking-in-progress-dialog.header': 'Linkitetään Sanity Createen',
   /** Lede text for the "Linking in progress" dialog */
-  'linking-in-progress-dialog.lede': undefined, // 'Head back to Sanity Create. Your document will sync automatically (usually takes a few seconds).'
+  'linking-in-progress-dialog.lede':
+    'Palaa takaisin Sanity Createen. Dokumenttisi synkronoituu automaattisesti (yleensä vie muutaman sekunnin).',
   /** "Linking in progress" troubleshooting button title */
-  'linking-in-progress-dialog.troubleshooting.button.title': undefined, // 'Linking delayed?'
+  'linking-in-progress-dialog.troubleshooting.button.title': 'Linkityksessä viivästyksiä?',
   /** "Linking in progress" troubleshooting content */
-  'linking-in-progress-dialog.troubleshooting.content': undefined, // 'Check if your Sanity Create window is still open, or if the process seems stuck. If problems persist, try refreshing the Studio and try again.'
+  'linking-in-progress-dialog.troubleshooting.content':
+    'Tarkista, onko Sanity Create -ikkunasi edelleen auki, tai vaikuttaako prosessi jumittuneelta. Jos ongelmia jatkuu, yritä päivittää Studio ja yritä uudelleen.',
 
   /** "Start in Sanity Create" action button text */
-  'start-in-create-action.label': undefined, // 'Start in Sanity Create'
+  'start-in-create-action.label': 'Aloita Sanity Createssa',
 
   /** CTA in "Start writing in Create" dialog: Continue to create */
-  'start-in-create-dialog.cta.continue': undefined, // 'Continue'
+  'start-in-create-dialog.cta.continue': 'Jatka',
   /** CTA in "Start writing in Create" dialog: Learn more */
-  'start-in-create-dialog.cta.learn-more': undefined, // 'Learn more'
+  'start-in-create-dialog.cta.learn-more': 'Lue lisää',
   /** The details on what "Start writing in Create" implies */
-  'start-in-create-dialog.details': undefined, // 'This opens in a new window, linked directly to this document.'
+  'start-in-create-dialog.details':
+    'Tämä avautuu uudessa ikkunassa, linkitettynä suoraan tähän dokumenttiin.',
   /** Text for checkbox in "Start writing in Create" dialog for disabling the dialog in the future */
-  'start-in-create-dialog.dont-remind-me-checkbox': undefined, // 'Don’t remind me again'
+  'start-in-create-dialog.dont-remind-me-checkbox': 'Älä muistuta minua uudelleen',
   /** Toast error message when we dont have a resolved Sanity Create url*/
-  'start-in-create-dialog.error-toast.unresolved-url': undefined, // 'Unable to determine Sanity Create URL.'
+  'start-in-create-dialog.error-toast.unresolved-url':
+    'Sanity Create URL-osoitetta ei voitu määrittää.',
   /** Header of the "Start writing in Create" dialog */
-  'start-in-create-dialog.header': undefined, // 'Write naturally, publish structured'
+  'start-in-create-dialog.header': 'Kirjoita luonnollisesti, julkaise rakenteisesti',
   /** The lede text in the "Start writing in Create" dialog */
-  'start-in-create-dialog.lede': undefined, // 'Create content your way in an AI-powered editor. Your writing automatically maps to the right fields in Sanity Studio as you type.'
+  'start-in-create-dialog.lede':
+    'Luo sisältöä omalla tavallasi tekoälyä hyödyntävässä editorissa. Kirjoituksesi kartoittuvat automaattisesti oikeisiin kenttiin Sanity Studiossa kirjoittaessasi.',
 
   /** Text for the document pane banner informing users that the document is linked to Sanity Create */
-  'studio-create-link-banner.text': undefined, // 'This document is linked to Sanity Create'
+  'studio-create-link-banner.text': 'Tämä dokumentti on linkitetty Sanity Createen',
 
   /** Unlink document from Sanity Create button text */
-  'unlink-from-create-button.text': undefined, // 'Unlink'
+  'unlink-from-create-button.text': 'Poista linkitys',
 
   /** Unlink from Create dialog: Cancel button text */
-  'unlink-from-create-dialog.cancel.text': undefined, // 'Cancel'
+  'unlink-from-create-dialog.cancel.text': 'Peruuta',
   /** Unlink from Create dialog: Document title used if no other title can be determined */
-  'unlink-from-create-dialog.document.untitled.text': undefined, // 'Untitled'
+  'unlink-from-create-dialog.document.untitled.text': 'Nimetön',
   /** Unlink from Create dialog – first informational paragraph */
-  'unlink-from-create-dialog.first-paragraph': undefined, // 'You’re unlinking “<strong>{{title}}</strong>” from Sanity Create so it can be edited here.'
+  'unlink-from-create-dialog.first-paragraph':
+    'Olet irrottamassa “<strong>{{title}}</strong>” Sanity Createsta, jotta sitä voidaan muokata täällä.',
   /** Unlink from Create dialog header */
-  'unlink-from-create-dialog.header': undefined, // 'Switch editing to Studio?'
+  'unlink-from-create-dialog.header': 'Vaihda muokkaus Studioon?',
   /** Unlink from Create dialog – second informational paragraph */
-  'unlink-from-create-dialog.second-paragraph': undefined, // 'You’ll keep your content in both places. Any new changes in Sanity Create will stop syncing to this Studio.'
+  'unlink-from-create-dialog.second-paragraph':
+    'Sisältösi säilyy molemmissa paikoissa. Kaikki uudet muutokset Sanity Create:ssa lakkaavat synkronoitumasta tähän Studioon.',
   /** Unlink from Create dialog: Unlink button text */
-  'unlink-from-create-dialog.unlink.text': undefined, // 'Unlink now'
+  'unlink-from-create-dialog.unlink.text': 'Katkaise linkitys nyt',
 })
