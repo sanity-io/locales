9eeecb1c8cf291e36428ef6ed168edb86898ec83
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-11-26 10:05:37 -0800

    fix(nb-NO): apply suggestions from code review

--- locales/nb-NO/src/create.ts
+++ locales/nb-NO/src/create.ts
@@ -6,10 +6,10 @@ export default removeUndefinedLocaleResources({
   /** Text above header in Create Link info popover */
   'create-link-info-popover.eyebrow-title': 'Sanity Create',
   /** Header in Create Link info popover */
-  'create-link-info-popover.header': 'Idé-først forfatterskap',
+  'create-link-info-popover.header': 'Idea-first authoring',
   /** Informational text in Create Link info popover */
   'create-link-info-popover.text':
-    'Skriv naturlig i en AI-drevet redaktør. Innholdet ditt kartlegges automatisk til Studio-feltene mens du skriver.',
+    'Skriv naturlig på en AI-drevet flate. Innholdet ditt kartlegges automatisk til Studio-feltene mens du skriver.',
 
   /** Tooltip for Create Link button */
   'create-link-info.tooltip': 'Lær mer',
@@ -26,7 +26,7 @@ export default removeUndefinedLocaleResources({
   'linking-in-progress-dialog.lede':
     'Gå tilbake til Sanity Create. Dokumentet ditt vil synkroniseres automatisk (tar vanligvis noen sekunder).',
   /** "Linking in progress" troubleshooting button title */
-  'linking-in-progress-dialog.troubleshooting.button.title': 'Forsinket kobling?',
+  'linking-in-progress-dialog.troubleshooting.button.title': 'Treg kobling?',
   /** "Linking in progress" troubleshooting content */
   'linking-in-progress-dialog.troubleshooting.content':
     'Sjekk om ditt Sanity Create-vindu fortsatt er åpent, eller om prosessen ser ut til å være fast. Hvis problemene vedvarer, prøv å oppdatere Studio og prøv igjen.',
@@ -42,14 +42,14 @@ export default removeUndefinedLocaleResources({
   'start-in-create-dialog.details':
     'Dette åpnes i et nytt vindu, koblet direkte til dette dokumentet.',
   /** Text for checkbox in "Start writing in Create" dialog for disabling the dialog in the future */
-  'start-in-create-dialog.dont-remind-me-checkbox': 'Ikke minn meg på igjen',
+  'start-in-create-dialog.dont-remind-me-checkbox': 'Ikke påminn meg',
   /** Toast error message when we dont have a resolved Sanity Create url*/
-  'start-in-create-dialog.error-toast.unresolved-url': 'Kan ikke bestemme Sanity Create URL.',
+  'start-in-create-dialog.error-toast.unresolved-url': 'Kunne ikke finne URL for Sanity Create.',
   /** Header of the "Start writing in Create" dialog */
   'start-in-create-dialog.header': 'Skriv naturlig, publiser strukturert',
   /** The lede text in the "Start writing in Create" dialog */
   'start-in-create-dialog.lede':
-    'Opprett innhold på din måte i en AI-drevet redaktør. Det du skriver kartlegges automatisk til de riktige feltene i Sanity Studio mens du skriver.',
+    'Opprett innhold på din måte i en AI-drevet flate. Det du skriver kartlegges automatisk til de riktige feltene i Sanity Studio mens du skriver.',
 
   /** Text for the document pane banner informing users that the document is linked to Sanity Create */
   'studio-create-link-banner.text': 'Dette dokumentet er koblet til Sanity Create',
