a72fd778715860e1f5246da3b6435fae35cb49eb
AutoTranslator <hello@sanity.io>
2024-12-06 16:18:34 +0000

    fix(nn-NO): automated translation updates

--- locales/nn-NO/src/studio.ts
+++ locales/nn-NO/src/studio.ts
@@ -349,7 +349,7 @@ export default removeUndefinedLocaleResources({
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': 'Publisert {{date}}',
   /** Label to show in the document footer indicating the revision from date of the document */
-  'document-status.revision-from': undefined, // 'Revision from <em>{{date}}</em>'
+  'document-status.revision-from': 'Revisjon frå <em>{{date}}</em>',
 
   /** The value of the <code>_key</code> property must be a unique string. */
   'form.error.duplicate-keys-alert.details.additional-description':
@@ -651,11 +651,13 @@ export default removeUndefinedLocaleResources({
   /** Hotspot & Crop */
   'inputs.imagetool.title': 'Hotspot & Crop',
   /** Warnings displayed to developers when using the crop/hotspot tool on vector images, notifying them that crops/hotspot are not respected when serving the image in vector format. For the crop/hotspot to apply, images must be served in a raster format such as JPG or PNG, by appending eg `fm=jpg` to the image url, or calling `format('jpg')` if using `@sanity/image-url` */
-  'inputs.imagetool.vector-warning.developer-info': undefined, // 'The Asset Pipeline does not support hotspot and crop for vector formats. To enable hotspot & crop, output this image to any of the supported raster formats. For example: <code>fm=jpg</code> to the <ImageUrlDocumentationLink>image URL</ImageUrlDocumentationLink> or call <code>.format(\'png\')</code> with <ImageUrlPackageDocumentationLink>@sanity/image-url</ImageUrlPackageDocumentationLink>.'
+  'inputs.imagetool.vector-warning.developer-info':
+    "Asset Pipeline støttar ikkje hotspot og crop for vektorformat. For å aktivere hotspot & crop, eksporter dette bildet til eit av dei støtta rasterformatene. Til dømes: <code>fm=jpg</code> til <ImageUrlDocumentationLink>bilete-URLen</ImageUrlDocumentationLink> eller kall <code>.format('png')</code> med <ImageUrlPackageDocumentationLink>@sanity/image-url</ImageUrlPackageDocumentationLink>.",
   /** See developer info */
-  'inputs.imagetool.vector-warning.expand-developer-info': undefined, // 'See developer info'
+  'inputs.imagetool.vector-warning.expand-developer-info': 'Sjå utviklarinfo',
   /** Gotcha: Serving vector images with hotspot and crop from the Sanity Image API */
-  'inputs.imagetool.vector-warning.title': undefined, // 'Warning: Hotspot and crop might not be applied to this image where it\'s presented.'
+  'inputs.imagetool.vector-warning.title':
+    'Åtvaring: Hotspot og crop kan hende ikkje blir brukt på dette bildet der det er presentert.',
   /** Convert to `{{targetType}}` */
   'inputs.invalid-value.convert-button.text': 'Konverter til <code>{{targetType}}</code>',
   /** The current value (<code>`{{actualType}}`</code>) */
@@ -1584,7 +1586,7 @@ export default removeUndefinedLocaleResources({
   'status-button.aria-label': 'Konfigurasjonsstatus',
 
   /** Title for the changes tooltip in the history inspector*/
-  'timeline.changes.title': undefined, // 'Changes by'
+  'timeline.changes.title': 'Endringar av',
   /** Description for error when the timeline for the given document can't be loaded */
   'timeline.error.load-document-changes-description':
     'Dokumenthistorikktransaksjonane har ikkje blitt påverka.',
