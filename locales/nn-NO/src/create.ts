d80d1fdc4e2318bad78be9432808c370464fb56a
AutoTranslator <hello@sanity.io>
2024-12-06 16:18:48 +0000

    fix(nn-NO): automated translation updates

--- locales/nn-NO/src/create.ts
+++ locales/nn-NO/src/create.ts
@@ -2,65 +2,73 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Text in badge above header in Create Link info popover */
-  'create-link-info-popover.eyebrow-badge': undefined, // 'Early access'
+  'create-link-info-popover.eyebrow-badge': 'Tidleg tilgang',
   /** Text above header in Create Link info popover */
-  'create-link-info-popover.eyebrow-title': undefined, // 'Sanity Create'
+  'create-link-info-popover.eyebrow-title': 'Sanity Create',
   /** Header in Create Link info popover */
-  'create-link-info-popover.header': undefined, // 'Idea-first authoring'
+  'create-link-info-popover.header': 'Idé-først forfattarskap',
   /** Informational text in Create Link info popover */
-  'create-link-info-popover.text': undefined, // 'Write naturally in an AI-powered editor. Your content automatically maps to Studio fields as you type.'
+  'create-link-info-popover.text':
+    'Skriv naturleg i ein AI-dreven redaktør. Innholdet ditt blir automatisk kartlagt til Studio-felt når du skriv.',
 
   /** Tooltip for Create Link button */
-  'create-link-info.tooltip': undefined, // 'Learn more'
+  'create-link-info.tooltip': 'Lær meir',
 
   /** Edit in Create button text */
-  'edit-in-create-button.text': undefined, // 'Edit with Sanity Create'
+  'edit-in-create-button.text': 'Rediger med Sanity Create',
 
   /** Details for the "Linking in progress" dialog */
-  'linking-in-progress-dialog.details': undefined, // 'Once linked, your edits will appear here in real-time.'
+  'linking-in-progress-dialog.details':
+    'Når lenkinga er ferdig, vil redigeringane dine visast her i sanntid.',
   /** Header of the "Linking in progress" dialog */
-  'linking-in-progress-dialog.header': undefined, // 'Linking to Sanity Create'
+  'linking-in-progress-dialog.header': 'Lenker til Sanity Create',
   /** Lede text for the "Linking in progress" dialog */
-  'linking-in-progress-dialog.lede': undefined, // 'Head back to Sanity Create. Your document will sync automatically (usually takes a few seconds).'
+  'linking-in-progress-dialog.lede':
+    'Gå tilbake til Sanity Create. Dokumentet ditt vil synkroniserast automatisk (vanlegvis tar det nokre sekund).',
   /** "Linking in progress" troubleshooting button title */
-  'linking-in-progress-dialog.troubleshooting.button.title': undefined, // 'Linking delayed?'
+  'linking-in-progress-dialog.troubleshooting.button.title': 'Lenking forsinka?',
   /** "Linking in progress" troubleshooting content */
-  'linking-in-progress-dialog.troubleshooting.content': undefined, // 'Check if your Sanity Create window is still open, or if the process seems stuck. If problems persist, try refreshing the Studio and try again.'
+  'linking-in-progress-dialog.troubleshooting.content':
+    'Sjekk om ditt Sanity Create-vindauge framleis er opent, eller om prosessen ser ut til å ha stoppa opp. Om problem vedvarer, prøv å oppfriske Studio og prøv igjen.',
 
   /** "Start in Sanity Create" action button text */
-  'start-in-create-action.label': undefined, // 'Start in Sanity Create'
+  'start-in-create-action.label': 'Start i Sanity Create',
 
   /** CTA in "Start writing in Create" dialog: Continue to create */
-  'start-in-create-dialog.cta.continue': undefined, // 'Continue'
+  'start-in-create-dialog.cta.continue': 'Fortsett',
   /** CTA in "Start writing in Create" dialog: Learn more */
-  'start-in-create-dialog.cta.learn-more': undefined, // 'Learn more'
+  'start-in-create-dialog.cta.learn-more': 'Lær meir',
   /** The details on what "Start writing in Create" implies */
-  'start-in-create-dialog.details': undefined, // 'This opens in a new window, linked directly to this document.'
+  'start-in-create-dialog.details':
+    'Dette opnar i eit nytt vindauge, direkte lenka til dette dokumentet.',
   /** Text for checkbox in "Start writing in Create" dialog for disabling the dialog in the future */
-  'start-in-create-dialog.dont-remind-me-checkbox': undefined, // 'Don’t remind me again'
+  'start-in-create-dialog.dont-remind-me-checkbox': 'Ikkje minn meg på igjen',
   /** Toast error message when we dont have a resolved Sanity Create url*/
-  'start-in-create-dialog.error-toast.unresolved-url': undefined, // 'Unable to determine Sanity Create URL.'
+  'start-in-create-dialog.error-toast.unresolved-url': 'Kan ikkje fastsetje Sanity Create URL.',
   /** Header of the "Start writing in Create" dialog */
-  'start-in-create-dialog.header': undefined, // 'Write naturally, publish structured'
+  'start-in-create-dialog.header': 'Skriv naturleg, publiser strukturert',
   /** The lede text in the "Start writing in Create" dialog */
-  'start-in-create-dialog.lede': undefined, // 'Create content your way in an AI-powered editor. Your writing automatically maps to the right fields in Sanity Studio as you type.'
+  'start-in-create-dialog.lede':
+    'Skap innhold på din måte i ein AI-dreven redaktør. Det du skriv blir automatisk kartlagt til dei rette felta i Sanity Studio når du skriv.',
 
   /** Text for the document pane banner informing users that the document is linked to Sanity Create */
-  'studio-create-link-banner.text': undefined, // 'This document is linked to Sanity Create'
+  'studio-create-link-banner.text': 'Dette dokumentet er lenka til Sanity Create',
 
   /** Unlink document from Sanity Create button text */
-  'unlink-from-create-button.text': undefined, // 'Unlink'
+  'unlink-from-create-button.text': 'Fjern lenke',
 
   /** Unlink from Create dialog: Cancel button text */
-  'unlink-from-create-dialog.cancel.text': undefined, // 'Cancel'
+  'unlink-from-create-dialog.cancel.text': 'Avbryt',
   /** Unlink from Create dialog: Document title used if no other title can be determined */
-  'unlink-from-create-dialog.document.untitled.text': undefined, // 'Untitled'
+  'unlink-from-create-dialog.document.untitled.text': 'Utan tittel',
   /** Unlink from Create dialog – first informational paragraph */
-  'unlink-from-create-dialog.first-paragraph': undefined, // 'You’re unlinking “<strong>{{title}}</strong>” from Sanity Create so it can be edited here.'
+  'unlink-from-create-dialog.first-paragraph':
+    'Du fjernar lenka for “<strong>{{title}}</strong>” frå Sanity Create slik at det kan redigerast her.',
   /** Unlink from Create dialog header */
-  'unlink-from-create-dialog.header': undefined, // 'Switch editing to Studio?'
+  'unlink-from-create-dialog.header': 'Bytte redigering til Studio?',
   /** Unlink from Create dialog – second informational paragraph */
-  'unlink-from-create-dialog.second-paragraph': undefined, // 'You’ll keep your content in both places. Any new changes in Sanity Create will stop syncing to this Studio.'
+  'unlink-from-create-dialog.second-paragraph':
+    'Du vil behalde innhaldet begge stader. Alle nye endringar i Sanity Create vil stoppe å synkronisere til dette Studioet.',
   /** Unlink from Create dialog: Unlink button text */
-  'unlink-from-create-dialog.unlink.text': undefined, // 'Unlink now'
+  'unlink-from-create-dialog.unlink.text': 'Koble frå no',
 })
