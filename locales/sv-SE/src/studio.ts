359f387ec44e76f97fe58a7a6edea6591f784335
AutoTranslator <hello@sanity.io>
2024-12-19 03:11:43 +0000

    fix(sv-SE): automated translation updates

--- locales/sv-SE/src/studio.ts
+++ locales/sv-SE/src/studio.ts
@@ -649,11 +649,13 @@ export default removeUndefinedLocaleResources({
   /** Hotspot & Crop */
   'inputs.imagetool.title': 'Hotspot & Crop',
   /** Warnings displayed to developers when using the crop/hotspot tool on vector images, notifying them that crops/hotspot are not respected when serving the image in vector format. For the crop/hotspot to apply, images must be served in a raster format such as JPG or PNG, by appending eg `fm=jpg` to the image url, or calling `format('jpg')` if using `@sanity/image-url` */
-  'inputs.imagetool.vector-warning.developer-info': undefined, // 'The Asset Pipeline does not support hotspot and crop for vector formats. To enable hotspot & crop, output this image to any of the supported raster formats. For example: <code>fm=jpg</code> to the <ImageUrlDocumentationLink>image URL</ImageUrlDocumentationLink> or call <code>.format(\'png\')</code> with <ImageUrlPackageDocumentationLink>@sanity/image-url</ImageUrlPackageDocumentationLink>.'
+  'inputs.imagetool.vector-warning.developer-info':
+    "Asset Pipeline stöder inte hotspot och beskärning för vektorformat. För att aktivera hotspot & beskärning, exportera denna bild till något av de stödda rasterformaten. Till exempel: <code>fm=jpg</code> till <ImageUrlDocumentationLink>bild-URL</ImageUrlDocumentationLink> eller anropa <code>.format('png')</code> med <ImageUrlPackageDocumentationLink>@sanity/image-url</ImageUrlPackageDocumentationLink>.",
   /** See developer info */
-  'inputs.imagetool.vector-warning.expand-developer-info': undefined, // 'See developer info'
+  'inputs.imagetool.vector-warning.expand-developer-info': 'Se utvecklarinformation',
   /** Gotcha: Serving vector images with hotspot and crop from the Sanity Image API */
-  'inputs.imagetool.vector-warning.title': undefined, // 'Warning: Hotspot and crop might not be applied to this image where it\'s presented.'
+  'inputs.imagetool.vector-warning.title':
+    'Varning: Hotspot och beskärning kanske inte tillämpas på denna bild där den presenteras.',
   /** Convert to `{{targetType}}` */
   'inputs.invalid-value.convert-button.text': 'Konvertera till <code>{{targetType}}</code>',
   /** The current value (<code>`{{actualType}}`</code>) */
