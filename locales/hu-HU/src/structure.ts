d9f324914d4468879f7b651d032d19874eb31fe1
AutoTranslator <hello@sanity.io>
2024-12-19 03:11:43 +0000

    fix(hu-HU): automated translation updates

--- locales/hu-HU/src/structure.ts
+++ locales/hu-HU/src/structure.ts
@@ -172,6 +172,9 @@ export default removeUndefinedLocaleResources({
   /** The toast title when the copy button has been clicked */
   'confirm-delete-dialog.cdr-table.id-copied-toast.title':
     'Dokumentum azonosító másolva a vágólapra!',
+  /** The toast title when the copy button has been clicked but copying failed */
+  'confirm-delete-dialog.cdr-table.id-copied-toast.title-failed':
+    'Nem sikerült a dokumentumazonosító másolása',
   /** The header for the project ID column in the list of cross-dataset references found */
   'confirm-delete-dialog.cdr-table.project-id.label': 'Projekt azonosító',
   /** The text in the "Delete anyway" button in the confirm delete dialog that confirms the action */
