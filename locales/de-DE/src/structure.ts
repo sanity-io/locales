485684e639fcb2c25f7ab954f330a4cedbee2fe5
AutoTranslator <hello@sanity.io>
2024-12-21 03:11:39 +0000

    fix(de-DE): automated translation updates

--- locales/de-DE/src/structure.ts
+++ locales/de-DE/src/structure.ts
@@ -175,6 +175,9 @@ export default removeUndefinedLocaleResources({
   /** The toast title when the copy button has been clicked */
   'confirm-delete-dialog.cdr-table.id-copied-toast.title':
     'Dokument-ID in die Zwischenablage kopiert!',
+  /** The toast title when the copy button has been clicked but copying failed */
+  'confirm-delete-dialog.cdr-table.id-copied-toast.title-failed':
+    'Dokumenten-ID kopieren fehlgeschlagen',
   /** The header for the project ID column in the list of cross-dataset references found */
   'confirm-delete-dialog.cdr-table.project-id.label': 'Projekt-ID',
   /** The text in the "Delete anyway" button in the confirm delete dialog that confirms the action */
