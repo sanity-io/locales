a5320deb19279fd1c7960da7572a944e64d2232b
AutoTranslator <hello@sanity.io>
2024-12-21 03:11:36 +0000

    fix(be-BY): automated translation updates

--- locales/be-BY/src/structure.ts
+++ locales/be-BY/src/structure.ts
@@ -157,6 +157,9 @@ export default removeUndefinedLocaleResources({
   /** The toast title when the copy button has been clicked */
   'confirm-delete-dialog.cdr-table.id-copied-toast.title':
     'ID дакумента скапіраваны ў буфер абмену!',
+  /** The toast title when the copy button has been clicked but copying failed */
+  'confirm-delete-dialog.cdr-table.id-copied-toast.title-failed':
+    'Не атрымалася скапіяваць ідэнтыфікатар дакумента',
   /** The header for the project ID column in the list of cross-dataset references found */
   'confirm-delete-dialog.cdr-table.project-id.label': 'ID праекта',
   /** The text in the "Delete anyway" button in the confirm delete dialog that confirms the action */
