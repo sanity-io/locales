a4b946ba3edd3345044aafe8aeb90a33547fea4e
AutoTranslator <hello@sanity.io>
2025-02-20 20:49:26 +0000

    fix(zh-Hans): automated translation updates

--- locales/zh-Hans/src/structure.ts
+++ locales/zh-Hans/src/structure.ts
@@ -97,7 +97,7 @@ export default removeUndefinedLocaleResources({
   'banners.permission-check-banner.request-permission-button.sent': '编辑请求已发送',
   /** The text for the request permission button that appears for viewer roles */
   'banners.permission-check-banner.request-permission-button.text': '申请编辑权限',
-  /** Description for the archived release banner, rendered when viewing the history of a version document from the publihed view */
+  /** Description for the archived release banner, rendered when viewing the history of a version document from the published view */
   'banners.published-release.description':
     '您正在查看作为<VersionBadge>发布</VersionBadge>的一部分而发布的只读文档。它不能被编辑',
   /** The text for the reload button */
@@ -112,6 +112,9 @@ export default removeUndefinedLocaleResources({
   'banners.release.action.add-to-release': '添加到发布',
   /** The text for the banner that appears when a document is not in the current global release */
   'banners.release.not-in-release': '不在<Label>{{title}}</Label>发布中。',
+  /** The text content for the unpublished document banner when is part of a release */
+  'banners.unpublished-release-banner.text':
+    '此文档将作为<VersionBadge>{{title}}</VersionBadge>发布的一部分而被取消发布',
 
   /** Browser/tab title when creating a new document of a given type */
   'browser-document-title.new-document': '新建 {{schemaType}}',
