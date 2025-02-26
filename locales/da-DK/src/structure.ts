a8f5c9258110db637943af0860606853074d154d
AutoTranslator <hello@sanity.io>
2025-02-26 01:03:13 +0000

    fix(da-DK): automated translation updates

--- locales/da-DK/src/structure.ts
+++ locales/da-DK/src/structure.ts
@@ -182,9 +182,6 @@ export default removeUndefinedLocaleResources({
   'confirm-delete-dialog.cdr-table.dataset.label': 'Datasæt',
   /** The header for the document ID column in the list of cross-dataset references found */
   'confirm-delete-dialog.cdr-table.document-id.label': 'Dokument-ID',
-  /** The toast title when the copy button has been clicked */
-  'confirm-delete-dialog.cdr-table.id-copied-toast.title':
-    'Kopieret dokument-ID til udklipsholder!',
   /** The toast title when the copy button has been clicked but copying failed */
   'confirm-delete-dialog.cdr-table.id-copied-toast.title-failed': 'Kunne ikke kopiere dokument-ID',
   /** The header for the project ID column in the list of cross-dataset references found */
