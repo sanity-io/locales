6e2f6d4f09454c04681d25a55fda80accc022554
AutoTranslator <hello@sanity.io>
2025-03-11 19:17:38 +0000

    fix(ja-JP): automated translation updates

--- locales/ja-JP/src/copy-paste.ts
+++ locales/ja-JP/src/copy-paste.ts
@@ -12,15 +12,6 @@ export default removeUndefinedLocaleResources({
   /** The error message that is shown when the clipboard is not supported */
   'copy-paste.on-copy.validation.clipboard-not-supported.title':
     'お使いのブラウザはこのアクションをサポートしていません',
-  /** The success message that is shown when a document is copied */
-  'copy-paste.on-copy.validation.copy-document-success.title':
-    'ドキュメント "{{fieldNames}}" をコピーしました',
-  /** The success message that is shown when a field is copied */
-  'copy-paste.on-copy.validation.copy-field_one-success.title':
-    'フィールド "{{fieldName}}" をコピーしました',
-  /** The success message that is shown when a array item is copied */
-  'copy-paste.on-copy.validation.copy-item_one-success.title':
-    'アイテム "{{typeName}}" をコピーしました',
   /** The error message that is shown when there is no value to copy */
   'copy-paste.on-copy.validation.no-value.title': '空の値、コピーするものがありません',
   /** --- On copy --- */
@@ -37,12 +28,6 @@ export default removeUndefinedLocaleResources({
   'copy-paste.on-paste.validation.clipboard-empty.title': '貼り付けるものがありません',
   /** The validation message that is shown when the clipboard item is invalid */
   'copy-paste.on-paste.validation.clipboard-invalid.title': '無効なクリップボードアイテムです',
-  /** The success message that is shown when a document is pasted */
-  'copy-paste.on-paste.validation.document-paste-success.title':
-    'ドキュメント "{{fieldNames}}" を更新しました',
-  /** The success message that is shown when a field is pasted */
-  'copy-paste.on-paste.validation.field_one-paste-success.title':
-    'フィールド "{{fieldName}}" を更新しました',
   /** The validation message that is shown when image files are incompatible */
   'copy-paste.on-paste.validation.image-file-incompatible.description':
     '"{{sourceSchemaType}}" は "{{targetSchemaType}}" には許可されていません',
