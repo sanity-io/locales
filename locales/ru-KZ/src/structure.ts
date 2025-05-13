61e286313b752d1db12ac8b02d9287e895fc431b
AutoTranslator <hello@sanity.io>
2025-05-13 18:09:35 +0000

    fix(ru-KZ): automated translation updates

--- locales/ru-KZ/src/structure.ts
+++ locales/ru-KZ/src/structure.ts
@@ -12,7 +12,8 @@ export default removeUndefinedLocaleResources({
   /** Label for the "Delete" document action while the document is being deleted */
   'action.delete.running.label': 'Удаление…',
   /** Tooltip when action is disabled because the document is linked to Canvas */
-  'action.disabled-by-canvas.tooltip': undefined, // 'Some document actions are disabled for documents linked to Canvas'
+  'action.disabled-by-canvas.tooltip':
+    'Некоторые действия с документом отключены для документов, связанных с Canvas',
   /** Message prompting the user to confirm discarding changes */
   'action.discard-changes.confirm-dialog.confirm-discard-changes':
     'Вы уверены, что хотите отменить все изменения с момента последней публикации?',
@@ -169,19 +170,20 @@ export default removeUndefinedLocaleResources({
   'buttons.split-pane-close-group-button.title': 'Закрыть группу панелей',
 
   /** The text for the canvas linked banner action button */
-  'canvas.banner.edit-in-canvas-action': undefined, // 'Edit in Canvas'
+  'canvas.banner.edit-in-canvas-action': 'Редактировать в Canvas',
   /** The text for the canvas linked banner when the document is a draft */
-  'canvas.banner.linked-text.draft': undefined, // 'This draft document is linked to Canvas'
+  'canvas.banner.linked-text.draft': 'Этот черновой документ связан с Canvas',
   /** The text for the canvas linked banner when the document is a live document */
-  'canvas.banner.linked-text.published': undefined, // 'This live document is linked to Canvas'
+  'canvas.banner.linked-text.published': 'Этот опубликованный документ связан с Canvas',
   /** The text for the canvas linked banner when the document is a version document */
-  'canvas.banner.linked-text.version': undefined, // 'This version document is linked to Canvas'
+  'canvas.banner.linked-text.version': 'Этот версионный документ связан с Canvas',
   /** The text for the canvas linked banner popover button */
-  'canvas.banner.popover-button-text': undefined, // 'Learn more'
+  'canvas.banner.popover-button-text': 'Узнать больше',
   /** The description for the canvas linked banner popover */
-  'canvas.banner.popover-description': undefined, // 'Canvas lets you author in a free-form editor that automatically maps back to the Studio as structured content - as you type.'
+  'canvas.banner.popover-description':
+    'Canvas позволяет создавать контент в свободной форме редактора, который автоматически отображается в Studio как структурированный контент - по мере ввода.',
   /** The heading for the canvas linked banner popover */
-  'canvas.banner.popover-heading': undefined, // 'Idea first authoring'
+  'canvas.banner.popover-heading': 'Авторство с идеей на первом месте',
 
   /** The label used in the changes inspector for the from selector */
   'changes.from.label': 'От',
@@ -204,7 +206,7 @@ export default removeUndefinedLocaleResources({
     'Параметр предыдущего документа недействителен.',
 
   /** The text for the tooltip when the "Compare versions" action for a document is disabled */
-  'compare-versions.menu-item.disabled-reason': undefined, // 'There are no other versions of this document to compare.'
+  'compare-versions.menu-item.disabled-reason': 'Нет других версий этого документа для сравнения.',
   /** The text for the "Compare versions" action for a document */
   'compare-versions.menu-item.title': 'Сравнить версии',
   /** The string used to label draft documents */
@@ -331,9 +333,9 @@ export default removeUndefinedLocaleResources({
   'document-view.form-view.sync-lock-toast.title': 'Синхронизация документа…',
 
   /** The description for the document favorite action */
-  'document.favorites.add-to-favorites': undefined, // 'Add to favorites'
+  'document.favorites.add-to-favorites': 'Добавить в избранное',
   /** The description for the document unfavorite action */
-  'document.favorites.remove-from-favorites': undefined, // 'Remove from favorites'
+  'document.favorites.remove-from-favorites': 'Удалить из избранного',
 
   /**The title for the menu items that will be shown when expanding a publish release event to inspect the document */
   'events.inspect.release': 'Просмотреть документ <VersionBadge>{{releaseTitle}}</VersionBadge>',
