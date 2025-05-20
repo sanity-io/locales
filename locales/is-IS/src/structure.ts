56c21b91acf4bf0f7d66380a8105f719a0c56cf2
AutoTranslator <hello@sanity.io>
2025-05-20 21:20:35 +0000

    fix(is-IS): automated translation updates

--- locales/is-IS/src/structure.ts
+++ locales/is-IS/src/structure.ts
@@ -12,7 +12,8 @@ export default removeUndefinedLocaleResources({
   /** Label for the "Delete" document action while the document is being deleted */
   'action.delete.running.label': 'Eyði…',
   /** Tooltip when action is disabled because the document is linked to Canvas */
-  'action.disabled-by-canvas.tooltip': undefined, // 'Some document actions are disabled for documents linked to Canvas'
+  'action.disabled-by-canvas.tooltip':
+    'Sumar aðgerðir skjalsins eru óvirkar fyrir skjöl tengd við Canvas',
   /** Message prompting the user to confirm discarding changes */
   'action.discard-changes.confirm-dialog.confirm-discard-changes':
     'Ertu viss um að þú viljir henda öllum breytingum síðan síðast var birt?',
@@ -178,19 +179,20 @@ export default removeUndefinedLocaleResources({
   'buttons.split-pane-close-group-button.title': 'Loka gluggahópi',
 
   /** The text for the canvas linked banner action button */
-  'canvas.banner.edit-in-canvas-action': undefined, // 'Edit in Canvas'
+  'canvas.banner.edit-in-canvas-action': 'Breyta í Canvas',
   /** The text for the canvas linked banner when the document is a draft */
-  'canvas.banner.linked-text.draft': undefined, // 'This draft document is linked to Canvas'
+  'canvas.banner.linked-text.draft': 'Þetta drög skjalsins er tengt við Canvas',
   /** The text for the canvas linked banner when the document is a live document */
-  'canvas.banner.linked-text.published': undefined, // 'This live document is linked to Canvas'
+  'canvas.banner.linked-text.published': 'Þetta lifandi skjal er tengt við Canvas',
   /** The text for the canvas linked banner when the document is a version document */
-  'canvas.banner.linked-text.version': undefined, // 'This version document is linked to Canvas'
+  'canvas.banner.linked-text.version': 'Þetta útgáfa skjalsins er tengt við Canvas',
   /** The text for the canvas linked banner popover button */
-  'canvas.banner.popover-button-text': undefined, // 'Learn more'
+  'canvas.banner.popover-button-text': 'Lærðu meira',
   /** The description for the canvas linked banner popover */
-  'canvas.banner.popover-description': undefined, // 'Canvas lets you author in a free-form editor that automatically maps back to the Studio as structured content - as you type.'
+  'canvas.banner.popover-description':
+    'Canvas leyfir þér að skrifa í frjálsform ritli sem sjálfkrafa kortleggur aftur til Studio sem skipulagt efni - á meðan þú skrifar.',
   /** The heading for the canvas linked banner popover */
-  'canvas.banner.popover-heading': undefined, // 'Idea first authoring'
+  'canvas.banner.popover-heading': 'Hugmynd fyrst ritun',
 
   /** The label used in the changes inspector for the from selector */
   'changes.from.label': 'Frá',
@@ -212,7 +214,8 @@ export default removeUndefinedLocaleResources({
   'compare-version.error.invalidPreviousDocumentParam': 'Fyrri skjals parameter er ógilt.',
 
   /** The text for the tooltip when the "Compare versions" action for a document is disabled */
-  'compare-versions.menu-item.disabled-reason': undefined, // 'There are no other versions of this document to compare.'
+  'compare-versions.menu-item.disabled-reason':
+    'Það eru engar aðrar útgáfur af þessu skjali til að bera saman.',
   /** The text for the "Compare versions" action for a document */
   'compare-versions.menu-item.title': 'Bera saman útgáfur',
   /** The string used to label draft documents */
@@ -350,9 +353,9 @@ export default removeUndefinedLocaleResources({
   'document-view.form-view.sync-lock-toast.title': 'Samstilli skjal…',
 
   /** The description for the document favorite action */
-  'document.favorites.add-to-favorites': undefined, // 'Add to favorites'
+  'document.favorites.add-to-favorites': 'Bæta við uppáhalds',
   /** The description for the document unfavorite action */
-  'document.favorites.remove-from-favorites': undefined, // 'Remove from favorites'
+  'document.favorites.remove-from-favorites': 'Fjarlægja úr uppáhalds',
 
   /**The title for the menu items that will be shown when expanding a publish release event to inspect the document */
   'events.inspect.release': 'Skoða <VersionBadge>{{releaseTitle}}</VersionBadge> skjal',
@@ -420,10 +423,19 @@ export default removeUndefinedLocaleResources({
   'panes.document-header-title.new.text': 'Nýtt {{schemaType}}',
   /** The text used in the document header title if no other title can be determined */
   'panes.document-header-title.untitled.text': 'Ótitlað',
+  /** The help text saying that we'll retry fetching the document list */
+  'panes.document-list-pane.error.retrying': 'Reyni aftur…',
   /** The error text on the document list pane */
   'panes.document-list-pane.error.text': 'Villa: <Code>{{error}}</Code>',
+  /** The error text on the document list pane */
+  'panes.document-list-pane.error.text.dev': 'Villa: <Code>{{error}}</Code>',
+  /** The error text on the document list pane if the browser appears to be offlline */
+  'panes.document-list-pane.error.text.offline': 'Internet tengingin virðist vera ótengd.',
   /** The error title on the document list pane */
   'panes.document-list-pane.error.title': 'Gat ekki sótt lista yfir atriði',
+  /** The help text saying that we'll retry fetching the document list */
+  'panes.document-list-pane.error.will-retry-automatically_one': 'Reyni aftur…',
+  'panes.document-list-pane.error.will-retry-automatically_other': 'Reyni aftur… (#{{count}}).',
   /** The text of the document list pane if more than a maximum number of documents are returned */
   'panes.document-list-pane.max-items.text': 'Birtir hámark af {{limit}} skjölum',
   /** The text of the document list pane if no documents are found for a specified type */
@@ -432,6 +444,8 @@ export default removeUndefinedLocaleResources({
   'panes.document-list-pane.no-documents.text': 'Engar niðurstöður fundust',
   /** The text of the document list pane if no documents are found matching specified criteria */
   'panes.document-list-pane.no-matching-documents.text': 'Engin passandi skjöl',
+  /** The search input for the search input on the document list pane */
+  'panes.document-list-pane.reconnecting': 'Reyni að tengjast…',
   /** The aria-label for the search input on the document list pane */
   'panes.document-list-pane.search-input.aria-label': 'Leita í lista',
   /** The search input for the search input on the document list pane */
