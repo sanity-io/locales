5621f227b6840d6823e52dbd885a81aa7d0fd56a
AutoTranslator <hello@sanity.io>
2025-07-18 21:42:21 +0000

    fix(hr-HR): automated translation updates

--- locales/hr-HR/src/structure.ts
+++ locales/hr-HR/src/structure.ts
@@ -86,13 +86,17 @@ export default removeUndefinedLocaleResources({
   'banners.archived-release.description':
     'Pregledavate dokument samo za čitanje koji je arhiviran kao dio <VersionBadge> izdanja</VersionBadge>. Ne može se uređivati',
   /** The explanation displayed when a user attempts to create a new draft document, but the draft model is not switched on */
-  'banners.choose-new-document-destination.cannot-create-draft-document': undefined, // 'Cannot create a draft document.'
+  'banners.choose-new-document-destination.cannot-create-draft-document':
+    'Nije moguće stvoriti dokument nacrta.',
   /** The explanation displayed when a user attempts to create a new published document, but the schema type doesn't support live-editing */
-  'banners.choose-new-document-destination.cannot-create-published-document': undefined, // 'Cannot create a published document.'
+  'banners.choose-new-document-destination.cannot-create-published-document':
+    'Nije moguće stvoriti objavljeni dokument.',
   /** The prompt displayed when a user must select a different perspective in order to create a document */
-  'banners.choose-new-document-destination.choose-destination': undefined, // 'Choose a destination for this document:'
+  'banners.choose-new-document-destination.choose-destination':
+    'Odaberite odredište za ovaj dokument:',
   /** The explanation displayed when a user attempts to create a new document in a release, but the selected release is inactive */
-  'banners.choose-new-document-destination.release-inactive': undefined, // 'The <VersionBadge>{{title}}</VersionBadge> release is not active.'
+  'banners.choose-new-document-destination.release-inactive':
+    '<VersionBadge>{{title}}</VersionBadge> izdanje nije aktivno.',
   /** The text for the restore button on the deleted document banner */
   'banners.deleted-document-banner.restore-button.text': 'Vrati najnoviju verziju',
   /** The text content for the deleted document banner */
@@ -107,13 +111,14 @@ export default removeUndefinedLocaleResources({
   'banners.live-edit-draft-banner.text':
     'Tip <strong>{{schemaType}}</strong> ima omogućeno <code>liveEdit</code>, ali postoji skica ovog dokumenta. Objavite ili odbacite skicu kako biste nastavili s uređivanjem uživo.',
   /** The label for the "compare draft" action */
-  'banners.obsolete-draft.actions.compare-draft.text': undefined, // 'Compare draft'
+  'banners.obsolete-draft.actions.compare-draft.text': 'Usporedi nacrt',
   /** The label for the "discard draft" action */
-  'banners.obsolete-draft.actions.discard-draft.text': undefined, // 'Discard draft'
+  'banners.obsolete-draft.actions.discard-draft.text': 'Odbaci nacrt',
   /** The label for the "publish draft" action */
-  'banners.obsolete-draft.actions.publish-draft.text': undefined, // 'Publish draft'
+  'banners.obsolete-draft.actions.publish-draft.text': 'Objavi nacrt',
   /** The warning displayed when editing a document that has an obsolete draft because the draft model is not switched on */
-  'banners.obsolete-draft.draft-model-inactive.text': undefined, // 'The workspace does not have drafts enabled, but a draft version of this document exists.'
+  'banners.obsolete-draft.draft-model-inactive.text':
+    'Radni prostor nema omogućene nacrte, ali postoji nacrt verzija ovog dokumenta.',
   /** The text for the permission check banner if the user only has one role, and it does not allow publishing this document */
   'banners.permission-check-banner.missing-permission_create_one':
     'Vaša uloga <Roles/> nema dozvolu za kreiranje ovog dokumenta.',
@@ -181,7 +186,8 @@ export default removeUndefinedLocaleResources({
   'banners.unpublished-release-banner.text':
     'Ovaj dokument će biti nepublikovan kao dio <VersionBadge>{{title}}</VersionBadge> izdanja',
   /** The text content for the unpublished document banner letting the user know that the current published version is being shown */
-  'banners.unpublished-release-banner.text-with-published': undefined, // 'Showing the current <strong>published</strong> version:'
+  'banners.unpublished-release-banner.text-with-published':
+    'Prikazuje se trenutna <strong>objavljena</strong> verzija:',
 
   /** Browser/tab title when creating a new document of a given type */
   'browser-document-title.new-document': 'Novi {{schemaType}}',
