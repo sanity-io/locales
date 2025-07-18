2e922b9d4272fc84f6f072a1200b16ef0a85b57c
AutoTranslator <hello@sanity.io>
2025-07-18 21:42:21 +0000

    fix(pt-PT): automated translation updates

--- locales/pt-PT/src/studio.ts
+++ locales/pt-PT/src/studio.ts
@@ -1265,13 +1265,17 @@ export default removeUndefinedLocaleResources({
   /** Tooltip message for not having permissions for creating new releases */
   'release.action.permission.error': 'Você não tem permissão para realizar esta ação',
   /** Error message description for when a version is reverted from being unpublished */
-  'release.action.revert-unpublish-version.failure.description': undefined, // 'Please try again or check your connection. The document is still going to be unpublished upon release.'
+  'release.action.revert-unpublish-version.failure.description':
+    'Por favor, tente novamente ou verifique sua conexão. O documento ainda vai estar não publicado ao ser lançado.',
   /** Error message title for when a version is reverted from being unpublished */
-  'release.action.revert-unpublish-version.failure.title': undefined, // 'Failed to revert from setting to unpublish on release.'
+  'release.action.revert-unpublish-version.failure.title':
+    'Falha ao reverter a configuração para não publicar na liberação.',
   /** Action message description for when a version is reverted from being unpublished */
-  'release.action.revert-unpublish-version.success.description': undefined, // 'You can now edit this version.'
+  'release.action.revert-unpublish-version.success.description':
+    'Você agora pode editar esta versão.',
   /** Action message title for when a version is reverted from being unpublished */
-  'release.action.revert-unpublish-version.success.title': undefined, // 'Successfully reverted from setting to unpublish on release.'
+  'release.action.revert-unpublish-version.success.title':
+    'Revertido com sucesso a configuração para não publicar na liberação.',
   /** Error message for when a version is set to be unpublished */
   'release.action.unpublish-version.failure':
     'Falha ao definir a versão para não ser publicada no lançamento',
