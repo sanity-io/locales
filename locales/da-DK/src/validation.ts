58e2ec1eae502df0593a01ac8d5ab980eabd5cb5
AutoTranslator <hello@sanity.io>
2025-07-22 15:53:00 +0000

    fix(da-DK): automated translation updates

--- locales/da-DK/src/validation.ts
+++ locales/da-DK/src/validation.ts
@@ -52,9 +52,9 @@ export default removeUndefinedLocaleResources({
   /** Object is missing a reference to an image asset document in its `asset` field */
   'object.asset-required_image': 'Billede er påkrævet',
   /** Media can't be found in the Media Library */
-  'object.media-not-found': undefined, // 'The asset could not found in the Media Library'
+  'object.media-not-found': 'Aktivet kunne ikke findes i Mediebiblioteket',
   /** Media object is missing a reference to a document in its `_ref` field */
-  'object.not-media-library-asset': undefined, // 'Must be a reference to a Media Library asset'
+  'object.not-media-library-asset': 'Skal være en reference til et aktiv i Mediebiblioteket',
   /** Object is not a reference to a document (eg `{_ref: 'documentId'}`) */
   'object.not-reference': 'Skal være en reference til et dokument',
   /** Object references a document which is not published */
@@ -67,7 +67,7 @@ export default removeUndefinedLocaleResources({
   /** Title for the actual "Validation" panel/feature */
   'panel.title': 'Validering',
   /** Message shown when the validation panel is opened but the document is going to unpublish */
-  'panel.unpublish-message': undefined, // 'Document is going to unpublish, no validation errors are shown'
+  'panel.unpublish-message': 'Dokumentet vil blive afpubliceret, ingen valideringsfejl vises',
 
   /** Slug is an object, but is missing a `current` string property */
   'slug.missing-current': 'Slug skal have en værdi',
