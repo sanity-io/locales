74ee7edfdc4956617fff84ba31569e883cc1973b
AutoTranslator <hello@sanity.io>
2025-11-13 00:50:13 +0000

    fix(ru-KZ): automated translation updates

--- locales/ru-KZ/src/structure.ts
+++ locales/ru-KZ/src/structure.ts
@@ -85,7 +85,7 @@ export default removeUndefinedLocaleResources({
   'banners.archived-release.description':
     'Вы просматриваете документ только для чтения, который был архивирован как часть <VersionBadge>{{title}}</VersionBadge>. Его нельзя редактировать',
   /** Description for the archived scheduled draft banner, rendered when viewing the history of a cardinality one release document */
-  'banners.archived-scheduled-draft.description': undefined, // 'This scheduled draft is archived'
+  'banners.archived-scheduled-draft.description': 'Этот запланированный черновик архивирован',
   /** The explanation displayed when a user attempts to create a new draft document, but the draft model is not switched on */
   'banners.choose-new-document-destination.cannot-create-draft-document':
     'Невозможно создать черновик документа.',
@@ -174,7 +174,8 @@ export default removeUndefinedLocaleResources({
   'banners.revision-not-found.description':
     'Мы не смогли найти выбранную версию документа, пожалуйста, выберите другую запись из списка истории.',
   /** The text content for the scheduled draft override banner */
-  'banners.scheduled-draft-override-banner.text': undefined, // 'A scheduled draft for this document exists. If you publish changes now they will be overwritten when the schedule runs.'
+  'banners.scheduled-draft-override-banner.text':
+    'Для этого документа существует запланированный черновик. Если вы опубликуете изменения сейчас, они будут перезаписаны, когда будет выполнено расписание.',
   /** The text content for the unpublished document banner when is part of a release */
   'banners.unpublished-release-banner.text':
     'Этот документ будет снят с публикации как часть релиза <VersionBadge>{{title}}</VersionBadge>',
@@ -607,5 +608,5 @@ export default removeUndefinedLocaleResources({
   'timeline-item.not-found-release.tooltip': 'Релиз с идентификатором "{{releaseId}}" не найден',
 
   /** The text for the "Inline changes" action, which is used to toggle the visibility of content diffs inside inputs */
-  'toggle-inline-changes.menu-item.title': undefined, // 'Inline changes'
+  'toggle-inline-changes.menu-item.title': 'Изменения в линии',
 })
