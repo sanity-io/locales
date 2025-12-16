1be75e6eae4eda9d152fdae54398e25c3ea48004
AutoTranslator <hello@sanity.io>
2025-12-16 20:55:17 +0000

    fix(nb-NO): automated translation updates

--- locales/nb-NO/src/structure.ts
+++ locales/nb-NO/src/structure.ts
@@ -18,6 +18,11 @@ export default removeUndefinedLocaleResources({
   /** Message prompting the user to confirm discarding changes */
   'action.discard-changes.confirm-dialog.confirm-discard-changes':
     'Er du sikker på at du vil forkaste alle endringer siden forrige gang dette dokumentet ble publisert?',
+  /** Message prompting the user to confirm discarding changes */
+  'action.discard-changes.confirm-dialog.confirm-discard-changes-draft':
+    'Er du sikker på at du vil forkaste alle endringer og slette dette utkastdokumentet?',
+  /**Header for the confirm discard dialog */
+  'action.discard-changes.confirm-dialog.header.text': 'Forkaste endringer?',
   /** Tooltip when action is disabled because the document has no unpublished changes */
   'action.discard-changes.disabled.no-change': 'Dette dokumentet har ingen endringer',
   /** Tooltip when action is disabled because the document is not published */
@@ -171,6 +176,11 @@ export default removeUndefinedLocaleResources({
   /** The text for the banner that appears when there are multiple versions but no drafts or published, more than one extra releases */
   'banners.release.navigate-to-edit-description-multiple_other':
     'Dette dokumentet er en del av <VersionBadge/> utgivelsen og {{count}} flere utgivelser',
+  /** The text for the banner that appears when a document is not part of any release
+   * @deprecated – no longer in use
+   * */
+  'banners.release.navigate-to-edit-description-none':
+    'Dette dokumentet er ikke en del av noen utgivelse',
   /** The text for the banner that appears when a document only has one version but is in a draft or published pinned release */
   'banners.release.navigate-to-edit-description-single':
     'Dette dokumentet er en del av <VersionBadge/> utgivelsen',
@@ -222,6 +232,13 @@ export default removeUndefinedLocaleResources({
 
   /** The text for the canvas linked banner action button */
   'canvas.banner.edit-in-canvas-action': 'Rediger i Canvas',
+  /** The text for the canvas linked banner when the document in editable mode*/
+  'canvas.banner.editable.linked-text': 'Dette dokumentet kan redigeres i Canvas.',
+  /** The description for the canvas linked banner popover in editable mode*/
+  'canvas.banner.editable.popover-description':
+    'Canvas lar deg skrive fritt, og deretter oppdatere innhold i Studio uten manuell kopiering felt for felt.',
+  /** The heading for the canvas linked banner popover in editable mode*/
+  'canvas.banner.editable.popover-heading': 'Fri-form skriving',
   /** The text for the canvas linked banner when the document is a draft */
   'canvas.banner.linked-text.draft': 'Dette utkastet er koblet til Canvas',
   /** The text for the canvas linked banner when the document is a live document */
