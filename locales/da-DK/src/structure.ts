3227ba4120a3c6363174e1ea6ffdb41ffb42f09c
AutoTranslator <hello@sanity.io>
2025-12-16 20:55:17 +0000

    fix(da-DK): automated translation updates

--- locales/da-DK/src/structure.ts
+++ locales/da-DK/src/structure.ts
@@ -18,6 +18,11 @@ export default removeUndefinedLocaleResources({
   /** Message prompting the user to confirm discarding changes */
   'action.discard-changes.confirm-dialog.confirm-discard-changes':
     'Er du sikker på, at du vil forkaste alle ændringer siden sidst publiceret?',
+  /** Message prompting the user to confirm discarding changes */
+  'action.discard-changes.confirm-dialog.confirm-discard-changes-draft':
+    'Er du sikker på, at du vil kassere alle ændringer og slette dette udkastsdokument?',
+  /**Header for the confirm discard dialog */
+  'action.discard-changes.confirm-dialog.header.text': 'Kassér ændringer?',
   /** Tooltip when action is disabled because the document has no unpublished changes */
   'action.discard-changes.disabled.no-change': 'Dette dokument har ingen upublicerede ændringer',
   /** Tooltip when action is disabled because the document is not published */
@@ -172,6 +177,11 @@ export default removeUndefinedLocaleResources({
   /** The text for the banner that appears when there are multiple versions but no drafts or published, more than one extra releases */
   'banners.release.navigate-to-edit-description-multiple_other':
     'Dette dokument er en del af <VersionBadge/> udgivelsen og {{count}} yderligere udgivelser',
+  /** The text for the banner that appears when a document is not part of any release
+   * @deprecated – no longer in use
+   * */
+  'banners.release.navigate-to-edit-description-none':
+    'Dette dokument er ikke en del af nogen udgivelse',
   /** The text for the banner that appears when a document only has one version but is in a draft or published pinned release */
   'banners.release.navigate-to-edit-description-single':
     'Dette dokument er en del af <VersionBadge/> udgivelsen',
@@ -204,6 +214,14 @@ export default removeUndefinedLocaleResources({
   'buttons.action-menu-button.aria-label': 'Åbn dokumenthandlinger',
   /** The action menu button tooltip */
   'buttons.action-menu-button.tooltip': 'Dokumenthandlinger',
+  /** The aria-label for the collapse pane button on the document panel header */
+  'buttons.focus-pane-button.aria-label.collapse': 'Forlad fokusmodus (vis navigation)',
+  /** The aria-label for the focus pane button on the document panel header */
+  'buttons.focus-pane-button.aria-label.focus': 'Gå ind i fokusmodus (skjul navigation)',
+  /** The tooltip for the collapse pane button on the document panel header */
+  'buttons.focus-pane-button.tooltip.collapse': 'Forlad fokusmodus',
+  /** The tooltip for the focus pane button on the document panel header */
+  'buttons.focus-pane-button.tooltip.focus': 'Gå ind i fokusmodus',
   /** The aria-label for the split pane button on the document panel header */
   'buttons.split-pane-button.aria-label': 'Opdel rude til højre',
   /** The tool tip for the split pane button on the document panel header */
@@ -215,6 +233,13 @@ export default removeUndefinedLocaleResources({
 
   /** The text for the canvas linked banner action button */
   'canvas.banner.edit-in-canvas-action': 'Rediger i Canvas',
+  /** The text for the canvas linked banner when the document in editable mode*/
+  'canvas.banner.editable.linked-text': 'Dette dokument kan redigeres i Canvas.',
+  /** The description for the canvas linked banner popover in editable mode*/
+  'canvas.banner.editable.popover-description':
+    'Canvas lader dig skrive frit og derefter opdatere indhold i Studio uden manuel kopiering felt for felt.',
+  /** The heading for the canvas linked banner popover in editable mode*/
+  'canvas.banner.editable.popover-heading': 'Frit formuleret skrivning',
   /** The text for the canvas linked banner when the document is a draft */
   'canvas.banner.linked-text.draft': 'Dette udkastdokument er linket til Canvas',
   /** The text for the canvas linked banner when the document is a live document */
