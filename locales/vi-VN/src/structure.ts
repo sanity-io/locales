f57ea27cd7d8b0115e50f67b09aa5651c65081f3
AutoTranslator <hello@sanity.io>
2026-03-04 09:11:41 +0000

    fix(vi-VN): automated translation updates

--- locales/vi-VN/src/structure.ts
+++ locales/vi-VN/src/structure.ts
@@ -1,8 +1,12 @@
 import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
-  /** Label for the "Copy Document URL" document action */
+  /** Label for the "Copy document ID" menu item */
+  'action.copy-document-id.label': 'Sao chép ID tài liệu',
+  /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': 'Sao chép URL Tài liệu',
+  /** Label for the "Copy document URL" menu item */
+  'action.copy-link-to-document.label': 'Sao chép URL tài liệu',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'Thao tác chưa sẵn sàng',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -548,6 +552,9 @@ export default removeUndefinedLocaleResources({
   /** The text when a generic operation succeeded (fallback, generally not shown)  */
   'panes.document-operation-results.operation-success':
     'Đã thực hiện thành công {{context}} trên tài liệu',
+  /** The text when copy document ID operation succeeded */
+  'panes.document-operation-results.operation-success_copy-id':
+    'ID tài liệu đã được sao chép vào bảng tạm',
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url':
     'URL Tài liệu đã được sao chép vào bảng tạm',
