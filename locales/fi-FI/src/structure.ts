f1a6bdddaa7dc895cc71be5b1275b48db6f5bb10
AutoTranslator <hello@sanity.io>
2026-03-04 09:09:42 +0000

    fix(fi-FI): automated translation updates

--- locales/fi-FI/src/structure.ts
+++ locales/fi-FI/src/structure.ts
@@ -1,8 +1,12 @@
 import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
-  /** Label for the "Copy Document URL" document action */
+  /** Label for the "Copy document ID" menu item */
+  'action.copy-document-id.label': 'Kopioi dokumentin ID',
+  /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': 'Kopioi dokumentin URL',
+  /** Label for the "Copy document URL" menu item */
+  'action.copy-link-to-document.label': 'Kopioi dokumentin URL',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'Toiminto ei ole valmis',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -576,6 +580,9 @@ export default removeUndefinedLocaleResources({
   /** The text when a generic operation succeeded (fallback, generally not shown)  */
   'panes.document-operation-results.operation-success':
     'Toiminto {{context}} suoritettu onnistuneesti dokumentille',
+  /** The text when copy document ID operation succeeded */
+  'panes.document-operation-results.operation-success_copy-id':
+    'Dokumentin ID kopioitu leikepöydälle',
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url':
     'Dokumentin URL kopioitu leikepöydälle',
