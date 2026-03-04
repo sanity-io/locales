627ebe5a1a007c33cf7254bb1a961effac506476
AutoTranslator <hello@sanity.io>
2026-03-04 09:33:48 +0000

    fix(kn-IN): automated translation updates

---
+++ locales/kn-IN/src/media-library.ts
@@ -0,0 +1,16 @@
+import {removeUndefinedLocaleResources} from 'sanity'
+
+export default removeUndefinedLocaleResources({
+  /** Warning description for invalid video value */
+  'invalid-video-warning.description':
+    'ಈ ಕ್ಷೇತ್ರದ ಮೌಲ್ಯವು ಮಾನ್ಯ ವೀಡಿಯೊ ಅಲ್ಲ. ಈ ಕ್ಷೇತ್ರವನ್ನು ಮರುಹೊಂದಿಸಿದರೆ ನೀವು ಹೊಸ ವೀಡಿಯೊ ಆಯ್ಕೆ ಮಾಡಬಹುದು.',
+  /** Reset button text for invalid video warning */
+  'invalid-video-warning.reset-button.text': 'ಮೌಲ್ಯವನ್ನು ಮರುಹೊಂದಿಸಿ',
+  /** Warning title for invalid video value */
+  'invalid-video-warning.title': 'ಅಮಾನ್ಯ ವೀಡಿಯೊ ಮೌಲ್ಯ',
+
+  /** Video error fallback description */
+  'video-error.description': 'ವೀಡಿಯೊ ಲೋಡ್ ಮಾಡಲು ವಿಫಲವಾಗಿದೆ',
+  /** Retry button text for video loading errors */
+  'video-error.retry-button.text': 'ಮರುಪ್ರಯತ್ನಿಸಿ',
+})
