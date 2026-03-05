0ac42c4a0cf5e84dff99372fb8da156a2540d989
AutoTranslator <hello@sanity.io>
2026-03-05 12:47:51 +0000

    fix(tr-TR): automated translation updates

--- locales/tr-TR/src/structure.ts
+++ locales/tr-TR/src/structure.ts
@@ -2,11 +2,11 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Label for the "Copy document ID" menu item */
-  'action.copy-document-id.label': undefined, // 'Copy document ID'
+  'action.copy-document-id.label': "Doküman ID'sini kopyala",
   /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': "Belge URL'sini Kopyala",
   /** Label for the "Copy document URL" menu item */
-  'action.copy-link-to-document.label': undefined, // 'Copy document URL'
+  'action.copy-link-to-document.label': "Doküman URL'sini kopyala",
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'İşlem hazır değil',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -574,7 +574,7 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success':
     'Belge üzerinde {{context}} başarıyla gerçekleştirildi',
   /** The text when copy document ID operation succeeded */
-  'panes.document-operation-results.operation-success_copy-id': undefined, // 'Document ID copied to clipboard'
+  'panes.document-operation-results.operation-success_copy-id': "Doküman ID'si panoya kopyalandı",
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url': "Belge URL'si panoya kopyalandı",
   /**  */
