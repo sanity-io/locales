1d5c7a51957758d9a233ba97d2fa54f350b0e876
AutoTranslator <hello@sanity.io>
2026-03-05 12:47:51 +0000

    fix(ro-RO): automated translation updates

--- locales/ro-RO/src/structure.ts
+++ locales/ro-RO/src/structure.ts
@@ -2,11 +2,11 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Label for the "Copy document ID" menu item */
-  'action.copy-document-id.label': undefined, // 'Copy document ID'
+  'action.copy-document-id.label': 'Copiați ID-ul documentului',
   /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': 'Copiază URL-ul documentului',
   /** Label for the "Copy document URL" menu item */
-  'action.copy-link-to-document.label': undefined, // 'Copy document URL'
+  'action.copy-link-to-document.label': 'Copiați URL-ul documentului',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'Operațiunea nu este pregătită',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -556,7 +556,8 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success':
     'Operațiunea {{op}} a fost efectuată cu succes pe document',
   /** The text when copy document ID operation succeeded */
-  'panes.document-operation-results.operation-success_copy-id': undefined, // 'Document ID copied to clipboard'
+  'panes.document-operation-results.operation-success_copy-id':
+    'ID-ul documentului a fost copiat în clipboard',
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url':
     'URL-ul documentului a fost copiat în clipboard',
