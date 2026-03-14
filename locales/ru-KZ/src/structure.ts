8f3cbb6959d919ebab90c012d24581ef119bcdd0
AutoTranslator <hello@sanity.io>
2026-03-14 19:15:22 +0000

    fix(ru-KZ): automated translation updates

--- locales/ru-KZ/src/structure.ts
+++ locales/ru-KZ/src/structure.ts
@@ -2,11 +2,11 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Label for the "Copy document ID" menu item */
-  'action.copy-document-id.label': undefined, // 'Copy document ID'
+  'action.copy-document-id.label': 'Құжат идентификаторын көшіру',
   /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': 'Скопировать URL документа',
   /** Label for the "Copy document URL" menu item */
-  'action.copy-link-to-document.label': undefined, // 'Copy document URL'
+  'action.copy-link-to-document.label': 'Құжат URL мекенжайын көшіру',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'Операция не готова',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -554,7 +554,8 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success':
     'Успешно выполнено {{context}} над документом',
   /** The text when copy document ID operation succeeded */
-  'panes.document-operation-results.operation-success_copy-id': undefined, // 'Document ID copied to clipboard'
+  'panes.document-operation-results.operation-success_copy-id':
+    'Құжат идентификаторы алмасу буферіне көшірілді',
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url':
     'URL документа скопирован в буфер обмена',
