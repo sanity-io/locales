90c0fd6133f72c2702dd37c26c5e86a9a05cc702
AutoTranslator <hello@sanity.io>
2026-03-14 19:14:57 +0000

    fix(it-IT): automated translation updates

--- locales/it-IT/src/structure.ts
+++ locales/it-IT/src/structure.ts
@@ -2,11 +2,11 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Label for the "Copy document ID" menu item */
-  'action.copy-document-id.label': undefined, // 'Copy document ID'
+  'action.copy-document-id.label': 'Copia ID documento',
   /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': 'Copia URL documento',
   /** Label for the "Copy document URL" menu item */
-  'action.copy-link-to-document.label': undefined, // 'Copy document URL'
+  'action.copy-link-to-document.label': 'Copia URL documento',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'Operazione non pronta',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -585,7 +585,8 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success':
     'Operazione su documento {{context}} eseguita con successo',
   /** The text when copy document ID operation succeeded */
-  'panes.document-operation-results.operation-success_copy-id': undefined, // 'Document ID copied to clipboard'
+  'panes.document-operation-results.operation-success_copy-id':
+    'ID documento copiato negli appunti',
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url':
     'URL del documento copiato negli appunti',
