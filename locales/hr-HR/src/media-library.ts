bffb8363491c909591bfb70658654a828043b2f8
AutoTranslator <hello@sanity.io>
2026-03-14 19:14:53 +0000

    fix(hr-HR): automated translation updates

--- locales/hr-HR/src/media-library.ts
+++ locales/hr-HR/src/media-library.ts
@@ -2,14 +2,15 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Warning description for invalid video value */
-  'invalid-video-warning.description': undefined, // 'The value of this field is not a valid video. Resetting this field will let you choose a new video.'
+  'invalid-video-warning.description':
+    'Vrijednost ovog polja nije valjani video. Resetiranjem ovog polja moći ćete odabrati novi video.',
   /** Reset button text for invalid video warning */
-  'invalid-video-warning.reset-button.text': undefined, // 'Reset value'
+  'invalid-video-warning.reset-button.text': 'Resetiraj vrijednost',
   /** Warning title for invalid video value */
-  'invalid-video-warning.title': undefined, // 'Invalid video value'
+  'invalid-video-warning.title': 'Nevaljana vrijednost videa',
 
   /** Video error fallback description */
-  'video-error.description': undefined, // 'Failed to load video'
+  'video-error.description': 'Učitavanje videa nije uspjelo',
   /** Retry button text for video loading errors */
-  'video-error.retry-button.text': undefined, // 'Retry'
+  'video-error.retry-button.text': 'Pokušaj ponovo',
 })
