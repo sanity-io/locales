442b7001b0f44a3df0a30d842b271e873df13561
AutoTranslator <hello@sanity.io>
2026-03-14 19:14:54 +0000

    fix(hu-HU): automated translation updates

--- locales/hu-HU/src/structure.ts
+++ locales/hu-HU/src/structure.ts
@@ -2,11 +2,11 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Label for the "Copy document ID" menu item */
-  'action.copy-document-id.label': undefined, // 'Copy document ID'
+  'action.copy-document-id.label': 'Dokumentum azonosító másolása',
   /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': 'Dokumentum URL másolása',
   /** Label for the "Copy document URL" menu item */
-  'action.copy-link-to-document.label': undefined, // 'Copy document URL'
+  'action.copy-link-to-document.label': 'Dokumentum URL másolása',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'A művelet még nincs kész',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -585,7 +585,8 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success':
     'Sikeresen végrehajtott {{context}} a dokumentumon',
   /** The text when copy document ID operation succeeded */
-  'panes.document-operation-results.operation-success_copy-id': undefined, // 'Document ID copied to clipboard'
+  'panes.document-operation-results.operation-success_copy-id':
+    'A dokumentum azonosítója a vágólapra másolva',
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url':
     'A dokumentum URL másolva a vágólapra',
