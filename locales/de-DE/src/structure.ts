83e03f0e38fe45c7efee0a9acffac75f7f3d5af2
AutoTranslator <hello@sanity.io>
2026-03-17 22:55:36 +0000

    fix(de-DE): automated translation updates

--- locales/de-DE/src/structure.ts
+++ locales/de-DE/src/structure.ts
@@ -2,11 +2,11 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Label for the "Copy document ID" menu item */
-  'action.copy-document-id.label': undefined, // 'Copy document ID'
+  'action.copy-document-id.label': 'Dokument-ID kopieren',
   /** Tooltip for the copy actions dropdown button in the document panel header */
   'action.copy-document-url.label': 'Dokumenten-URL kopieren',
   /** Label for the "Copy document URL" menu item */
-  'action.copy-link-to-document.label': undefined, // 'Copy document URL'
+  'action.copy-link-to-document.label': 'Dokument-URL kopieren',
   /** Tooltip when action button is disabled because the operation is not ready   */
   'action.delete.disabled.not-ready': 'Vorgang nicht bereit',
   /** Tooltip when action button is disabled because the document does not exist */
@@ -590,7 +590,8 @@ export default removeUndefinedLocaleResources({
   'panes.document-operation-results.operation-success':
     '{{context}} am Dokument erfolgreich durchgeführt',
   /** The text when copy document ID operation succeeded */
-  'panes.document-operation-results.operation-success_copy-id': undefined, // 'Document ID copied to clipboard'
+  'panes.document-operation-results.operation-success_copy-id':
+    'Dokument-ID in die Zwischenablage kopiert',
   /** The text when copy URL operation succeeded  */
   'panes.document-operation-results.operation-success_copy-url':
     'Dokumenten-URL in die Zwischenablage kopiert',
