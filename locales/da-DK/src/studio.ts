ace2ea10316274751a508f342bb43db5dfde2c77
AutoTranslator <hello@sanity.io>
2026-03-23 08:38:28 +0000

    fix(da-DK): automated translation updates

--- locales/da-DK/src/studio.ts
+++ locales/da-DK/src/studio.ts
@@ -458,6 +458,26 @@ export default removeUndefinedLocaleResources({
   /** Title for the default ordering/SortOrder if no orderings are provided and the title field is found */
   'default-orderings.title': 'Sortér efter titel',
 
+  /** Label for action that closes divergence inspector */
+  'divergence.action.close.label': 'Luk',
+  /** Label for action that marks divergence as resolved */
+  'divergence.action.markResolved.label': 'Ignorer',
+  /** Label for action that moves inspector to the next divergence in the document */
+  'divergence.action.next.label': 'Næste',
+  /** Label for action that moves inspector to the previous divergence in the document */
+  'divergence.action.previous.label': 'Forrige',
+  /** Label for action that replaces the node's value in the current version with its latest value in the upstream version */
+  'divergence.action.takeFromUpstream.label': 'Kopiér fra base',
+  /** Verb to describe the node's value changed */
+  'divergence.effect.changed': 'ændret',
+  /** Summary of the change that occurred */
+  'divergence.effect.summary': '{{title}} {{effect}} i {{versionName}}-versionen',
+  /** Label for divergence in a single node */
+  'divergence.unresolved-divergence_one': 'Uløst ændring i {{versionName}}-versionen',
+  /** Label for divergences in multiple nodes */
+  'divergence.unresolved-divergence_other':
+    '{{count}} uløste ændringer i {{versionName}}-versionen',
+
   /** Label to show in the document footer indicating the creation date of the document */
   'document-status.created': 'Oprettet {{date}}',
   /** Label to show in the document status indicating the date of the status */
