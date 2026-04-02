717b617ca2240c0efcd2445e2a796b806986ded0
AutoTranslator <hello@sanity.io>
2026-04-02 00:39:58 +0000

    fix(ka-GE): automated translation updates

--- locales/ka-GE/src/studio.ts
+++ locales/ka-GE/src/studio.ts
@@ -459,6 +459,26 @@ export default removeUndefinedLocaleResources({
   /** Title for the default ordering/SortOrder if no orderings are provided and the title field is found */
   'default-orderings.title': 'დალაგება სათაურის მიხედვით',
 
+  /** Label for action that closes divergence inspector */
+  'divergence.action.close.label': 'დახურვა',
+  /** Label for action that marks divergence as resolved */
+  'divergence.action.markResolved.label': 'იგნორირება',
+  /** Label for action that moves inspector to the next divergence in the document */
+  'divergence.action.next.label': 'შემდეგი',
+  /** Label for action that moves inspector to the previous divergence in the document */
+  'divergence.action.previous.label': 'წინა',
+  /** Label for action that replaces the node's value in the current version with its latest value in the upstream version */
+  'divergence.action.takeFromUpstream.label': 'კოპირება ბაზიდან',
+  /** Verb to describe the node's value changed */
+  'divergence.effect.changed': 'შეიცვალა',
+  /** Summary of the change that occurred */
+  'divergence.effect.summary': '{{title}} {{effect}} {{versionName}} ვერსიაში',
+  /** Label for divergence in a single node */
+  'divergence.unresolved-divergence_one': 'გადაუჭრელი ცვლილება {{versionName}} ვერსიაში',
+  /** Label for divergences in multiple nodes */
+  'divergence.unresolved-divergence_other':
+    '{{count}} გადაუჭრელი ცვლილება {{versionName}} ვერსიაში',
+
   /** Label to show in the document footer indicating the creation date of the document */
   'document-status.created': 'შეიქმნა {{date}}',
   /** Label to show in the document status indicating the date of the status */
