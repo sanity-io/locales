ec76e84d618047cde65c45ee6641acbf08cbfd59
AutoTranslator <hello@sanity.io>
2026-05-11 07:58:19 +0000

    fix(is-IS): automated translation updates

--- locales/is-IS/src/studio.ts
+++ locales/is-IS/src/studio.ts
@@ -462,23 +462,24 @@ export default removeUndefinedLocaleResources({
   'default-orderings.title': 'Raða eftir Titli',
 
   /** Label for action that closes divergence inspector */
-  'divergence.action.close.label': undefined, // 'Close'
+  'divergence.action.close.label': 'Loka',
   /** Label for action that marks divergence as resolved */
-  'divergence.action.markResolved.label': undefined, // 'Ignore'
+  'divergence.action.markResolved.label': 'Hunsa',
   /** Label for action that moves inspector to the next divergence in the document */
-  'divergence.action.next.label': undefined, // 'Next'
+  'divergence.action.next.label': 'Næsta',
   /** Label for action that moves inspector to the previous divergence in the document */
-  'divergence.action.previous.label': undefined, // 'Previous'
+  'divergence.action.previous.label': 'Fyrra',
   /** Label for action that replaces the node's value in the current version with its latest value in the upstream version */
-  'divergence.action.takeFromUpstream.label': undefined, // 'Copy from base'
+  'divergence.action.takeFromUpstream.label': 'Afrita frá grunni',
   /** Verb to describe the node's value changed */
-  'divergence.effect.changed': undefined, // 'changed'
+  'divergence.effect.changed': 'breyttist',
   /** Summary of the change that occurred */
-  'divergence.effect.summary': undefined, // '{{title}} {{effect}} in {{versionName}} version'
+  'divergence.effect.summary': '{{title}} {{effect}} í {{versionName}} útgáfu',
   /** Label for divergence in a single node */
-  'divergence.unresolved-divergence_one': undefined, // 'Unresolved change in {{versionName}} version'
+  'divergence.unresolved-divergence_one': 'Óleyst breyting í {{versionName}} útgáfu',
   /** Label for divergences in multiple nodes */
-  'divergence.unresolved-divergence_other': undefined, // '{{count}} unresolved changes in {{versionName}} version'
+  'divergence.unresolved-divergence_other':
+    '{{count}} óleystar breytingar í {{versionName}} útgáfu',
 
   /** Label to show in the document footer indicating the creation date of the document */
   'document-status.created': 'Búið til {{date}}',
