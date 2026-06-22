fe641108e57669f05f20df3eb1de27968b74bcec
AutoTranslator <hello@sanity.io>
2026-06-22 15:22:53 +0000

    fix(hu-HU): automated translation updates

--- locales/hu-HU/src/structure.ts
+++ locales/hu-HU/src/structure.ts
@@ -174,7 +174,7 @@ export default removeUndefinedLocaleResources({
   'banners.reference-changed-banner.reason-removed.text':
     'Ez a hivatkozás eltávolításra került, mióta megnyitottad.',
   /** The text that appears for the action button to add the current document to the global bundle, this happens when user is viewing an anonymous bundle */
-  'banners.release.action.add-to-bundle': undefined, // 'Add to bundle'
+  'banners.release.action.add-to-bundle': 'Hozzáadás a bundle-höz',
   /** The text that appears for the action button to add the current document to the global release */
   'banners.release.action.add-to-release': 'Hozzáadás a kiadáshoz',
   /** The text that appears for the action button to add the current document to the global release */
@@ -483,7 +483,7 @@ export default removeUndefinedLocaleResources({
     'Ehhez a típushoz nem definiáltak bejövő hivatkozásokat, további információkért lásd a dokumentációt.',
 
   /** The text shown if there are no incoming references in the inspector */
-  'incoming-references-pane.no-references': undefined, // 'No incoming references found.'
+  'incoming-references-pane.no-references': 'Nem található bejövő hivatkozás.',
   /** The text shown if there are no incoming references for a type */
   'incoming-references-pane.no-references-found': 'Ebben a típusban nem találhatók hivatkozások.',
   /** The text shown if there is no schema type found for a document in the incoming references pane */
@@ -576,7 +576,8 @@ export default removeUndefinedLocaleResources({
   /** The search input for the search input on the document list pane */
   'panes.document-list-pane.search-input.placeholder': 'Lista keresése',
   /** The tooltip text shown when a sort menu item references fields not present in the current schema */
-  'panes.document-list-pane.sort-order.disabled-reason': undefined, // 'This sorting option uses fields that are not part of this document type'
+  'panes.document-list-pane.sort-order.disabled-reason':
+    'Ez a rendezési lehetőség olyan mezőket használ, amelyek nem részei ennek a dokumentumtípusnak',
   /** The summary title when displaying an error for a document operation result */
   'panes.document-operation-results.error.summary.title': 'Részletek',
   /** The text when a generic operation failed (fallback, generally not shown)  */
@@ -610,7 +611,8 @@ export default removeUndefinedLocaleResources({
   /** The text when a publish operation succeeded  */
   'panes.document-operation-results.operation-success_publish': 'A dokumentum közzétéve',
   /** The text when a publish operation succeeded in an anonymous version */
-  'panes.document-operation-results.operation-success_publishVersion': undefined, // 'Version was published'
+  'panes.document-operation-results.operation-success_publishVersion':
+    'A verzió közzétételre került',
   /** The text when a restore operation succeeded  */
   'panes.document-operation-results.operation-success_restore':
     '<Strong>{{title}}</Strong> visszaállítása megtörtént',
