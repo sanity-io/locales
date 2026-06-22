7fc33b1eed29658f5771d5738a8e0b1d9d3c32b3
AutoTranslator <hello@sanity.io>
2026-06-22 14:34:17 +0000

    fix(vi-VN): automated translation updates

--- locales/vi-VN/src/studio.ts
+++ locales/vi-VN/src/studio.ts
@@ -405,7 +405,8 @@ export default removeUndefinedLocaleResources({
   /** Title for the Review Changes pane */
   'changes.title': 'Xem lại các thay đổi',
   /** Shown above raw JSON diff for document fields that are not defined in the schema */
-  'changes.unknown-schema-field.description': undefined, // 'This field is not defined in the schema. Values are shown as JSON.'
+  'changes.unknown-schema-field.description':
+    'Trường này không được định nghĩa trong schema. Các giá trị được hiển thị dưới dạng JSON.',
   /**The title that will be shown in the badge inside the events when the item is a draft */
   'changes.versions.draft': 'Bản nháp',
 
