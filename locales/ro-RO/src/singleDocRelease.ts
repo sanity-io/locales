237c558b7f74a2aa822c106bcc9cb2115ebffca3
AutoTranslator <hello@sanity.io>
2026-06-22 14:33:59 +0000

    fix(ro-RO): automated translation updates

--- locales/ro-RO/src/singleDocRelease.ts
+++ locales/ro-RO/src/singleDocRelease.ts
@@ -2,22 +2,25 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Action text for scheduling publish of a draft document */
-  'action.schedule-publish': undefined, // 'Schedule publish'
+  'action.schedule-publish': 'Programează publicarea',
   /** Tooltip text for when schedule publish fails */
-  'action.schedule-publish-error': undefined, // 'Failed to schedule publishing'
+  'action.schedule-publish-error': 'Programarea publicării a eșuat',
   /** Tooltip text for when a document is scheduled for publishing */
-  'action.schedule-publish-success': undefined, // 'Document scheduled for publishing'
+  'action.schedule-publish-success': 'Document programat pentru publicare',
   /** Tooltip description for when a document is scheduled for publishing */
-  'action.schedule-publish-success-description': undefined, // 'Publishing scheduled for {{publishAt}}'
+  'action.schedule-publish-success-description': 'Publicare programată pentru {{publishAt}}',
   /** Tooltip text for when schedule publish is disabled due to cardinality one releases */
-  'action.schedule-publish.disabled.cardinality-one': undefined, // 'A Scheduled Draft for this document already exists.'
+  'action.schedule-publish.disabled.cardinality-one':
+    'Un Draft Programat pentru acest document există deja.',
   /** Tooltip text for when schedule publish is disabled due to validation errors */
-  'action.schedule-publish.disabled.validation-issues': undefined, // 'Cannot Schedule Draft due to validation errors in the current draft.'
+  'action.schedule-publish.disabled.validation-issues':
+    'Nu se poate programa draft-ul din cauza erorilor de validare din draft-ul curent.',
 
   /** Empty state action documentation for scheduled drafts */
-  'empty-state.action.documentation': undefined, // 'Learn about scheduling'
+  'empty-state.action.documentation': 'Aflați despre programare',
   /** Empty state description for scheduled drafts */
-  'empty-state.description': undefined, // 'Schedule and lock draft documents for publishing at a future date and time.'
+  'empty-state.description':
+    'Programați și blocați documentele draft pentru publicare la o dată și oră viitoare.',
   /** Empty state title for scheduled drafts */
-  'empty-state.title': undefined, // 'Scheduled Drafts'
+  'empty-state.title': 'Draft-uri Programate',
 })
