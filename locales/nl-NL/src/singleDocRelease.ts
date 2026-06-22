566b88793b879935573e025b7e9e3d2322512809
AutoTranslator <hello@sanity.io>
2026-06-22 17:40:02 +0000

    fix(nl-NL): automated translation updates

--- locales/nl-NL/src/singleDocRelease.ts
+++ locales/nl-NL/src/singleDocRelease.ts
@@ -2,22 +2,25 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Action text for scheduling publish of a draft document */
-  'action.schedule-publish': undefined, // 'Schedule publish'
+  'action.schedule-publish': 'Publicatie inplannen',
   /** Tooltip text for when schedule publish fails */
-  'action.schedule-publish-error': undefined, // 'Failed to schedule publishing'
+  'action.schedule-publish-error': 'Inplannen van publicatie mislukt',
   /** Tooltip text for when a document is scheduled for publishing */
-  'action.schedule-publish-success': undefined, // 'Document scheduled for publishing'
+  'action.schedule-publish-success': 'Document ingepland voor publicatie',
   /** Tooltip description for when a document is scheduled for publishing */
-  'action.schedule-publish-success-description': undefined, // 'Publishing scheduled for {{publishAt}}'
+  'action.schedule-publish-success-description': 'Publicatie ingepland voor {{publishAt}}',
   /** Tooltip text for when schedule publish is disabled due to cardinality one releases */
-  'action.schedule-publish.disabled.cardinality-one': undefined, // 'A Scheduled Draft for this document already exists.'
+  'action.schedule-publish.disabled.cardinality-one':
+    'Er bestaat al een ingepland concept voor dit document.',
   /** Tooltip text for when schedule publish is disabled due to validation errors */
-  'action.schedule-publish.disabled.validation-issues': undefined, // 'Cannot Schedule Draft due to validation errors in the current draft.'
+  'action.schedule-publish.disabled.validation-issues':
+    'Kan concept niet inplannen vanwege validatiefouten in het huidige concept.',
 
   /** Empty state action documentation for scheduled drafts */
-  'empty-state.action.documentation': undefined, // 'Learn about scheduling'
+  'empty-state.action.documentation': 'Meer informatie over inplannen',
   /** Empty state description for scheduled drafts */
-  'empty-state.description': undefined, // 'Schedule and lock draft documents for publishing at a future date and time.'
+  'empty-state.description':
+    'Plan conceptdocumenten in en vergrendel ze voor publicatie op een toekomstige datum en tijd.',
   /** Empty state title for scheduled drafts */
-  'empty-state.title': undefined, // 'Scheduled Drafts'
+  'empty-state.title': 'Ingeplande concepten',
 })
