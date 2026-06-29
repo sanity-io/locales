a18652705ec1abe5e8a602852b4d771c353036e0
AutoTranslator <hello@sanity.io>
2026-06-29 13:27:18 +0000

    fix(be-BY): automated translation updates

--- locales/be-BY/src/singleDocRelease.ts
+++ locales/be-BY/src/singleDocRelease.ts
@@ -2,22 +2,25 @@ import {removeUndefinedLocaleResources} from 'sanity'
 
 export default removeUndefinedLocaleResources({
   /** Action text for scheduling publish of a draft document */
-  'action.schedule-publish': undefined, // 'Schedule publish'
+  'action.schedule-publish': 'Запланаваць публікацыю',
   /** Tooltip text for when schedule publish fails */
-  'action.schedule-publish-error': undefined, // 'Failed to schedule publishing'
+  'action.schedule-publish-error': 'Не ўдалося запланаваць публікацыю',
   /** Tooltip text for when a document is scheduled for publishing */
-  'action.schedule-publish-success': undefined, // 'Document scheduled for publishing'
+  'action.schedule-publish-success': 'Дакумент запланаваны для публікацыі',
   /** Tooltip description for when a document is scheduled for publishing */
-  'action.schedule-publish-success-description': undefined, // 'Publishing scheduled for {{publishAt}}'
+  'action.schedule-publish-success-description': 'Публікацыя запланавана на {{publishAt}}',
   /** Tooltip text for when schedule publish is disabled due to cardinality one releases */
-  'action.schedule-publish.disabled.cardinality-one': undefined, // 'A Scheduled Draft for this document already exists.'
+  'action.schedule-publish.disabled.cardinality-one':
+    'Запланаваны чарнавік для гэтага дакумента ўжо існуе.',
   /** Tooltip text for when schedule publish is disabled due to validation errors */
-  'action.schedule-publish.disabled.validation-issues': undefined, // 'Cannot Schedule Draft due to validation errors in the current draft.'
+  'action.schedule-publish.disabled.validation-issues':
+    'Немагчыма запланаваць чарнавік з-за памылак валідацыі ў бягучым чарнавіку.',
 
   /** Empty state action documentation for scheduled drafts */
-  'empty-state.action.documentation': undefined, // 'Learn about scheduling'
+  'empty-state.action.documentation': 'Даведацца пра планаванне',
   /** Empty state description for scheduled drafts */
-  'empty-state.description': undefined, // 'Schedule and lock draft documents for publishing at a future date and time.'
+  'empty-state.description':
+    'Плануйце і блакуйце чарнавыя дакументы для публікацыі ў будучай даце і часе.',
   /** Empty state title for scheduled drafts */
-  'empty-state.title': undefined, // 'Scheduled Drafts'
+  'empty-state.title': 'Запланаваныя чарнавікі',
 })
