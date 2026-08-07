d6d9c8725a4af522e1709107ece2a34d9ab13897
AutoTranslator <hello@sanity.io>
2026-08-07 11:49:10 +0000

    fix(ka-GE): automated translation updates

--- locales/ka-GE/src/studio.ts
+++ locales/ka-GE/src/studio.ts
@@ -1504,7 +1504,7 @@ export default removeUndefinedLocaleResources({
   /** Description for toast when version discarding failed */
   'release.action.discard-version.failure': 'ვერსიის გაუქმება ჩავარდა',
   /** Tooltip/label for the action that opens the release edit dialog on the detail page */
-  'release.action.edit-details': undefined, // 'Edit details'
+  'release.action.edit-details': 'დეტალების რედაქტირება',
   /** Action message for editing the schedule of a scheduled publish */
   'release.action.edit-schedule': 'განრიგის რედაქტირება',
   /** Action message for when a new release is created off an existing version, draft or published document */
@@ -1612,13 +1612,13 @@ export default removeUndefinedLocaleResources({
   /** Header for change schedule dialog */
   'release.dialog.edit-schedule.header': 'განრიგის შეცვლა',
   /** Label for the save action in the edit release dialog */
-  'release.dialog.edit.confirm': undefined, // 'Save'
+  'release.dialog.edit.confirm': 'შენახვა',
   /** Field label for the release description in the edit release dialog */
-  'release.dialog.edit.description-label': undefined, // 'Description'
+  'release.dialog.edit.description-label': 'აღწერა',
   /** Title for the edit release dialog */
-  'release.dialog.edit.title': undefined, // 'Edit release'
+  'release.dialog.edit.title': 'release-ის რედაქტირება',
   /** Field label for the release title in the edit release dialog */
-  'release.dialog.edit.title-label': undefined, // 'Title'
+  'release.dialog.edit.title-label': 'სათაური',
   /** Body text for the dialog confirming running a scheduled draft immediately */
   'release.dialog.publish-scheduled-draft.body':
     'დარწმუნებული ხართ, რომ გსურთ ამ დაგეგმილი ჩანაწერის მყისიერად გამოქვეყნება?',
@@ -1674,7 +1674,7 @@ export default removeUndefinedLocaleResources({
   'release.toast.delete-schedule-draft.success':
     'დაგეგმილი ჩანაწერის დოკუმენტი <strong>{{title}}</strong> წაშლილია.',
   /** The toast title shown when saving edits to a release's details fails */
-  'release.toast.edit-release-error.title': undefined, // 'Failed to save release details'
+  'release.toast.edit-release-error.title': 'release-ის დეტალების შენახვა ვერ მოხერხდა',
   /** The toast title that will be shown when the user has a release perspective which is now deleted
    * @deprecated – no longer needed
    * */
