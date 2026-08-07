a197984f66dac470739dc5c5a7804260a94b0231
AutoTranslator <hello@sanity.io>
2026-08-07 11:49:13 +0000

    fix(be-BY): automated translation updates

--- locales/be-BY/src/structure.ts
+++ locales/be-BY/src/structure.ts
@@ -428,13 +428,13 @@ export default removeUndefinedLocaleResources({
   /** The title shown in the dialog header, when the document being inspected is not created yet/has no value */
   'document-inspector.dialog.title-no-value': 'Няма значэння',
   /** Accessibility label for the close button shown when an inspector panel failed to render */
-  'document-inspector.error.close-button.aria-label': undefined, // 'Close panel'
+  'document-inspector.error.close-button.aria-label': 'Закрыць панэль',
   /** Text explaining that the inspector panel failed to render */
-  'document-inspector.error.description': undefined, // 'An error occurred while rendering this panel.'
+  'document-inspector.error.description': 'Падчас адлюстравання гэтай панэлі адбылася памылка.',
   /** Label for the button that attempts to render the inspector panel again */
-  'document-inspector.error.retry-button.text': undefined, // 'Retry'
+  'document-inspector.error.retry-button.text': 'Паўтарыць',
   /** The title shown in the inspector panel header when the panel failed to render */
-  'document-inspector.error.title': undefined, // 'Something went wrong'
+  'document-inspector.error.title': 'Нешта пайшло не так',
   /** Title shown for menu item that opens the "Inspect" dialog */
   'document-inspector.menu-item.title': 'Інспекцыя',
   /** the placeholder text for the search input on the inspect dialog */
@@ -445,7 +445,7 @@ export default removeUndefinedLocaleResources({
   'document-inspector.view-mode.raw-json': 'Сыры JSON',
 
   /** Tooltip on target badges when the document does not exist in the selected perspective */
-  'document-target-badges.not-in-target.tooltip': undefined, // 'Document doesn\'t exist in the selected perspective yet.'
+  'document-target-badges.not-in-target.tooltip': 'Дакумент яшчэ не існуе ў выбраным perspective.',
 
   /** The text for when a form is hidden */
   'document-view.form-view.form-hidden': 'Гэтая форма схаваная',
