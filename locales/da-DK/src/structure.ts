4e62fbf73139b8410dd3a3b4396088a0cadfa75a
AutoTranslator <hello@sanity.io>
2026-08-07 11:49:09 +0000

    fix(da-DK): automated translation updates

--- locales/da-DK/src/structure.ts
+++ locales/da-DK/src/structure.ts
@@ -456,13 +456,13 @@ export default removeUndefinedLocaleResources({
   /** The title shown in the dialog header, when the document being inspected is not created yet/has no value */
   'document-inspector.dialog.title-no-value': 'Ingen værdi',
   /** Accessibility label for the close button shown when an inspector panel failed to render */
-  'document-inspector.error.close-button.aria-label': undefined, // 'Close panel'
+  'document-inspector.error.close-button.aria-label': 'Luk panel',
   /** Text explaining that the inspector panel failed to render */
-  'document-inspector.error.description': undefined, // 'An error occurred while rendering this panel.'
+  'document-inspector.error.description': 'Der opstod en fejl under gengivelsen af dette panel.',
   /** Label for the button that attempts to render the inspector panel again */
-  'document-inspector.error.retry-button.text': undefined, // 'Retry'
+  'document-inspector.error.retry-button.text': 'Prøv igen',
   /** The title shown in the inspector panel header when the panel failed to render */
-  'document-inspector.error.title': undefined, // 'Something went wrong'
+  'document-inspector.error.title': 'Noget gik galt',
   /** Title shown for menu item that opens the "Inspect" dialog */
   'document-inspector.menu-item.title': 'Inspekter',
   /** the placeholder text for the search input on the inspect dialog */
@@ -473,7 +473,8 @@ export default removeUndefinedLocaleResources({
   'document-inspector.view-mode.raw-json': 'Rå JSON',
 
   /** Tooltip on target badges when the document does not exist in the selected perspective */
-  'document-target-badges.not-in-target.tooltip': undefined, // 'Document doesn\'t exist in the selected perspective yet.'
+  'document-target-badges.not-in-target.tooltip':
+    'Dokumentet findes endnu ikke i det valgte perspective.',
 
   /** The text for when a form is hidden */
   'document-view.form-view.form-hidden': 'Denne formular er skjult',
