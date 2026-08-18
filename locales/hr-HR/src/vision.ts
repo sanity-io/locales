cb9ba5591ba58baa2a8bd8643835c8bdd59a4871
AutoTranslator <hello@sanity.io>
2026-08-18 17:38:53 +0000

    fix(hr-HR): automated translation updates

--- locales/hr-HR/src/vision.ts
+++ locales/hr-HR/src/vision.ts
@@ -109,7 +109,8 @@ export default removeUndefinedLocaleResources({
   /** Label for the "API version" dropdown in settings */
   'settings.api-version-label': 'Verzija API-ja',
   /** Tooltip shown when the API version selector is locked to vX because a variant is selected */
-  'settings.api-version-locked-for-variant': undefined, // 'When a variant is selected, the API version needs to be vX.'
+  'settings.api-version-locked-for-variant':
+    'Kada je odabrana varijanta, verzija API-ja mora biti vX.',
   /** Label for the "Custom API version" input in settings, shown when "other" is chosen as API version */
   'settings.custom-api-version-label': 'Prilagođena verzija API-ja',
   /** Label for the "Dataset" dropdown in vision settings */
