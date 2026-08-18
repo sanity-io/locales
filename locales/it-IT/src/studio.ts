f1b81ebd97a29c9a3db4d1ee547818136c42913b
AutoTranslator <hello@sanity.io>
2026-08-18 17:38:59 +0000

    fix(it-IT): automated translation updates

--- locales/it-IT/src/studio.ts
+++ locales/it-IT/src/studio.ts
@@ -495,7 +495,7 @@ export default removeUndefinedLocaleResources({
   /** The label shown when dismissing the document group inventory */
   'document-group-inventory.action.cancel': 'Annulla',
   /** The label for the button that opens the document group inventory */
-  'document-group-inventory.action.manage-versions': undefined, // 'Manage versions'
+  'document-group-inventory.action.manage-versions': 'Gestisci versioni',
   /** The label used in the feedback dialog asking how easy the document group inventory is to use */
   'document-group-inventory.feedback.sentiment-label':
     'Quanto è facile o difficile utilizzare il nuovo inventario delle versioni?',
@@ -1519,7 +1519,7 @@ export default removeUndefinedLocaleResources({
   /** Description for toast when version discarding failed */
   'release.action.discard-version.failure': 'Impossibile scartare la versione',
   /** Tooltip/label for the action that opens the release edit dialog on the detail page */
-  'release.action.edit-details': undefined, // 'Edit details'
+  'release.action.edit-details': 'Modifica dettagli',
   /** Action message for editing the schedule of a scheduled publish */
   'release.action.edit-schedule': 'Modifica pianificazione',
   /** Action message for when a new release is created off an existing version, draft or published document */
@@ -1630,13 +1630,13 @@ export default removeUndefinedLocaleResources({
   /** Header for change schedule dialog */
   'release.dialog.edit-schedule.header': 'Modifica pianificazione',
   /** Label for the save action in the edit release dialog */
-  'release.dialog.edit.confirm': undefined, // 'Save'
+  'release.dialog.edit.confirm': 'Salva',
   /** Field label for the release description in the edit release dialog */
-  'release.dialog.edit.description-label': undefined, // 'Description'
+  'release.dialog.edit.description-label': 'Descrizione',
   /** Title for the edit release dialog */
-  'release.dialog.edit.title': undefined, // 'Edit release'
+  'release.dialog.edit.title': 'Modifica release',
   /** Field label for the release title in the edit release dialog */
-  'release.dialog.edit.title-label': undefined, // 'Title'
+  'release.dialog.edit.title-label': 'Titolo',
   /** Body text for the dialog confirming running a scheduled draft immediately */
   'release.dialog.publish-scheduled-draft.body':
     'Sei sicuro di voler pubblicare immediatamente questa bozza pianificata?',
@@ -1693,7 +1693,7 @@ export default removeUndefinedLocaleResources({
   'release.toast.delete-schedule-draft.success':
     'Il documento di bozza pianificato <strong>{{title}}</strong> è stato eliminato.',
   /** The toast title shown when saving edits to a release's details fails */
-  'release.toast.edit-release-error.title': undefined, // 'Failed to save release details'
+  'release.toast.edit-release-error.title': 'Impossibile salvare i dettagli della release',
   /** The toast title that will be shown when the user has a release perspective which is now deleted
    * @deprecated – no longer needed
    * */
@@ -2289,7 +2289,7 @@ export default removeUndefinedLocaleResources({
   'tree-editing-dialog.sidebar.action.open': 'Apri barra laterale',
 
   /** Label for action to claim a project created through unauthenticated provisioning */
-  'user-menu.action.claim-project': undefined, // 'Claim this project'
+  'user-menu.action.claim-project': 'Rivendica questo progetto',
   /** Label for button showing the free trial days left */
   'user-menu.action.free-trial_one': '{{count}} giorno rimanente nel periodo di prova',
   'user-menu.action.free-trial_other': '{{count}} giorni rimanenti nel periodo di prova',
