06600ff5bca8697b4a73e2096db9e8d63e989378
AutoTranslator <hello@sanity.io>
2026-08-18 17:38:34 +0000

    fix(zh-Hans): automated translation updates

--- locales/zh-Hans/src/vision.ts
+++ locales/zh-Hans/src/vision.ts
@@ -108,7 +108,7 @@ export default removeUndefinedLocaleResources({
   /** Label for the "API version" dropdown in settings */
   'settings.api-version-label': 'API 版本',
   /** Tooltip shown when the API version selector is locked to vX because a variant is selected */
-  'settings.api-version-locked-for-variant': undefined, // 'When a variant is selected, the API version needs to be vX.'
+  'settings.api-version-locked-for-variant': '选择变体后,API 版本需要为 vX。',
   /** Label for the "Custom API version" input in settings, shown when "other" is chosen as API version */
   'settings.custom-api-version-label': '自定义 API 版本',
   /** Label for the "Dataset" dropdown in vision settings */
