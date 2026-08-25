45c8463a55b3587e2fdb55b1623039b44c4157b4
AutoTranslator <hello@sanity.io>
2026-08-25 20:50:10 +0000

    fix(fi-FI): automated translation updates

--- locales/fi-FI/src/releases.ts
+++ locales/fi-FI/src/releases.ts
@@ -336,7 +336,7 @@ export default removeUndefinedLocaleResources({
   'presence.tooltip.one':
     '{{displayName}} muokkaa tätä asiakirjaa "{{releaseTitle}}"-releaseessa juuri nyt',
   /** Tooltip text for when one user is editing a published or draft document */
-  'presence.tooltip.one-without-release': undefined, // '{{displayName}} is editing this document right now'
+  'presence.tooltip.one-without-release': '{{displayName}} muokkaa tätä dokumenttia juuri nyt',
 
   /** Tooltip text for publish release action when there are no documents */
   'publish-action.validation.no-documents': 'Ei ole asiakirjoja julkaistavaksi',
--- locales/fi-FI/src/studio.ts
+++ locales/fi-FI/src/studio.ts
@@ -462,7 +462,7 @@ export default removeUndefinedLocaleResources({
   'default-orderings.title': 'Lajittele otsikon mukaan',
 
   /** Menu item that opens Studio diagnostics */
-  'diagnostics.menu-item': undefined, // 'Diagnostics'
+  'diagnostics.menu-item': 'Diagnostiikka',
 
   /** Label for action that closes divergence inspector */
   'divergence.action.close.label': 'Sulje',
@@ -598,7 +598,7 @@ export default removeUndefinedLocaleResources({
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': 'Julkaistu {{date}}',
   /** Label to show in document versions status for a published (non-live-edit) document */
-  'document-status.published-at': undefined, // 'Published {{date}}'
+  'document-status.published-at': 'Julkaistu {{date}}',
   /** Label to show in the document footer indicating the revision from date of the document */
   'document-status.revision-from': 'Versio päivämäärästä <em>{{date}}</em>',
   /** Label to show in the document footer indicating that the revision was not found */
@@ -683,7 +683,7 @@ export default removeUndefinedLocaleResources({
   /** The title for the error card rendered inside a field in place of a crashing input */
   'form.error.unhandled-runtime-error.title': 'Käsittelemätön suoritusaikainen virhe',
   /** Tooltip / accessible label for the field actions overflow menu button */
-  'form.field.actions-menu.title': undefined, // 'Field actions'
+  'form.field.actions-menu.title': 'Kentän toiminnot',
   /** Form field deprecated label */
   'form.field.deprecated-label': 'vanhentunut',
   /** Fallback title shown above field if it has no defined title */
