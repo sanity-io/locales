cf3c0a4072b23893d2e7afec25617ce6bee20fbb
AutoTranslator <hello@sanity.io>
2026-09-01 11:32:49 +0000

    fix(da-DK): automated translation updates

--- locales/da-DK/src/studio.ts
+++ locales/da-DK/src/studio.ts
@@ -461,7 +461,7 @@ export default removeUndefinedLocaleResources({
   'default-orderings.title': 'Sortér efter titel',
 
   /** Menu item that opens Studio diagnostics */
-  'diagnostics.menu-item': undefined, // 'Diagnostics'
+  'diagnostics.menu-item': 'Diagnostik',
 
   /** Label for action that closes divergence inspector */
   'divergence.action.close.label': 'Luk',
@@ -596,7 +596,7 @@ export default removeUndefinedLocaleResources({
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': 'Offentliggjort {{date}}',
   /** Label to show in document versions status for a published (non-live-edit) document */
-  'document-status.published-at': undefined, // 'Published {{date}}'
+  'document-status.published-at': 'Udgivet {{date}}',
   /** Label to show in the document footer indicating the revision from date of the document */
   'document-status.revision-from': 'Revision fra <em>{{date}}</em>',
   /** Label to show in the document footer indicating that the revision was not found */
@@ -681,7 +681,7 @@ export default removeUndefinedLocaleResources({
   /** The title for the error card rendered inside a field in place of a crashing input */
   'form.error.unhandled-runtime-error.title': 'Uhåndteret kørselsfejl',
   /** Tooltip / accessible label for the field actions overflow menu button */
-  'form.field.actions-menu.title': undefined, // 'Field actions'
+  'form.field.actions-menu.title': 'Felthandlinger',
   /** Form field deprecated label */
   'form.field.deprecated-label': 'forældet',
   /** Fallback title shown above field if it has no defined title */
