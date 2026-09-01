7a45eaa20cc5e26e2bacdc9f63d71974bf56c38c
AutoTranslator <hello@sanity.io>
2026-09-01 11:32:50 +0000

    fix(ru-KZ): automated translation updates

--- locales/ru-KZ/src/studio.ts
+++ locales/ru-KZ/src/studio.ts
@@ -453,7 +453,7 @@ export default removeUndefinedLocaleResources({
   'default-orderings.title': 'Сортировать по названию',
 
   /** Menu item that opens Studio diagnostics */
-  'diagnostics.menu-item': undefined, // 'Diagnostics'
+  'diagnostics.menu-item': 'Диагностика',
 
   /** Label for action that closes divergence inspector */
   'divergence.action.close.label': 'Жабу',
@@ -562,7 +562,7 @@ export default removeUndefinedLocaleResources({
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': 'Опубликовано {{date}}',
   /** Label to show in document versions status for a published (non-live-edit) document */
-  'document-status.published-at': undefined, // 'Published {{date}}'
+  'document-status.published-at': 'Опубликовано {{date}}',
   /** Label to show in the document footer indicating the revision from date of the document */
   'document-status.revision-from': 'Ревизия от <em>{{date}}</em>',
   /** Label to show in the document footer indicating that the revision was not found */
@@ -647,7 +647,7 @@ export default removeUndefinedLocaleResources({
   /** The title for the error card rendered inside a field in place of a crashing input */
   'form.error.unhandled-runtime-error.title': 'Необработанная ошибка времени выполнения',
   /** Tooltip / accessible label for the field actions overflow menu button */
-  'form.field.actions-menu.title': undefined, // 'Field actions'
+  'form.field.actions-menu.title': 'Действия с полем',
   /** Form field deprecated label */
   'form.field.deprecated-label': 'устаревший',
   /** Fallback title shown above field if it has no defined title */
