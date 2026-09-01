6eb48b5392138fedd5fde8b2550ad7c89966deb6
AutoTranslator <hello@sanity.io>
2026-09-01 11:33:11 +0000

    fix(it-IT): automated translation updates

--- locales/it-IT/src/studio.ts
+++ locales/it-IT/src/studio.ts
@@ -472,7 +472,7 @@ export default removeUndefinedLocaleResources({
   'default-orderings.title': 'Ordina per Titolo',
 
   /** Menu item that opens Studio diagnostics */
-  'diagnostics.menu-item': undefined, // 'Diagnostics'
+  'diagnostics.menu-item': 'Diagnostica',
 
   /** Label for action that closes divergence inspector */
   'divergence.action.close.label': 'Chiudi',
@@ -607,7 +607,7 @@ export default removeUndefinedLocaleResources({
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': 'Pubblicato il {{date}}',
   /** Label to show in document versions status for a published (non-live-edit) document */
-  'document-status.published-at': undefined, // 'Published {{date}}'
+  'document-status.published-at': 'Pubblicato {{date}}',
   /** Label to show in the document footer indicating the revision from date of the document */
   'document-status.revision-from': 'Revisione del <em>{{date}}</em>',
   /** Label to show in the document footer indicating that the revision was not found */
@@ -692,7 +692,7 @@ export default removeUndefinedLocaleResources({
   /** The title for the error card rendered inside a field in place of a crashing input */
   'form.error.unhandled-runtime-error.title': 'Errore di Runtime non gestito',
   /** Tooltip / accessible label for the field actions overflow menu button */
-  'form.field.actions-menu.title': undefined, // 'Field actions'
+  'form.field.actions-menu.title': 'Azioni del campo',
   /** Form field deprecated label */
   'form.field.deprecated-label': 'deprecato',
   /** Fallback title shown above field if it has no defined title */
