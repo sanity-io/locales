6526e03a44178eb3e9b7e99eccb57a2d5a4f1da6
AutoTranslator <hello@sanity.io>
2026-09-08 17:36:29 +0000

    fix(nl-NL): automated translation updates

--- locales/nl-NL/src/structure.ts
+++ locales/nl-NL/src/structure.ts
@@ -156,7 +156,8 @@ export default removeUndefinedLocaleResources({
   /** The label for the "compare draft" action */
   'banners.obsolete-draft.actions.compare-draft.text': 'Vergelijk concept',
   /** Tooltip when compare draft is disabled because the published document does not exist */
-  'banners.obsolete-draft.actions.compare-draft.tooltip': undefined, // 'The published document doesn\'t exist, so this draft can\'t be compared.'
+  'banners.obsolete-draft.actions.compare-draft.tooltip':
+    'Het gepubliceerde document bestaat niet, dus dit concept kan niet worden vergeleken.',
   /** The label for the "discard draft" action */
   'banners.obsolete-draft.actions.discard-draft.text': 'Verwerp concept',
   /** The label for the "publish draft" action */
@@ -339,7 +340,7 @@ export default removeUndefinedLocaleResources({
   /** The error message shown when releases failed to load */
   'compare-version.error.loadReleases.title': 'Laden van publicaties mislukt',
   /** The error message shown when document versions or variants failed to load */
-  'compare-version.error.loadVersions.title': undefined, // 'Failed to load versions'
+  'compare-version.error.loadVersions.title': 'Laden van versies mislukt',
 
   /** The text for the tooltip when the "Compare versions" action for a document is disabled */
   'compare-versions.menu-item.disabled-reason':
