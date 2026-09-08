4fa2392230722c0b13aaafc6114b7c4389fd7e1d
AutoTranslator <hello@sanity.io>
2026-09-08 17:36:29 +0000

    fix(fi-FI): automated translation updates

--- locales/fi-FI/src/studio.ts
+++ locales/fi-FI/src/studio.ts
@@ -336,7 +336,7 @@ export default removeUndefinedLocaleResources({
   /** File diff: Fallback title for the meta info section when there is no original filename to use  */
   'changes.file.meta-info-fallback-title': 'Nimetön',
   /** The label displayed to indicate a document's value is different to its base variant. */
-  'changes.from-base-variant.label': undefined, // 'Changed from base variant'
+  'changes.from-base-variant.label': 'Muutettu perusversiosta',
   /** Image diff: Text shown in tooltip when hovering hotspot that has changed in diff view */
   'changes.image.crop-changed': 'Rajaus muuttunut',
   /** Image diff: Text shown if the previous image asset was deleted (shouldn't theoretically happen) */
@@ -432,7 +432,7 @@ export default removeUndefinedLocaleResources({
   /** Default text for dialog confirm button */
   'common.dialog.confirm-button.text': 'Vahvista',
   /** Default text in shared error text */
-  'common.error': undefined, // 'Error'
+  'common.error': 'Virhe',
   /** Default text in shared loader text / spinner lockup */
   'common.loading': 'Ladataan',
   /** The title of the reconnecting toast */
