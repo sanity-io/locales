02d70d8f9614dd000419fb01e2ed7d04cae6e76c
AutoTranslator <hello@sanity.io>
2026-09-08 17:36:28 +0000

    fix(be-BY): automated translation updates

--- locales/be-BY/src/structure.ts
+++ locales/be-BY/src/structure.ts
@@ -149,7 +149,8 @@ export default removeUndefinedLocaleResources({
   /** The label for the "compare draft" action */
   'banners.obsolete-draft.actions.compare-draft.text': 'Параўнаць чарнавік',
   /** Tooltip when compare draft is disabled because the published document does not exist */
-  'banners.obsolete-draft.actions.compare-draft.tooltip': undefined, // 'The published document doesn\'t exist, so this draft can\'t be compared.'
+  'banners.obsolete-draft.actions.compare-draft.tooltip':
+    'Апублікаваны дакумент не існуе, таму гэты чарнавік немагчыма параўнаць.',
   /** The label for the "discard draft" action */
   'banners.obsolete-draft.actions.discard-draft.text': 'Адхіліць чарнавік',
   /** The label for the "publish draft" action */
@@ -323,7 +324,7 @@ export default removeUndefinedLocaleResources({
   /** The error message shown when releases failed to load */
   'compare-version.error.loadReleases.title': 'Не ўдалося загрузіць выпускі',
   /** The error message shown when document versions or variants failed to load */
-  'compare-version.error.loadVersions.title': undefined, // 'Failed to load versions'
+  'compare-version.error.loadVersions.title': 'Не ўдалося загрузіць версіі',
 
   /** The text for the tooltip when the "Compare versions" action for a document is disabled */
   'compare-versions.menu-item.disabled-reason':
