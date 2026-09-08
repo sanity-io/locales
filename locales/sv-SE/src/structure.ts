32ffd90f6f39ab3012ec09262ea85a7a2324d4ba
AutoTranslator <hello@sanity.io>
2026-09-08 17:37:27 +0000

    fix(sv-SE): automated translation updates

--- locales/sv-SE/src/structure.ts
+++ locales/sv-SE/src/structure.ts
@@ -156,7 +156,8 @@ export default removeUndefinedLocaleResources({
   /** The label for the "compare draft" action */
   'banners.obsolete-draft.actions.compare-draft.text': 'Jämför utkast',
   /** Tooltip when compare draft is disabled because the published document does not exist */
-  'banners.obsolete-draft.actions.compare-draft.tooltip': undefined, // 'The published document doesn\'t exist, so this draft can\'t be compared.'
+  'banners.obsolete-draft.actions.compare-draft.tooltip':
+    'Det publicerade dokumentet finns inte, så det här utkastet kan inte jämföras.',
   /** The label for the "discard draft" action */
   'banners.obsolete-draft.actions.discard-draft.text': 'Kassera utkast',
   /** The label for the "publish draft" action */
@@ -338,7 +339,7 @@ export default removeUndefinedLocaleResources({
   /** The error message shown when releases failed to load */
   'compare-version.error.loadReleases.title': 'Misslyckades med att ladda utgåvor',
   /** The error message shown when document versions or variants failed to load */
-  'compare-version.error.loadVersions.title': undefined, // 'Failed to load versions'
+  'compare-version.error.loadVersions.title': 'Det gick inte att läsa in versioner',
 
   /** The text for the tooltip when the "Compare versions" action for a document is disabled */
   'compare-versions.menu-item.disabled-reason':
