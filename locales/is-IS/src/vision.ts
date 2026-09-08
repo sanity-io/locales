fb1d8d4ed2aab47b0b980c88ddbcf237dfe38e8b
AutoTranslator <hello@sanity.io>
2026-09-08 17:36:56 +0000

    fix(is-IS): automated translation updates

--- locales/is-IS/src/vision.ts
+++ locales/is-IS/src/vision.ts
@@ -70,9 +70,11 @@ export default removeUndefinedLocaleResources({
   /** Label for 'Line' indicator when there is an error within the query */
   'query.error.line': 'Lína',
   /** Extra explanation when a 400 is returned for a release perspective on an old API version */
-  'query.error.unsupported-release-perspective': undefined, // 'This API version does not support complex perspectives. Use {{apiVersion}} or later.'
+  'query.error.unsupported-release-perspective':
+    'Þessi útgáfa API styður ekki flókin sjónarhorn (perspectives). Notaðu {{apiVersion}} eða nýrra.',
   /** Extra explanation when a 400 is returned for a variant on an incompatible API version */
-  'query.error.unsupported-variant': undefined, // 'This API version does not support content variants. Use the experimental API version ({{apiVersion}}).'
+  'query.error.unsupported-variant':
+    'Þessi útgáfa API styður ekki efnisafbrigði (content variants). Notaðu tilraunaútgáfu API ({{apiVersion}}).',
   /** Label for "Query" editor/input */
   'query.label': 'Fyrirspurn',
   /** Label for the "Query URL" field, shown after executing a query, and allows for copying */
