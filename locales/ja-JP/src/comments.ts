b277d0df97da6a6acf4cc5b2d853595c5513b1fe
AutoTranslator <hello@sanity.io>
2026-09-08 17:37:00 +0000

    fix(ja-JP): automated translation updates

--- locales/ja-JP/src/comments.ts
+++ locales/ja-JP/src/comments.ts
@@ -114,9 +114,9 @@ export default removeUndefinedLocaleResources({
   /** The aria label for the comments menu button to open the actions menu */
   'list-item.open-menu-aria-label': 'コメントアクションメニューを開く',
   /** Badge shown when a comment was created on the draft but is viewed elsewhere */
-  'list-item.origin.draft': undefined, // 'From draft'
+  'list-item.origin.draft': '下書きから',
   /** Badge shown when a comment was created on the published document but is viewed elsewhere */
-  'list-item.origin.published': undefined, // 'From published'
+  'list-item.origin.published': '公開済みから',
   /** The button text to re-open a resolved comment  */
   'list-item.re-open-resolved': '再開',
   /** The button aria label to re-open a comment that is resolved */
--- locales/ja-JP/src/studio.ts
+++ locales/ja-JP/src/studio.ts
@@ -340,7 +340,7 @@ export default removeUndefinedLocaleResources({
   /** File diff: Fallback title for the meta info section when there is no original filename to use  */
   'changes.file.meta-info-fallback-title': '無題',
   /** The label displayed to indicate a document's value is different to its base variant. */
-  'changes.from-base-variant.label': undefined, // 'Changed from base variant'
+  'changes.from-base-variant.label': 'ベースバリアントから変更されました',
   /** Image diff: Text shown in tooltip when hovering hotspot that has changed in diff view */
   'changes.image.crop-changed': 'クロップが変更されました',
   /** Image diff: Text shown if the previous image asset was deleted (shouldn't theoretically happen) */
@@ -436,7 +436,7 @@ export default removeUndefinedLocaleResources({
   /** Default text for dialog confirm button */
   'common.dialog.confirm-button.text': '確認',
   /** Default text in shared error text */
-  'common.error': undefined, // 'Error'
+  'common.error': 'エラー',
   /** Default text in shared loader text / spinner lockup */
   'common.loading': '読み込み中',
   /** The title of the reconnecting toast */
@@ -470,7 +470,7 @@ export default removeUndefinedLocaleResources({
   'default-orderings.title': 'タイトルで並べ替え',
 
   /** Menu item that opens Studio diagnostics */
-  'diagnostics.menu-item': undefined, // 'Diagnostics'
+  'diagnostics.menu-item': '診断',
 
   /** Label for action that closes divergence inspector */
   'divergence.action.close.label': '閉じる',
@@ -580,7 +580,7 @@ export default removeUndefinedLocaleResources({
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': '{{date}}に公開されました',
   /** Label to show in document versions status for a published (non-live-edit) document */
-  'document-status.published-at': undefined, // 'Published {{date}}'
+  'document-status.published-at': '{{date}}に公開済み',
   /** Label to show in the document footer indicating the revision from date of the document */
   'document-status.revision-from': '改訂日 <em>{{date}}</em>',
   /** Label to show in the document footer indicating that the revision was not found */
@@ -665,7 +665,7 @@ export default removeUndefinedLocaleResources({
   /** The title for the error card rendered inside a field in place of a crashing input */
   'form.error.unhandled-runtime-error.title': '処理されないランタイムエラー',
   /** Tooltip / accessible label for the field actions overflow menu button */
-  'form.field.actions-menu.title': undefined, // 'Field actions'
+  'form.field.actions-menu.title': 'フィールドアクション',
   /** Form field deprecated label */
   'form.field.deprecated-label': '非推奨',
   /** Fallback title shown above field if it has no defined title */
