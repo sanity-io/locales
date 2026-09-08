404c49c23ec2c2c2249e0fc6145efa25048d53ac
AutoTranslator <hello@sanity.io>
2026-09-08 17:37:23 +0000

    fix(ro-RO): automated translation updates

--- locales/ro-RO/src/comments.ts
+++ locales/ro-RO/src/comments.ts
@@ -116,9 +116,9 @@ export default removeUndefinedLocaleResources({
   /** The aria label for the comments menu button to open the actions menu */
   'list-item.open-menu-aria-label': 'Deschide meniul de acțiuni pentru comentarii',
   /** Badge shown when a comment was created on the draft but is viewed elsewhere */
-  'list-item.origin.draft': undefined, // 'From draft'
+  'list-item.origin.draft': 'Din ciornă',
   /** Badge shown when a comment was created on the published document but is viewed elsewhere */
-  'list-item.origin.published': undefined, // 'From published'
+  'list-item.origin.published': 'Din documentul publicat',
   /** The button text to re-open a resolved comment  */
   'list-item.re-open-resolved': 'Redeschide',
   /** The button aria label to re-open a comment that is resolved */
--- locales/ro-RO/src/releases.ts
+++ locales/ro-RO/src/releases.ts
@@ -323,7 +323,7 @@ export default removeUndefinedLocaleResources({
   'presence.tooltip.one':
     '{{displayName}} editează acest document în lansarea „{{releaseTitle}}" chiar acum',
   /** Tooltip text for when one user is editing a published or draft document */
-  'presence.tooltip.one-without-release': undefined, // '{{displayName}} is editing this document right now'
+  'presence.tooltip.one-without-release': '{{displayName}} editează acest document chiar acum',
 
   /** Tooltip text for publish release action when there are no documents */
   'publish-action.validation.no-documents': 'Nu există documente de publicat',
