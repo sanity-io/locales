1ead1ab23756d9b9cf3efa42a03d8b9c763c66fc
AutoTranslator <hello@sanity.io>
2026-09-08 17:36:28 +0000

    fix(cs-CZ): automated translation updates

--- locales/cs-CZ/src/studio.ts
+++ locales/cs-CZ/src/studio.ts
@@ -336,7 +336,7 @@ export default removeUndefinedLocaleResources({
   /** File diff: Fallback title for the meta info section when there is no original filename to use  */
   'changes.file.meta-info-fallback-title': 'Bez názvu',
   /** The label displayed to indicate a document's value is different to its base variant. */
-  'changes.from-base-variant.label': undefined, // 'Changed from base variant'
+  'changes.from-base-variant.label': 'Změněno oproti základní variantě',
   /** Image diff: Text shown in tooltip when hovering hotspot that has changed in diff view */
   'changes.image.crop-changed': 'Změněný ořez',
   /** Image diff: Text shown if the previous image asset was deleted (shouldn't theoretically happen) */
@@ -432,7 +432,7 @@ export default removeUndefinedLocaleResources({
   /** Default text for dialog confirm button */
   'common.dialog.confirm-button.text': 'Potvrdit',
   /** Default text in shared error text */
-  'common.error': undefined, // 'Error'
+  'common.error': 'Chyba',
   /** Default text in shared loader text / spinner lockup */
   'common.loading': 'Načítání',
   /** The title of the reconnecting toast */
