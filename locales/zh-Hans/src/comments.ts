ac35af8458d525f0163b94d11ea7134a30801a72
AutoTranslator <hello@sanity.io>
2026-09-08 17:37:36 +0000

    fix(zh-Hans): automated translation updates

--- locales/zh-Hans/src/comments.ts
+++ locales/zh-Hans/src/comments.ts
@@ -114,9 +114,9 @@ export default removeUndefinedLocaleResources({
   /** The aria label for the comments menu button to open the actions menu */
   'list-item.open-menu-aria-label': '打开评论操作菜单',
   /** Badge shown when a comment was created on the draft but is viewed elsewhere */
-  'list-item.origin.draft': undefined, // 'From draft'
+  'list-item.origin.draft': '来自草稿',
   /** Badge shown when a comment was created on the published document but is viewed elsewhere */
-  'list-item.origin.published': undefined, // 'From published'
+  'list-item.origin.published': '来自已发布内容',
   /** The button text to re-open a resolved comment  */
   'list-item.re-open-resolved': '重新打开',
   /** The button aria label to re-open a comment that is resolved */
