f060d3cbe4676b0c6c2a8b9eab3d993d8018e5ec
AutoTranslator <hello@sanity.io>
2026-09-08 17:36:30 +0000

    fix(uk-UA): automated translation updates

--- locales/uk-UA/src/structure.ts
+++ locales/uk-UA/src/structure.ts
@@ -153,7 +153,8 @@ export default removeUndefinedLocaleResources({
   /** The label for the "compare draft" action */
   'banners.obsolete-draft.actions.compare-draft.text': 'Порівняти чернетку',
   /** Tooltip when compare draft is disabled because the published document does not exist */
-  'banners.obsolete-draft.actions.compare-draft.tooltip': undefined, // 'The published document doesn\'t exist, so this draft can\'t be compared.'
+  'banners.obsolete-draft.actions.compare-draft.tooltip':
+    'Опублікований документ не існує, тому цей чернетковий варіант неможливо порівняти.',
   /** The label for the "discard draft" action */
   'banners.obsolete-draft.actions.discard-draft.text': 'Відхилити чернетку',
   /** The label for the "publish draft" action */
@@ -327,7 +328,7 @@ export default removeUndefinedLocaleResources({
   /** The error message shown when releases failed to load */
   'compare-version.error.loadReleases.title': 'Не вдалося завантажити релізи',
   /** The error message shown when document versions or variants failed to load */
-  'compare-version.error.loadVersions.title': undefined, // 'Failed to load versions'
+  'compare-version.error.loadVersions.title': 'Не вдалося завантажити версії',
 
   /** The text for the tooltip when the "Compare versions" action for a document is disabled */
   'compare-versions.menu-item.disabled-reason':
--- locales/uk-UA/src/vision.ts
+++ locales/uk-UA/src/vision.ts
@@ -70,9 +70,11 @@ export default removeUndefinedLocaleResources({
   /** Label for 'Line' indicator when there is an error within the query */
   'query.error.line': 'Лінія',
   /** Extra explanation when a 400 is returned for a release perspective on an old API version */
-  'query.error.unsupported-release-perspective': undefined, // 'This API version does not support complex perspectives. Use {{apiVersion}} or later.'
+  'query.error.unsupported-release-perspective':
+    'Ця версія API не підтримує складні перспективи. Використовуйте {{apiVersion}} або новішу версію.',
   /** Extra explanation when a 400 is returned for a variant on an incompatible API version */
-  'query.error.unsupported-variant': undefined, // 'This API version does not support content variants. Use the experimental API version ({{apiVersion}}).'
+  'query.error.unsupported-variant':
+    'Ця версія API не підтримує варіанти контенту. Використовуйте експериментальну версію API ({{apiVersion}}).',
   /** Label for "Query" editor/input */
   'query.label': 'Запит',
   /** Label for the "Query URL" field, shown after executing a query, and allows for copying */
