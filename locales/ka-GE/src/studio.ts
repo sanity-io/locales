4a279c229c5b245adcd2f1c0a469265a61f6e126
AutoTranslator <hello@sanity.io>
2026-09-08 17:37:03 +0000

    fix(ka-GE): automated translation updates

--- locales/ka-GE/src/studio.ts
+++ locales/ka-GE/src/studio.ts
@@ -336,7 +336,7 @@ export default removeUndefinedLocaleResources({
   /** File diff: Fallback title for the meta info section when there is no original filename to use  */
   'changes.file.meta-info-fallback-title': 'უსათაურო',
   /** The label displayed to indicate a document's value is different to its base variant. */
-  'changes.from-base-variant.label': undefined, // 'Changed from base variant'
+  'changes.from-base-variant.label': 'შეცვლილია საბაზისო ვარიანტიდან',
   /** Image diff: Text shown in tooltip when hovering hotspot that has changed in diff view */
   'changes.image.crop-changed': 'კროპი შეცვლილია',
   /** Image diff: Text shown if the previous image asset was deleted (shouldn't theoretically happen) */
@@ -432,7 +432,7 @@ export default removeUndefinedLocaleResources({
   /** Default text for dialog confirm button */
   'common.dialog.confirm-button.text': 'დადასტურება',
   /** Default text in shared error text */
-  'common.error': undefined, // 'Error'
+  'common.error': 'შეცდომა',
   /** Default text in shared loader text / spinner lockup */
   'common.loading': 'იტვირთება',
   /** The title of the reconnecting toast */
@@ -466,7 +466,7 @@ export default removeUndefinedLocaleResources({
   'default-orderings.title': 'დალაგება სათაურის მიხედვით',
 
   /** Menu item that opens Studio diagnostics */
-  'diagnostics.menu-item': undefined, // 'Diagnostics'
+  'diagnostics.menu-item': 'დიაგნოსტიკა',
 
   /** Label for action that closes divergence inspector */
   'divergence.action.close.label': 'დახურვა',
@@ -601,7 +601,7 @@ export default removeUndefinedLocaleResources({
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': 'გამოქვეყნებულია {{date}}',
   /** Label to show in document versions status for a published (non-live-edit) document */
-  'document-status.published-at': undefined, // 'Published {{date}}'
+  'document-status.published-at': 'გამოქვეყნდა {{date}}',
   /** Label to show in the document footer indicating the revision from date of the document */
   'document-status.revision-from': 'რედაქციიდან <em>{{date}}</em>',
   /** Label to show in the document footer indicating that the revision was not found */
@@ -686,7 +686,7 @@ export default removeUndefinedLocaleResources({
   /** The title for the error card rendered inside a field in place of a crashing input */
   'form.error.unhandled-runtime-error.title': 'დაუმართავი შესრულების შეცდომა',
   /** Tooltip / accessible label for the field actions overflow menu button */
-  'form.field.actions-menu.title': undefined, // 'Field actions'
+  'form.field.actions-menu.title': 'ველის მოქმედებები',
   /** Form field deprecated label */
   'form.field.deprecated-label': 'გაუქმებული',
   /** Fallback title shown above field if it has no defined title */
