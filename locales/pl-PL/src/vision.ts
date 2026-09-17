4509b4e81154e041fca9bfc83cb7cfb73b8522b3
AutoTranslator <hello@sanity.io>
2026-09-17 09:14:03 +0000

    fix(pl-PL): automated translation updates

--- locales/pl-PL/src/vision.ts
+++ locales/pl-PL/src/vision.ts
@@ -70,9 +70,11 @@ export default removeUndefinedLocaleResources({
   /** Label for 'Line' indicator when there is an error within the query */
   'query.error.line': 'Linia',
   /** Extra explanation when a 400 is returned for a release perspective on an old API version */
-  'query.error.unsupported-release-perspective': undefined, // 'This API version does not support complex perspectives. Use {{apiVersion}} or later.'
+  'query.error.unsupported-release-perspective':
+    'Ta wersja API nie obsługuje złożonych perspektyw. Użyj wersji {{apiVersion}} lub nowszej.',
   /** Extra explanation when a 400 is returned for a variant on an incompatible API version */
-  'query.error.unsupported-variant': undefined, // 'This API version does not support content variants. Use the experimental API version ({{apiVersion}}).'
+  'query.error.unsupported-variant':
+    'Ta wersja API nie obsługuje wariantów treści. Użyj eksperymentalnej wersji API ({{apiVersion}}).',
   /** Label for "Query" editor/input */
   'query.label': 'Zapytanie',
   /** Label for the "Query URL" field, shown after executing a query, and allows for copying */
